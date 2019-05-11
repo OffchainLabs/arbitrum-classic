6ca40215353187dc15566f1d17c75b68d0e88260
Avniel Dravid <av@offchainlabs.com>
2019-05-11 11:51:32 -0400

    change navigation and landing page

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,10 +38,10 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'AVM_Specification', label: 'Arbitrum Spec'},
     {doc: 'Developer_Quickstart', label: 'Developer Quickstart'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: 'AVM_Specification', label: 'Arbitrum Spec'},
+    {href: 'https://medium.com/offchainlabs', label: 'Blog', external: true},
+    {href: 'https://github.com/OffchainLabs', label: 'GitHub', external: true},
   ],
 
   // If you have users set above, you add it here:
