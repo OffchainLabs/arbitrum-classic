687cf0f043e9f64aae0943ad80057e21e9cabafe
Harry Kalodner <harry.kalodner@gmail.com>
2019-05-14 16:14:20 -0400

    Provise web3 interface and hardcode network id

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/index.js')
+var ArbProvider = require('./lib/index.js');
+var ProviderBridge = require('ethers-web3-bridge');
+
+module.exports = function(managerUrl, contracts, provider) {
+	let arbProvider = new ArbProvider(managerUrl, contracts, provider)
+	let wallet = arbProvider.getSigner(0);
+	return new ProviderBridge(arbProvider, wallet);
+}
