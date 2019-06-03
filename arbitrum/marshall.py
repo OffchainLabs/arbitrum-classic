b7f3a6c10fb486e68c857c4d2921a0a18694f424
Harry Kalodner <harry.kalodner@gmail.com>
2019-06-03 01:45:48 -0400

    Switch all numbers to bigendian for simplicity

--- arbitrum/marshall.py
+++ arbitrum/marshall.py
@@ -14,23 +14,23 @@ def marshall_int(val, file):
 def marshall_op(val, file):
     if isinstance(val, BasicOp):
         op_type = 0
-        file.write(op_type.to_bytes(1, byteorder='little', signed=False))
-        file.write(val.op_code.to_bytes(1, byteorder='little', signed=False))
+        file.write(op_type.to_bytes(1, byteorder='big', signed=False))
+        file.write(val.op_code.to_bytes(1, byteorder='big', signed=False))
     elif isinstance(val, int):
         op_type = 0
-        file.write(op_type.to_bytes(1, byteorder='little', signed=False))
-        file.write(val.to_bytes(1, byteorder='little', signed=False))
+        file.write(op_type.to_bytes(1, byteorder='big', signed=False))
+        file.write(val.to_bytes(1, byteorder='big', signed=False))
     elif isinstance(val, ImmediateOp):
         op_type = 1
-        file.write(op_type.to_bytes(1, byteorder='little', signed=False))
-        file.write(val.get_op().to_bytes(1, byteorder='little', signed=False))
+        file.write(op_type.to_bytes(1, byteorder='big', signed=False))
+        file.write(val.get_op().to_bytes(1, byteorder='big', signed=False))
         marshall_value(val.val, file)
     else:
         raise Exception(f"Tried to marshall bad operation type {val}")
 
 
 def marshall_codepoint(val, file):
-    file.write(val.pc.to_bytes(8, byteorder='little', signed=True))
+    file.write(val.pc.to_bytes(8, byteorder='big', signed=True))
     marshall_op(val.op, file)
     val.next_hash = b'0' * (32 - len(val.next_hash)) + val.next_hash
     file.write(val.next_hash)
@@ -45,28 +45,28 @@ def marshall_value(val, file):
     if isinstance(val, value.Tuple):
         file.write((TUPLE_TYPE_CODE + len(val)).to_bytes(
             1,
-            byteorder='little',
+            byteorder='big',
             signed=False
         ))
         marshall_tuple(val, file)
     elif isinstance(val, int):
         file.write(INT_TYPE_CODE.to_bytes(
             1,
-            byteorder='little',
+            byteorder='big',
             signed=False
         ))
         marshall_int(val, file)
     elif isinstance(val, value.AVMCodePoint):
         file.write(CODE_POINT_TYPE_CODE.to_bytes(
             1,
-            byteorder='little',
+            byteorder='big',
             signed=False
         ))
         marshall_codepoint(val, file)
     elif isinstance(val, AVMLabeledCodePoint):
         file.write(CODE_POINT_TYPE_CODE.to_bytes(
             1,
-            byteorder='little',
+            byteorder='big',
             signed=False
         ))
         marshall_codepoint(val.pc, file)
@@ -75,7 +75,7 @@ def marshall_value(val, file):
 
 
 def marshall_vm(vm, file):
-    file.write(len(vm.code).to_bytes(8, byteorder='little', signed=True))
+    file.write(len(vm.code).to_bytes(8, byteorder='big', signed=True))
     for instr in vm.code:
         marshall_op(instr.op, file)
     marshall_value(vm.static, file)
