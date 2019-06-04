4ef94debb7a7b5d6773e96f7952a4508aa471ba1
Harry Kalodner <harry.kalodner@gmail.com>
2019-06-04 13:34:30 -0400

    Update license and version

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@
 from setuptools import setup
 
 setup(name='arbc-solidity',
-      version='0.1',
+      version='0.1.0',
       description='Compiler from solidity to AVM bytecode',
       url='http://github.com/OffchainLabs/arbc-solidity',
       author='Harry Kalodner',
       author_email='harry@offchainlabs.com',
-      license='UNLICENSED',
+      license='Apache-2.0',
       packages=['arbitrum', 'arbitrum.std', 'arbitrum.evm'],
       zip_safe=False,
       scripts=['bin/arbc-truffle-compile', 'bin/arbc-compile'],
