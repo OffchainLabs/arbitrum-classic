101cd0679e51a1c7ab602d7b00f6d1effe3a6903
Harry Kalodner <harry.kalodner@gmail.com>
2019-06-19 02:43:12 -0400

    Inline datastack

--- datastack.hpp
+++ datastack.hpp
@@ -9,33 +9,59 @@
 #ifndef datastack_hpp
 #define datastack_hpp
 
+#include "pool.hpp"
+#include "tuple.hpp"
+#include "value.hpp"
+
 #include <stdio.h>
 #include <stack>
 #include <unordered_map>
-#include "pool.hpp"
-#include "value.hpp"
 
 class datastack{
 public:
     
     std::vector<value> basedatastack;
-//    std::unordered_map<uint64_t, uint64_t> pcmap;
     unsigned int size;
     
     datastack() {
         basedatastack.reserve(1000);
     }
     
-    void push(value && newdata);
-    value pop();
-    value &peek();
-    void popSet(value &val);
-    uint64_t stacksize();
+    void push(value && newdata) {
+        basedatastack.push_back(std::move(newdata));
+    };
     
-//    void pcpush(uint64_t i, uint64_t j);
-//    std::pair<int,uint64_t> jmp();
-//    int tget();
-//    int tset();
+    value pop() {
+        auto stackEmpty = basedatastack.empty();
+        if (stackEmpty){
+            throw std::runtime_error("Stack is empty");
+        }
+        
+        auto val = std::move(basedatastack.back());
+        basedatastack.pop_back();
+        return val;
+    }
+    
+    value &peek() {
+        if (basedatastack.size()==0){
+            throw std::runtime_error("Stack is empty");
+        }
+        
+        return basedatastack.back();
+    }
+    
+    void popSet(value &val) {
+        if (basedatastack.size()==0){
+            throw std::runtime_error("Stack is empty");
+        }
+        
+        val = std::move(basedatastack.back());
+        basedatastack.pop_back();
+    }
+    
+    uint64_t stacksize() {
+        return basedatastack.size();
+    }
 };
 
 #endif /* datastack_hpp */
