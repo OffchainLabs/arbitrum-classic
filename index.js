e76576afd52bf21c0015698910b4d6828d506a9f
William Ughetta <william@offchainlabs.com>
2019-07-24 18:16:07 -0400

    getSigner async

--- index.js
+++ index.js
@@ -11,16 +11,16 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 
 const ethers = require('ethers');
 const ArbProvider = require('arb-ethers-provider');
 var ProviderBridge = require('./ethers-web3-bridge');
 
-module.exports = function(managerUrl, contracts, provider) {
+module.exports = async function(managerUrl, contracts, provider) {
 	let wrappedProv = new ethers.providers.Web3Provider(provider)
 	let arbProvider = new ArbProvider(managerUrl, contracts, wrappedProv)
-	let wallet = arbProvider.getSigner(0);
+	let wallet = await arbProvider.getSigner(0);
 	return new ProviderBridge(arbProvider, wallet);
 }
