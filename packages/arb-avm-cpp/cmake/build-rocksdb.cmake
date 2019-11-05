e694e5cda91a37a9039289c09e214d2849e3a878
Harry Kalodner <harry@offchainlabs.com>
2019-11-05 16:20:35 -0500

    Fix up rocksdb build

--- packages/arb-avm-cpp/cmake/build-rocksdb.cmake
+++ packages/arb-avm-cpp/cmake/build-rocksdb.cmake
@@ -24,17 +24,23 @@ include(ExternalProject)
 set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
 set(THREADS_PREFER_PTHREAD_FLAG TRUE)
 find_package(Threads REQUIRED)
+find_package(zlib REQUIRED)
+find_package(bzip2 REQUIRED)
 
 ExternalProject_Add(rocksdb
     GIT_REPOSITORY "https://github.com/facebook/rocksdb.git"
-    GIT_TAG "master"
-    PREFIX "${CMAKE_CURRENT_BINARY_DIR}/rocksdb"
-    CMAKE_ARGS 
+    GIT_TAG "v6.4.6"
+    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/rocksdb"
+    BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/rocksdb"
+    CMAKE_ARGS
         -DCMAKE_CXX_STANDARD=14
         -DWITH_TESTS=OFF
-    CONFIGURE_COMMAND ""
-    BUILD_IN_SOURCE ON
-    BUILD_COMMAND make static_lib
+        -DCMAKE_POSITION_INDEPENDENT_CODE=True
+        -DWITH_LZ4=OFF
+        -DWITH_ZSTD=OFF
+        -DWITH_SNAPPY=OFF
+        -DWITH_JEMALLOC=OFF
+    BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --target rocksdb
     INSTALL_COMMAND cmake -E echo "Skipping install step."
 )
 
@@ -54,8 +60,7 @@ message(STATUS "Found RocksDB includes: ${ROCKSDB_INCLUDE_DIRS}")
 add_library(rocks INTERFACE)
 add_dependencies(rocks rocksdb)
 target_include_directories(rocks INTERFACE ${ROCKSDB_INCLUDE_DIRS})
-target_link_libraries(rocks INTERFACE ${ROCKSDB_LIBRARIES} Threads::Threads)
-
+target_link_libraries(rocks INTERFACE ${ROCKSDB_LIBRARIES} Threads::Threads ZLIB::ZLIB BZip2::BZip2)
 
 mark_as_advanced(
     ROCKSDB_LIBRARIES
