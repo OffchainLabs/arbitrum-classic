0bb71bb178bd2980b39321241e61d1242aec4925
Harry Kalodner <harry@offchainlabs.com>
2020-01-21 17:19:54 -0500

    Fix last commit

--- packages/arb-validator/rollup/opinionThread.go
+++ packages/arb-validator/rollup/opinionThread.go
@@ -227,7 +227,6 @@ func (chain *ChainObserver) prepareAssertion() *preparedAssertion {
 	beforePendingTop := beforeState.PendingTop
 	messageStack, _ := chain.pendingInbox.Substack(beforePendingTop, afterPendingTop)
 	messagesVal := chain.pendingInbox.ValueForSubseq(beforePendingTop, afterPendingTop)
-	hasNewMessages := chain.calculatedValidNode.vmProtoData.PendingTop != chain.pendingInbox.GetTopHash()
 	mach := currentOpinion.machine.Clone()
 	timeBounds := chain.currentTimeBounds()
 	maxSteps := chain.nodeGraph.params.MaxExecutionSteps
@@ -239,9 +238,9 @@ func (chain *ChainObserver) prepareAssertion() *preparedAssertion {
 
 	afterHash := mach.Hash()
 
-	blockReason := chain.calculatedValidNode.machine.IsBlocked(chain.latestBlockId.Height, hasNewMessages)
+	blockReason := chain.calculatedValidNode.machine.IsBlocked(chain.latestBlockId.Height, false)
 
-	log.Printf("Prepared assertion of %v steps, [%v, %v] with block reason %v and timebounds [%v, %v] on top of leaf %v\n", stepsRun, blockReason, timeBounds.Start.AsInt(), timeBounds.End.AsInt(), beforeHash, afterHash, currentOpinionHash)
+	log.Printf("Prepared assertion of %v steps, [%v, %v] with block reason %v and timebounds [%v, %v] on top of leaf %v\n", stepsRun, timeBounds.Start.AsInt(), timeBounds.End.AsInt(), blockReason, beforeHash, afterHash, currentOpinionHash)
 
 	var params *structures.AssertionParams
 	var claim *structures.AssertionClaim
