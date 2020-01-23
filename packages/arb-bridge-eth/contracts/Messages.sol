ce4527a8557175f1eac9badee591a743aca29e58
Harry Kalodner <harry@offchainlabs.com>
2020-01-22 22:32:05 -0500

    Separate message hashing out from global inbox

---
+++ packages/arb-bridge-eth/contracts/Messages.sol
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2019, Offchain Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+pragma solidity ^0.5.3;
+
+import "./arch/Value.sol";
+
+
+library Messages {
+    uint8 internal constant TRANSACTION_MSG = 0;
+    uint8 internal constant ETH_DEPOSIT = 1;
+    uint8 internal constant ERC20_DEPOSIT = 2;
+    uint8 internal constant ERC721_DEPOSIT = 3;
+
+    using Value for Value.Data;
+
+    function transactionMessageHash(
+        address chain,
+        address to,
+        address from,
+        uint256 seqNumber,
+        uint256 value,
+        bytes memory data,
+        uint256 blockNumber
+    )
+        internal
+        pure
+        returns(bool, bytes32)
+    {
+        (bool valid,, bytes32 dataHash) = Value.deserializeHashed(data, 0);
+        if (!valid) {
+            return (false, 0);
+        }
+        bytes32 txHash = keccak256(
+            abi.encodePacked(
+                TRANSACTION_MSG,
+                chain,
+                to,
+                from,
+                seqNumber,
+                value,
+                dataHash
+            )
+        );
+
+        Value.Data[] memory msgValues = new Value.Data[](4);
+        msgValues[0] = Value.newInt(uint256(to));
+        msgValues[1] = Value.newInt(seqNumber);
+        msgValues[2] = Value.newInt(value);
+        msgValues[3] = Value.newHashOnly(dataHash);
+
+        Value.Data[] memory msgType = new Value.Data[](3);
+        msgType[0] = Value.newInt(TRANSACTION_MSG);
+        msgType[1] = Value.newInt(uint256(from));
+        msgType[2] = Value.newTuple(msgValues);
+
+        return (true, Value.hashTuple([
+            Value.newInt(blockNumber),
+            Value.newInt(uint256(txHash)),
+            Value.newTuple(msgType)
+        ]));
+    }
+
+    function ethMessageHash(
+        address chain,
+        address from,
+        address to,
+        uint256 value,
+        uint256 blockNumber
+    )
+        internal
+        pure
+        returns(bytes32)
+    {
+        bytes32 txHash = keccak256(
+            abi.encodePacked(
+                ETH_DEPOSIT,
+                chain,
+                to,
+                from,
+                value
+            )
+        );
+
+        Value.Data[] memory msgValues = new Value.Data[](3);
+        msgValues[0] = Value.newInt(uint256(to));
+        msgValues[1] = Value.newInt(value);
+
+        Value.Data[] memory msgType = new Value.Data[](3);
+        msgType[0] = Value.newInt(ETH_DEPOSIT);
+        msgType[1] = Value.newInt(uint256(from));
+        msgType[2] = Value.newTuple(msgValues);
+
+        Value.Data[] memory ethMsg = new Value.Data[](3);
+        ethMsg[0] = Value.newInt(blockNumber);
+        ethMsg[1] = Value.newInt(uint256(txHash));
+        ethMsg[2] = Value.newTuple(msgType);
+
+        return Value.newTuple(ethMsg).hash().hash;
+    }
+
+    function erc20MessageHash(
+        address chain,
+        address from,
+        address to,
+        address erc20,
+        uint256 value,
+        uint256 blockNumber
+    )
+        internal
+        pure
+        returns(bytes32)
+    {
+        return tokenMessageHash(
+            ERC20_DEPOSIT,
+            chain,
+            from,
+            to,
+            erc20,
+            value,
+            blockNumber
+        );
+    }
+
+    function erc721MessageHash(
+        address chain,
+        address from,
+        address to,
+        address erc721,
+        uint256 id,
+        uint256 blockNumber
+    )
+        internal
+        pure
+        returns(bytes32)
+    {
+        return tokenMessageHash(
+            ERC20_DEPOSIT,
+            chain,
+            from,
+            to,
+            erc721,
+            id,
+            blockNumber
+        );
+    }
+
+    function tokenMessageHash(
+        uint8 messageType,
+        address chain,
+        address from,
+        address to,
+        address tokenContract,
+        uint256 value,
+        uint256 blockNumber
+    )
+        private
+        pure
+        returns(bytes32)
+    {
+        bytes32 txHash = keccak256(
+            abi.encodePacked(
+                messageType,
+                chain,
+                from,
+                to,
+                tokenContract,
+                value
+            )
+        );
+
+        Value.Data[] memory msgValues = new Value.Data[](3);
+        msgValues[0] = Value.newInt(uint256(tokenContract));
+        msgValues[1] = Value.newInt(uint256(to));
+        msgValues[2] = Value.newInt(value);
+
+        Value.Data[] memory msgType = new Value.Data[](3);
+        msgType[0] = Value.newInt(messageType);
+        msgType[1] = Value.newInt(uint256(from));
+        msgType[2] = Value.newTuple(msgValues);
+
+        Value.Data[] memory ercTokenMsg = new Value.Data[](3);
+        ercTokenMsg[0] = Value.newInt(blockNumber);
+        ercTokenMsg[1] = Value.newInt(uint(txHash));
+        ercTokenMsg[2] = Value.newTuple(msgType);
+
+        return  Value.newTuple(ercTokenMsg).hash().hash;
+    }
+}
