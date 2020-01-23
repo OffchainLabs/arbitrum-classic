425b81bf432251152a7e86b462db44c2903be40c
Ed Felten <ed@felten.com>
2020-01-23 07:56:17 -0500

    Bugfix

--- packages/arb-validator/rollup/stakerSet.go
+++ packages/arb-validator/rollup/stakerSet.go
@@ -111,7 +111,7 @@ func (ss *StakerSet) DebugString(prefix string) string {
 func (s *Staker) DebugString(prefix string) string {
 	ret := prefix + "addr:" + s.address.ShortString() + " loc:" + s.location.hash.ShortString()
 	if !s.challenge.IsZero() {
-		ret = ret + "chal:" + s.challenge.ShortString()
+		ret = ret + " chal:" + s.challenge.ShortString()
 	}
 	return ret + "\n"
 }
