ea8f87f193ca108bc527e583357a4e332bd324f6
Harry Kalodner <harry@offchainlabs.com>
2020-01-28 13:36:34 -0500

    Eliminate old unused code

--- packages/arb-compiler-evm/tests/sol-syscall/contracts/ArbSys.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity >=0.4.21 <0.6.0;
-
-interface ArbSys {
-	function sendERC20(address dest, address tokenAddress, uint256 amount) external;
-	function sendERC721(address dest, address tokenAddress, uint256 id) external;
-}
