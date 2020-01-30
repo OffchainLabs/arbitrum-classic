894dcc75169a5a5239e37783959f0a7971757d1b
Harry Kalodner <harry@offchainlabs.com>
2020-01-30 15:26:49 -0500

    Add truffle config

--- packages/arb-bridge-eth/truffle-config.js
+++ packages/arb-bridge-eth/truffle-config.js
@@ -10,6 +10,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0xf4a7f2c6bbe40a67e74f1b44bed16c6302eb07f6", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4
     }
   },
   plugins: ["truffle-security"],
