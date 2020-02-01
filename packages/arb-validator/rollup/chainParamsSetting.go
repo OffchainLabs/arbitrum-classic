b1a73043035755b8483af92433514ad5510e8bbb
Ed Felten <ed@felten.com>
2020-02-01 13:54:26 -0500

    Revert "Bugfix"
    This reverts commit e2e10eefcaf94e252e261ccec78692f396e83e5b.

--- packages/arb-validator/rollup/chainParamsSetting.go
+++ packages/arb-validator/rollup/chainParamsSetting.go
@@ -27,7 +27,7 @@ func DefaultChainParams() structures.ChainParams {
 	return structures.ChainParams{
 		StakeRequirement:        big.NewInt(10),
 		GracePeriod:             common.TimeFromBlockNum(common.NewTimeBlocks(big.NewInt(gracePeriodInBlocks))),
-		MaxExecutionSteps:       10000000000000,
-		ArbGasSpeedLimitPerTick: 20000000,
+		MaxExecutionSteps:       10000000,
+		ArbGasSpeedLimitPerTick: 100000,
 	}
 }
