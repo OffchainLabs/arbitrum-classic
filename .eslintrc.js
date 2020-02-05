5c48e687ebd1771b799958aa756a2a779c17f182
Julien Tregoat <julien@offchainlabs.com>
2020-02-05 15:35:18 -0500

    move format + link pkgs up the tree, remove unfound pkg from eslint, update locks

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
       extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint",
         "plugin:prettier/recommended"
       ],
       plugins: ["@typescript-eslint", "prettier"],
