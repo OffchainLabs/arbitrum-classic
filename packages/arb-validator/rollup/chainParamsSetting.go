ad7d1802900e038946e6a6871d8a9cc13d5c83bb
Ed Felten <ed@felten.com>
2020-02-07 08:21:25 -0500

    Change default stake requirement to 0.01 Eth

--- packages/arb-validator/rollup/chainParamsSetting.go
+++ packages/arb-validator/rollup/chainParamsSetting.go
@@ -26,10 +26,7 @@ import (
 func DefaultChainParams() valprotocol.ChainParams {
 	gracePeriodInBlocks := int64(30)
 	return valprotocol.ChainParams{
-		StakeRequirement: new(big.Int).Mul(
-			big.NewInt(1),
-			big.NewInt(1000*1000*1000*1000*1000*1000),
-		),
+		StakeRequirement:        big.NewInt(10 * 1000 * 1000 * 1000 * 1000 * 1000), // 0.01 Eth
 		GracePeriod:             common.TicksFromBlockNum(common.NewTimeBlocks(big.NewInt(gracePeriodInBlocks))),
 		MaxExecutionSteps:       1000000000,
 		MaxTimeBoundsWidth:      20,
