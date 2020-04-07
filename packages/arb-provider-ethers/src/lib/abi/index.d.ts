fcd04bebafda6375e66969a269af3eb86b17af6b
Minh Truong <minhptruong1@gmail.com>
2020-04-06 23:22:41 -0400

    regenerated ao files

--- packages/arb-provider-ethers/src/lib/abi/index.d.ts
+++ packages/arb-provider-ethers/src/lib/abi/index.d.ts
@@ -2,27 +2,27 @@
 /* tslint:disable */
 
 import {
-    BigNumberish,
-    EventDescription,
-    FunctionDescription
-} from "ethers/utils";
+  BigNumberish,
+  EventDescription,
+  FunctionDescription,
+} from 'ethers/utils'
 
 export class TransactionOverrides {
-    nonce?: BigNumberish | Promise<BigNumberish>;
-    gasLimit?: BigNumberish | Promise<BigNumberish>;
-    gasPrice?: BigNumberish | Promise<BigNumberish>;
-    value?: BigNumberish | Promise<BigNumberish>;
-    chainId?: number | Promise<number>;
+  nonce?: BigNumberish | Promise<BigNumberish>
+  gasLimit?: BigNumberish | Promise<BigNumberish>
+  gasPrice?: BigNumberish | Promise<BigNumberish>
+  value?: BigNumberish | Promise<BigNumberish>
+  chainId?: number | Promise<number>
 }
 
 export interface TypedEventDescription<
-    T extends Pick<EventDescription, "encodeTopics">
+  T extends Pick<EventDescription, 'encodeTopics'>
 > extends EventDescription {
-    encodeTopics: T["encodeTopics"];
+  encodeTopics: T['encodeTopics']
 }
 
 export interface TypedFunctionDescription<
-    T extends Pick<FunctionDescription, "encode">
+  T extends Pick<FunctionDescription, 'encode'>
 > extends FunctionDescription {
-    encode: T["encode"];
+  encode: T['encode']
 }
