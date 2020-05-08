639480fea9a52880b1cddcf98b07dc1cb610a6c6
rgates94 <rgates275@gmail.com>
2020-05-08 13:09:08 -0400

    Remove unnecessary else branch in parseInitialVmValues

--- packages/arb-avm-cpp/avm_values/src/vmValueParser.cpp
+++ packages/arb-avm-cpp/avm_values/src/vmValueParser.cpp
@@ -63,34 +63,33 @@ InitialVmValues parseInitialVmValues(const std::string& contract_filename,
 
         initial_state.valid_state = false;
         return initial_state;
-    } else {
-        uint32_t extentionId = 1;
-        while (extentionId != 0) {
-            memcpy(&extentionId, bufptr, sizeof(extentionId));
-            extentionId = boost::endian::big_to_native(extentionId);
-            bufptr += sizeof(extentionId);
-            if (extentionId > 0) {
-                uint32_t extensionLength;
-                memcpy(&extensionLength, bufptr, sizeof(extensionLength));
-                extensionLength = boost::endian::big_to_native(extensionLength);
-                bufptr += sizeof(extensionLength) + extensionLength;
-            }
-        }
-        uint64_t codeCount;
-        memcpy(&codeCount, bufptr, sizeof(codeCount));
-        bufptr += sizeof(codeCount);
-        codeCount = boost::endian::big_to_native(codeCount);
-
-        initial_state.code.reserve(codeCount);
-
-        std::vector<Operation> ops;
-        for (uint64_t i = 0; i < codeCount; i++) {
-            ops.emplace_back(deserializeOperation(bufptr, pool));
-        }
-        initial_state.code = opsToCodePoints(ops);
-        initial_state.staticVal = deserialize_value(bufptr, pool);
-        initial_state.valid_state = true;
-
-        return initial_state;
     }
+    uint32_t extentionId = 1;
+    while (extentionId != 0) {
+        memcpy(&extentionId, bufptr, sizeof(extentionId));
+        extentionId = boost::endian::big_to_native(extentionId);
+        bufptr += sizeof(extentionId);
+        if (extentionId > 0) {
+            uint32_t extensionLength;
+            memcpy(&extensionLength, bufptr, sizeof(extensionLength));
+            extensionLength = boost::endian::big_to_native(extensionLength);
+            bufptr += sizeof(extensionLength) + extensionLength;
+        }
+    }
+    uint64_t codeCount;
+    memcpy(&codeCount, bufptr, sizeof(codeCount));
+    bufptr += sizeof(codeCount);
+    codeCount = boost::endian::big_to_native(codeCount);
+
+    initial_state.code.reserve(codeCount);
+
+    std::vector<Operation> ops;
+    for (uint64_t i = 0; i < codeCount; i++) {
+        ops.emplace_back(deserializeOperation(bufptr, pool));
+    }
+    initial_state.code = opsToCodePoints(ops);
+    initial_state.staticVal = deserialize_value(bufptr, pool);
+    initial_state.valid_state = true;
+
+    return initial_state;
 }
