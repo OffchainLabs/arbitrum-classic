8c8f86f08c57287e0b8d8309243cb5e1521c0d82
Minh Truong <minhptruong1@gmail.com>
2020-05-14 14:33:18 -0600

    tests

--- packages/arb-bridge-eth/test/globalInbox.js
+++ packages/arb-bridge-eth/test/globalInbox.js
@@ -26,9 +26,8 @@ const {
   time,
 } = require('@openzeppelin/test-helpers')
 
-async function getMessageData(sender, receiver, value_tester) {
+async function getMessageData(sender, receiver, value, value_tester) {
   let msgType = 1
-  let value = 50
 
   const msg = new ArbValue.TupleValue([
     new ArbValue.IntValue(1),
@@ -58,95 +57,97 @@ async function getMessageData(sender, receiver, value_tester) {
 }
 
 contract('GlobalInbox', accounts => {
-  // it("should make initial call", async () => {
-  //   let global_inbox = await GlobalInbox.deployed();
-  //   await global_inbox.sendTransactionMessage(
-  //     "0xffffffffffffffffffffffffffffffffffffffff",
-  //     "0xffffffffffffffffffffffffffffffffffffffff",
-  //     2000,
-  //     54254535454544,
-  //     "0x"
-  //   );
-  // });
+  it('should make initial call', async () => {
+    let global_inbox = await GlobalInbox.deployed()
+    await global_inbox.sendTransactionMessage(
+      '0xffffffffffffffffffffffffffffffffffffffff',
+      '0xffffffffffffffffffffffffffffffffffffffff',
+      2000,
+      54254535454544,
+      '0x'
+    )
+  })
 
-  // it("should make second call", async () => {
-  //   let global_inbox = await GlobalInbox.deployed();
-  //   await global_inbox.sendTransactionMessage(
-  //     "0xffffffffffffffffffffffffffffffffffffffff",
-  //     "0xffffffffffffffffffffffffffffffffffffffff",
-  //     2000,
-  //     54254535454544,
-  //     "0x"
-  //   );
-  // });
+  it('should make second call', async () => {
+    let global_inbox = await GlobalInbox.deployed()
+    await global_inbox.sendTransactionMessage(
+      '0xffffffffffffffffffffffffffffffffffffffff',
+      '0xffffffffffffffffffffffffffffffffffffffff',
+      2000,
+      54254535454544,
+      '0x'
+    )
+  })
 
-  // it("should make bigger call", async () => {
-  //   let global_inbox = await GlobalInbox.deployed();
-  //   await global_inbox.sendTransactionMessage(
-  //     "0xffffffffffffffffffffffffffffffffffffffff",
-  //     "0xffffffffffffffffffffffffffffffffffffffff",
-  //     2000,
-  //     54254535454544,
-  //     // 64 bytes of data
-  //     "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-  //   );
-  // });
+  it('should make bigger call', async () => {
+    let global_inbox = await GlobalInbox.deployed()
+    await global_inbox.sendTransactionMessage(
+      '0xffffffffffffffffffffffffffffffffffffffff',
+      '0xffffffffffffffffffffffffffffffffffffffff',
+      2000,
+      54254535454544,
+      // 64 bytes of data
+      '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
+    )
+  })
 
-  // it("should make a batch call", async () => {
-  //   let chain = "0xffffffffffffffffffffffffffffffffffffffff";
-  //   let txDataTemplate = {
-  //     to: "0xffffffffffffffffffffffffffffffffffffffff",
-  //     sequenceNum: 2000,
-  //     value: 54254535454544,
-  //     messageData: "0x"
-  //   };
+  it('should make a batch call', async () => {
+    let chain = '0xffffffffffffffffffffffffffffffffffffffff'
+    let txDataTemplate = {
+      to: '0xffffffffffffffffffffffffffffffffffffffff',
+      sequenceNum: 2000,
+      value: 54254535454544,
+      messageData: '0x',
+    }
 
-  //   let transactionsData = [];
-  //   for (let i = 0; i < 100; i++) {
-  //     transactionsData.push(txDataTemplate);
-  //   }
+    let transactionsData = []
+    for (let i = 0; i < 100; i++) {
+      transactionsData.push(txDataTemplate)
+    }
 
-  //   let tos = [];
-  //   let sequenceNums = [];
-  //   let values = [];
-  //   let messageData = "0x";
-  //   let messageLengths = [];
-  //   let signatures = "0x";
+    let tos = []
+    let sequenceNums = []
+    let values = []
+    let messageData = '0x'
+    let messageLengths = []
+    let signatures = '0x'
 
-  //   for (var i = 0; i < transactionsData.length; i++) {
-  //     let txData = transactionsData[i];
+    for (var i = 0; i < transactionsData.length; i++) {
+      let txData = transactionsData[i]
 
-  //     let txHash = web3.utils.soliditySha3(
-  //       { t: "address", v: chain },
-  //       { t: "address", v: txData["to"] },
-  //       { t: "uint256", v: txData["sequenceNum"] },
-  //       { t: "uint256", v: txData["value"] },
-  //       { t: "bytes", v: txData["messageData"] }
-  //     );
-  //     let signedTxHash = await web3.eth.sign(txHash, accounts[0]);
-  //     tos.push(txData["to"]);
-  //     sequenceNums.push(txData["sequenceNum"]);
-  //     values.push(txData["value"]);
-  //     messageLengths.push((txData["messageData"].length - 2) / 2);
-  //     messageData += txData["messageData"].slice(2);
-  //     signatures += signedTxHash.slice(2);
-  //   }
+      let txHash = web3.utils.soliditySha3(
+        { t: 'address', v: chain },
+        { t: 'address', v: txData['to'] },
+        { t: 'uint256', v: txData['sequenceNum'] },
+        { t: 'uint256', v: txData['value'] },
+        { t: 'bytes', v: txData['messageData'] }
+      )
+      let signedTxHash = await web3.eth.sign(txHash, accounts[0])
+      tos.push(txData['to'])
+      sequenceNums.push(txData['sequenceNum'])
+      values.push(txData['value'])
+      messageLengths.push((txData['messageData'].length - 2) / 2)
+      messageData += txData['messageData'].slice(2)
+      signatures += signedTxHash.slice(2)
+    }
 
-  //   let global_inbox = await GlobalInbox.deployed();
-  //   await global_inbox.deliverTransactionBatch(
-  //     chain,
-  //     tos,
-  //     sequenceNums,
-  //     values,
-  //     messageLengths,
-  //     messageData,
-  //     signatures
-  //   );
-  // });
+    let global_inbox = await GlobalInbox.deployed()
+    await global_inbox.deliverTransactionBatch(
+      chain,
+      tos,
+      sequenceNums,
+      values,
+      messageLengths,
+      messageData,
+      signatures
+    )
+  })
 
   let chain_address = accounts[6]
   let nodeHash =
     '0x10c9d77c3846591fdfc3f966935819eb7dd71ebb7e71d5d081b880868ca33e4d'
+  let nodeHash2 =
+    '0x20c9d77c3846591fdfc3f966935819eb7dd71ebb7e71d5d081b880868ca33e4d'
   let messageIndex = 0
   let originalOwner = accounts[0]
   let address2 = accounts[1]
@@ -154,7 +155,6 @@ contract('GlobalInbox', accounts => {
   let curr_owner = accounts[0]
 
   it('tradeable-exits: initial', async () => {
-    console.log('inside tradeable-exits initial...')
     let global_inbox = await GlobalInbox.deployed()
 
     curr_owner = await global_inbox.getPaymentOwner(
@@ -194,7 +194,6 @@ contract('GlobalInbox', accounts => {
   })
 
   it('tradeable-exits: subsequent transfers', async () => {
-    console.log('inside tradeable-exits: subsequent transfers...')
     let global_inbox = await GlobalInbox.deployed()
     curr_owner = await global_inbox.getPaymentOwner(
       originalOwner,
@@ -248,39 +247,32 @@ contract('GlobalInbox', accounts => {
       }
     )
     await expectRevert(recieptr, 'Must be payment owner.')
+  })
 
+  it('tradeable-exits: commiting transfers', async () => {
+    let global_inbox = await GlobalInbox.deployed()
     let value_tester = await ValueTester.new()
-
-    console.log('inside tradeable-exits: commiting transfers...')
-    // let global_inbox = await GlobalInbox.deployed();
-
-    let chain_balance = await global_inbox.getEthBalance(chain_address)
-    console.log('chain_address_balance: ' + chain_balance)
-    let curr_owner_balance = await global_inbox.getEthBalance(curr_owner)
-    console.log('curr_owner_balance: ' + curr_owner_balance)
-    let originalOwner_balance = await global_inbox.getEthBalance(originalOwner)
-    console.log('originalOwner_balance: ' + originalOwner_balance)
-
     let reciept3 = await global_inbox.depositEthMessage(
       chain_address,
       originalOwner,
       {
         from: originalOwner,
-        value: 50,
+        value: 100,
       }
     )
     await expectEvent(reciept3, 'EthDepositMessageDelivered')
 
     chain_balance = await global_inbox.getEthBalance(chain_address)
-    console.log('chain_address_balance: ' + chain_balance)
+    assert.equal(100, chain_balance.toNumber())
     curr_owner_balance = await global_inbox.getEthBalance(curr_owner)
-    console.log('curr_owner_balance: ' + curr_owner_balance)
+    assert.equal(0, curr_owner_balance.toNumber())
     originalOwner_balance = await global_inbox.getEthBalance(originalOwner)
-    console.log('originalOwner_balance: ' + originalOwner_balance)
+    assert.equal(0, originalOwner_balance.toNumber())
 
     let msg_data = await getMessageData(
       chain_address,
       originalOwner,
+      50,
       value_tester
     )
     let msgCounts = [1]
@@ -297,38 +289,43 @@ contract('GlobalInbox', accounts => {
     )
 
     chain_balance = await global_inbox.getEthBalance(chain_address)
-    console.log('chain_address_balance: ' + chain_balance)
+    assert.equal(50, chain_balance.toNumber())
     curr_owner_balance = await global_inbox.getEthBalance(curr_owner)
-    console.log('curr_owner_balance: ' + curr_owner_balance)
+    assert.equal(50, curr_owner_balance.toNumber())
     originalOwner_balance = await global_inbox.getEthBalance(originalOwner)
-    console.log('originalOwner_balance: ' + originalOwner_balance)
-    // it("tradeable-exits: commiting transfers", async () => {
-    //   let value_tester = await ValueTester.new();
+    assert.equal(0, originalOwner_balance.toNumber())
 
-    //   console.log("inside tradeable-exits: commiting transfers...");
-    //   let global_inbox = await GlobalInbox.deployed();
+    let msg_data2 = await getMessageData(
+      chain_address,
+      originalOwner,
+      50,
+      value_tester
+    )
 
-    //   let reciept3 = await global_inbox.depositEthMessage(
-    //     chain_address,
-    //     originalOwner,
-    //     {
-    //       from: originalOwner,
-    //       value: 50
-    //     });
-    //   await expectEvent(reciept3, "EthDepositMessageDelivered");
+    let msgCounts2 = [1]
+    let bytes2 = web3.utils.hexToBytes(nodeHash2)
+    let nodeHashes2 = [bytes2]
 
-    //   let msg_data = await getMessageData(chain_address, originalOwner, value_tester);
-    //   let msgCounts = [1];
-    //   let bytes = web3.utils.hexToBytes(nodeHash);
-    //   let nodeHashes = [bytes];
+    let reciept5 = await global_inbox.sendMessages(
+      msg_data2,
+      msgCounts2,
+      nodeHashes2,
+      {
+        from: chain_address,
+      }
+    )
 
-    //   let reciept4 = await global_inbox.sendMessages(msg_data, msgCounts, nodeHashes);
-    //   // await time.advanceBlock();
-    //   // await time.advanceBlock();
-
-    //   // let balance = await global_inbox.getEthBalance(address3);
-    //   // console.log("balance: "+ balance);
-    //   // let balance2 = await global_inbox.getEthBalance(chain_address);
-    //   // console.log("balance2: "+ balance2);
+    chain_balance = await global_inbox.getEthBalance(chain_address)
+    assert.equal(0, chain_balance.toNumber())
+    curr_owner_balance = await global_inbox.getEthBalance(curr_owner)
+    assert.equal(50, curr_owner_balance.toNumber())
+    originalOwner_balance = await global_inbox.getEthBalance(originalOwner)
+    assert.equal(50, originalOwner_balance.toNumber())
   })
+
+  // it("tradeable-exits: commiting transfers, two msgs", async () => {
+  //   let global_inbox = await GlobalInbox.deployed();
+  //   let value_tester = await ValueTester.new();
+
+  // });
 })
