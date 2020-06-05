974b5eefb1e2c5e51ad83f7ec3d27a1fb489b065
Harry Kalodner <harry@offchainlabs.com>
2020-06-05 11:41:48 -0400

    Add more tests for message package

--- packages/arb-validator-core/message/data_test.go
+++ packages/arb-validator-core/message/data_test.go
@@ -2,6 +2,7 @@ package message
 
 import (
 	"bytes"
+	"github.com/offchainlabs/arbitrum/packages/arb-util/value"
 	"math/rand"
 	"testing"
 
@@ -20,5 +21,45 @@ func TestByteStackConversion(t *testing.T) {
 		t.Log("data:", hexutil.Encode(data))
 		t.Log("data2:", hexutil.Encode(data2))
 		t.Error("data changed in conversion")
+	}
+}
+
+func TestStackValueToListFailures(t *testing.T) {
+	intVal := value.NewInt64Value(0)
+	if _, err := StackValueToList(intVal); err == nil {
+		t.Error("should fail when passed non-tuple")
+	}
+
+	tup, _ := value.NewTupleFromSlice([]value.Value{intVal, intVal, intVal})
+	if _, err := StackValueToList(tup); err == nil {
+		t.Error("should fail when passed tuple not of size 2")
+	}
+}
+
+func TestByteStackToHexFailures(t *testing.T) {
+	intVal := value.NewInt64Value(0)
+	if _, err := ByteStackToHex(intVal); err == nil {
+		t.Error("should fail when passed non-tuple")
+	}
+
+	tup, _ := value.NewTupleFromSlice([]value.Value{intVal, intVal, intVal})
+	if _, err := ByteStackToHex(tup); err == nil {
+		t.Error("should fail when passed tuple not of size 2")
+	}
+
+	tup, _ = value.NewTupleFromSlice([]value.Value{value.NewEmptyTuple(), intVal})
+	if _, err := ByteStackToHex(tup); err == nil {
+		t.Error("should fail when first value is not an int")
+	}
+
+	tup, _ = value.NewTupleFromSlice([]value.Value{intVal, intVal})
+	if _, err := ByteStackToHex(tup); err == nil {
+		t.Error("should fail when second value is not a stack value")
+	}
+
+	listVal := ListToStackValue([]value.Value{value.NewEmptyTuple()})
+	tup, _ = value.NewTupleFromSlice([]value.Value{intVal, listVal})
+	if _, err := ByteStackToHex(tup); err == nil {
+		t.Error("should fail when second value contains non ints in the stack")
 	}
 }
