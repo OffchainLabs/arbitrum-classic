ea3e7110cc5e9b2737991de8cc1146da2470e270
Harry Kalodner <harry@offchainlabs.com>
2020-06-17 12:47:33 -0400

    Update avm-replay.go

--- packages/arb-validator/cmd/avm-replay/avm-replay.go
+++ packages/arb-validator/cmd/avm-replay/avm-replay.go
@@ -18,14 +18,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/offchainlabs/arbitrum/packages/arb-util/common"
+	"github.com/offchainlabs/arbitrum/packages/arb-util/machine"
+	"github.com/offchainlabs/arbitrum/packages/arb-util/protocol"
 	"github.com/offchainlabs/arbitrum/packages/arb-util/value"
 	"github.com/offchainlabs/arbitrum/packages/arb-validator-core/ethbridge"
+	"github.com/offchainlabs/arbitrum/packages/arb-validator-core/evm"
 	"github.com/offchainlabs/arbitrum/packages/arb-validator-core/message"
+	"github.com/offchainlabs/arbitrum/packages/arb-validator/loader"
 	"github.com/offchainlabs/arbitrum/packages/arb-validator/structures"
 	"log"
+	"math"
 	"math/big"
 	"os"
 )
@@ -34,7 +40,7 @@ func main() {
 	filename := "messages.dat"
 	//if err := getMessages(
 	//	"https://ropsten.infura.io/v3/5851cb75448e4f8da37c5015006589a6",
-	//	common.HexToAddress("0x87a7bAd640eaa9089be09d827952bf9E45Ce8780"),
+	//	common.HexToAddress("0xEb57E0FB729E892E84259B4e6Dc08442Aa6E9Ee4"),
 	//	filename,
 	//); err != nil {
 	//	panic(err)
@@ -45,19 +51,48 @@ func main() {
 	}
 }
 
+func toEth(val *big.Int) *big.Float {
+	fbalance := new(big.Float)
+	fbalance.SetString(val.String())
+	return new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+}
+
+func runMessage(mach machine.Machine, msg message.Delivered) (evm.Result, error) {
+	tb := protocol.NewRandomTimeBounds()
+	vmInbox := structures.NewVMInbox()
+	vmInbox.DeliverMessage(msg)
+	assertion, _ := mach.ExecuteAssertion(
+		100000,
+		tb,
+		vmInbox.AsValue(),
+		1000,
+	)
+	//log.Println("ran assertion")
+	logs := assertion.Logs
+	if len(logs) != 1 {
+		log.Fatal("returned incorrect log count")
+	}
+	evmResult, err := evm.ProcessLog(logs[0])
+	if err != nil {
+		return nil, err
+	}
+	return evmResult, nil
+}
+
 func testMessages(filename string, contract string) error {
 	messages, err := loadMessages(filename)
 	if err != nil {
 		return err
 	}
-	//
-	//mach, err := loader.LoadMachineFromFile(contract, false, "cpp")
-	//if err != nil {
-	//	return err
-	//}
-	//
+
+	mach, err := loader.LoadMachineFromFile(contract, false, "cpp")
+	if err != nil {
+		return err
+	}
+
 	//tb := protocol.NewRandomTimeBounds()
 
+	addressesMap := make(map[common.Address]bool)
 	singleMessages := make([]message.Delivered, 0)
 	for _, msg := range messages {
 		for _, del := range msg.VMInboxMessages() {
@@ -65,53 +100,150 @@ func testMessages(filename string, contract string) error {
 				Message:      del.Message,
 				DeliveryInfo: del.DeliveryInfo,
 			})
+			addressesMap[del.Message.DestAddress()] = true
+			addressesMap[del.Message.SenderAddress()] = true
 		}
-
+	}
+	addresses := make([]common.Address, 0, len(addressesMap))
+	for address := range addressesMap {
+		addresses = append(addresses, address)
 	}
 
-	//runMsg := func(msg message.Delivered) error {
-	//	vmInbox := structures.NewVMInbox()
-	//	vmInbox.DeliverMessage(msg)
-	//
-	//	_, _ = mach.ExecuteAssertion(
-	//		10000000,
-	//		tb,
-	//		vmInbox.AsValue(),
-	//		1000,
-	//	)
-	//	blocked := mach.IsBlocked(common.NewTimeBlocksInt(0), true)
-	//	if blocked != nil {
-	//		return fmt.Errorf("machine is blocked: %v", blocked)
-	//	}
-	//	return nil
+	totalSupplyData, _ := hexutil.Decode("0x18160ddd")
+	totalSupplyCall := message.Call{
+		To:   common.HexToAddress("0x3c1be20be169df0d99cca3730aae70580c3edf9a"),
+		From: common.Address{},
+		Data: totalSupplyData,
+	}
+
+	prevEthBalances := make(map[common.Address]*big.Int)
+	prevTokenBalances := make(map[common.Address]*big.Int)
+	for _, address := range addresses {
+		prevEthBalances[address] = big.NewInt(0)
+		prevTokenBalances[address] = big.NewInt(0)
+	}
+	prevTotalSupply := big.NewInt(0)
+
+	runMsg := func(msg message.Delivered) error {
+		log.Println()
+		log.Println(msg.Message)
+
+		txReturn, err := runMessage(mach, msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("tx result", txReturn)
+
+		tokenSupplyResult, err := runMessage(mach.Clone(), message.Delivered{
+			Message:      totalSupplyCall,
+			DeliveryInfo: msg.DeliveryInfo,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		totalBalance := new(big.Int).SetBytes(tokenSupplyResult.GetReturnData())
+		log.Println("total supply", toEth(totalBalance))
+		ethBalances := make(map[common.Address]*big.Int)
+		tokenBalances := make(map[common.Address]*big.Int)
+		for _, address := range addresses {
+			getTokenBalanceData, _ := hexutil.Decode("0x70a08231000000000000000000000000" + address.String()[2:])
+			getTokenBalanceCall := message.Call{
+				To:   common.HexToAddress("0x716f0d674efeeca329f141d0ca0d97a98057bdbf"),
+				From: common.Address{},
+				Data: getTokenBalanceData,
+			}
+			tokenBalanceResult, err := runMessage(mach.Clone(), message.Delivered{
+				Message:      getTokenBalanceCall,
+				DeliveryInfo: msg.DeliveryInfo,
+			})
+			getEthBalanceData, _ := hexutil.Decode("0xf8b2cb4f000000000000000000000000" + address.String()[2:])
+			call := message.Call{
+				To:   common.HexToAddress("0x0000000000000000000000000000000000000065"),
+				From: common.Address{},
+				Data: getEthBalanceData,
+			}
+			ethBalanceResult, err := runMessage(mach.Clone(), message.Delivered{
+				Message:      call,
+				DeliveryInfo: msg.DeliveryInfo,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			ethBalances[address] = new(big.Int).SetBytes(ethBalanceResult.GetReturnData())
+			tokenBalances[address] = new(big.Int).SetBytes(tokenBalanceResult.GetReturnData())
+
+			log.Println("balance", address, toEth(ethBalances[address]), toEth(tokenBalances[address]))
+		}
+
+		blocked := mach.IsBlocked(common.NewTimeBlocksInt(0), true)
+		if blocked != nil {
+			return fmt.Errorf("machine is blocked: %v", blocked)
+		}
+		if txReturn.Type() == evm.RevertCode {
+			for _, address := range addresses {
+				if ethBalances[address].Cmp(prevEthBalances[address]) != 0 {
+					log.Fatal("eth balance changed after revert")
+				}
+				if tokenBalances[address].Cmp(prevTokenBalances[address]) != 0 {
+					log.Fatal("token balance changed after revert")
+				}
+			}
+			if prevTotalSupply.Cmp(totalBalance) != 0 {
+				log.Fatal("total supply changed after revert")
+			}
+		}
+		prevEthBalances = ethBalances
+		prevTokenBalances = tokenBalances
+		prevTotalSupply = totalBalance
+		return nil
+	}
+
+	for i, msg := range singleMessages {
+		//if i == 35 {
+		//	break
+		//}
+		log.Println(i)
+		if err := runMsg(msg); err != nil {
+			log.Println(err)
+			return nil
+		}
+	}
+
+	//lastMessage := singleMessages[34]
+	//_, err = runMessage(mach, lastMessage)
+	//if err != nil {
+	//	log.Fatal(err)
 	//}
 
-	//for _, msg := range singleMessages[:len(singleMessages)-1] {
-	//	if err := runMsg(msg); err != nil {
-	//		log.Println(err)
-	//		return nil
-	//	}
-	//}
+	//info := lastMessage.DeliveryInfo
+	//msg := lastMessage.Message.(message.Transaction)
+	//msg.SequenceNum = big.NewInt(0)
 
-	lastMessage := singleMessages[len(singleMessages)-1]
-
-	info := lastMessage.DeliveryInfo
-	msg := lastMessage.Message.(message.Transaction)
-	msg.SequenceNum = big.NewInt(0)
-
-	vmInbox := structures.NewVMInbox()
-	vmInbox.DeliverMessage(message.Delivered{
-		Message:      msg,
-		DeliveryInfo: info,
-	})
-	val := vmInbox.AsValue()
-	log.Println(hexutil.Encode(value.MarshalValueToBytes(val)))
+	//vmInbox := structures.NewVMInbox()
+	//vmInbox.DeliverMessage(message.Delivered{
+	//	Message:      msg,
+	//	DeliveryInfo: info,
+	//})
+	//val := vmInbox.AsValue()
+	//log.Println(hexutil.Encode(value.MarshalValueToBytes(val)))
 
 	//log.Println("Delivering crash message", msg)
+	//tokenBalanceFinalMessage := message.Delivered{
+	//	Message:      getTokenBalanceCall,
+	//	DeliveryInfo: lastMessage.DeliveryInfo,
+	//}
+	//vmInbox := structures.NewVMInbox()
+	//vmInbox.DeliverMessage(tokenBalanceFinalMessage)
 	//i := 0
+	//_, _ = mach.ExecuteAssertion(
+	//	1,
+	//	tb,
+	//	vmInbox.AsValue(),
+	//	1000,
+	//)
 	//for {
 	//	mach.PrintState()
-	//	blocked := mach.IsBlocked(common.NewTimeBlocksInt(0), true)
+	//	blocked := mach.IsBlocked(common.NewTimeBlocksInt(0), false)
 	//	if blocked != nil {
 	//		log.Printf("machine after %v steps is blocked: %v\n", i, blocked)
 	//		return nil
@@ -119,7 +251,7 @@ func testMessages(filename string, contract string) error {
 	//	_, _ = mach.ExecuteAssertion(
 	//		1,
 	//		tb,
-	//		vmInbox.AsValue(),
+	//		value.NewEmptyTuple(),
 	//		1000,
 	//	)
 	//	i++
