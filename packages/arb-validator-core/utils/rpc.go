abb46d201ab72ead7166a1575bc8eb834890d0e8
Harry Kalodner <harry@offchainlabs.com>
2020-06-19 18:42:52 -0400

    Print file locations

--- packages/arb-validator-core/utils/rpc.go
+++ packages/arb-validator-core/utils/rpc.go
@@ -54,7 +54,7 @@ func LaunchRPC(handler http.Handler, port string, flags RPCFlags) error {
 	h := handlers.CORS(headersOk, originsOk, methodsOk)(r)
 
 	if flags.certFile != "" && flags.keyFile != "" {
-		log.Println("Launching rpc server over http")
+		log.Println("Launching rpc server over http with cert", flags.certFile, "and key", flags.keyFile)
 		return http.ListenAndServeTLS(
 			":"+port,
 			flags.certFile,
