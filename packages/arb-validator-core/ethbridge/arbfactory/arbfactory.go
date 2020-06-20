333532c28b7067e607985d63aa9f420c108970d1
Harry Kalodner <harry@offchainlabs.com>
2020-06-20 10:50:51 -0400

    Update ABI

--- packages/arb-validator-core/ethbridge/arbfactory/arbfactory.go
+++ packages/arb-validator-core/ethbridge/arbfactory/arbfactory.go
@@ -20,7 +20,6 @@ var (
 	_ = big.NewInt
 	_ = strings.NewReader
 	_ = ethereum.NotFound
-	_ = abi.U256
 	_ = bind.Bind
 	_ = common.Big1
 	_ = types.BloomLookup
@@ -39,7 +38,7 @@ var ArbFactoryFuncSigs = map[string]string{
 }
 
 // ArbFactoryBin is the compiled bytecode used for deploying new contracts.
-var ArbFactoryBin = "0x608060405234801561001057600080fd5b5060405161051e38038061051e8339818101604052606081101561003357600080fd5b5080516020820151604090920151600080546001600160a01b039384166001600160a01b031991821617909155600180549484169482169490941790935560028054929091169190921617905561048f8061008f6000396000f3fe608060405234801561001057600080fd5b506004361061004c5760003560e01c8063582923c71461005157806362e3c0b1146100755780638689d9961461007d578063b10c5f8414610085575b600080fd5b6100596100e9565b604080516001600160a01b039092168252519081900360200190f35b6100596100f8565b610059610107565b6100e7600480360361010081101561009c57600080fd5b5080359060208101356001600160801b03908116916040810135821691606082013567ffffffffffffffff1691608081019160c0820135169060e001356001600160a01b0316610116565b005b6001546001600160a01b031681565b6002546001600160a01b031681565b6000546001600160a01b031681565b6000805461012c906001600160a01b03166102a1565b60025460015460408051638e0f716760e01b8152600481018d81526001600160801b03808e1660248401528c16604483015267ffffffffffffffff8b1660648301529495506001600160a01b0380871695638e0f7167958f958f958f958f958f958f958f95908316949216929091608490910190879080828437600081840152601f19601f820116905080830192505050856001600160801b03166001600160801b03168152602001846001600160a01b03166001600160a01b03168152602001836001600160a01b03166001600160a01b03168152602001826001600160a01b03166001600160a01b031681526020019950505050505050505050600060405180830381600087803b15801561024257600080fd5b505af1158015610256573d6000803e3d6000fd5b5050604080516001600160a01b038516815290517f84c162f1396badc29f9c932c79d7495db699b615e2c0da163ae26bd5dbe71d7c9350908190036020019150a15050505050505050565b60006060604051806020016102b5906103be565b601f1982820381018352601f9091011660408181526001600160a01b038616602083810191909152815180840382018152828401909252835191926060019182918501908083835b6020831061031c5780518252601f1990920191602091820191016102fd565b51815160209384036101000a600019018019909216911617905285519190930192850191508083835b602083106103645780518252601f199092019160209182019101610345565b6001836020036101000a038019825116818451168082178552505050505050905001925050506040516020818303038152906040529050806020018151808234f09350836103b6573d6000803e3d6000fd5b505050919050565b6090806103cb8339019056fe6080604052348015600f57600080fd5b506040516090380380609083398181016040526020811015602f57600080fd5b50516040805169363d3d373d3d3d363d7360b01b6020828101919091526001600160601b0319606085901b16602a8301526e5af43d82803e903d91602b57fd5bf360881b603e8301528251602d81840381018252604d9093019093528201f3fea265627a7a72315820408bdb1f0fee17ae0bb0305c373c15dbf5276ca0dde44a6d11257230dbfb2c8264736f6c634300050f0032"
+var ArbFactoryBin = "0x608060405234801561001057600080fd5b5060405161051e38038061051e8339818101604052606081101561003357600080fd5b5080516020820151604090920151600080546001600160a01b039384166001600160a01b031991821617909155600180549484169482169490941790935560028054929091169190921617905561048f8061008f6000396000f3fe608060405234801561001057600080fd5b506004361061004c5760003560e01c8063582923c71461005157806362e3c0b1146100755780638689d9961461007d578063b10c5f8414610085575b600080fd5b6100596100e9565b604080516001600160a01b039092168252519081900360200190f35b6100596100f8565b610059610107565b6100e7600480360361010081101561009c57600080fd5b5080359060208101356001600160801b03908116916040810135821691606082013567ffffffffffffffff1691608081019160c0820135169060e001356001600160a01b0316610116565b005b6001546001600160a01b031681565b6002546001600160a01b031681565b6000546001600160a01b031681565b6000805461012c906001600160a01b03166102a1565b60025460015460408051638e0f716760e01b8152600481018d81526001600160801b03808e1660248401528c16604483015267ffffffffffffffff8b1660648301529495506001600160a01b0380871695638e0f7167958f958f958f958f958f958f958f95908316949216929091608490910190879080828437600081840152601f19601f820116905080830192505050856001600160801b03166001600160801b03168152602001846001600160a01b03166001600160a01b03168152602001836001600160a01b03166001600160a01b03168152602001826001600160a01b03166001600160a01b031681526020019950505050505050505050600060405180830381600087803b15801561024257600080fd5b505af1158015610256573d6000803e3d6000fd5b5050604080516001600160a01b038516815290517f84c162f1396badc29f9c932c79d7495db699b615e2c0da163ae26bd5dbe71d7c9350908190036020019150a15050505050505050565b60006060604051806020016102b5906103be565b601f1982820381018352601f9091011660408181526001600160a01b038616602083810191909152815180840382018152828401909252835191926060019182918501908083835b6020831061031c5780518252601f1990920191602091820191016102fd565b51815160209384036101000a600019018019909216911617905285519190930192850191508083835b602083106103645780518252601f199092019160209182019101610345565b6001836020036101000a038019825116818451168082178552505050505050905001925050506040516020818303038152906040529050806020018151808234f09350836103b6573d6000803e3d6000fd5b505050919050565b6090806103cb8339019056fe6080604052348015600f57600080fd5b506040516090380380609083398181016040526020811015602f57600080fd5b50516040805169363d3d373d3d3d363d7360b01b6020828101919091526001600160601b0319606085901b16602a8301526e5af43d82803e903d91602b57fd5bf360881b603e8301528251602d81840381018252604d9093019093528201f3fea265627a7a72315820c487d33c2137a02e2226b3f3ba3bd9de8f946b27f98fc3a309111c5b4643856464736f6c63430005110032"
 
 // DeployArbFactory deploys a new Ethereum contract, binding an instance of ArbFactory to it.
 func DeployArbFactory(auth *bind.TransactOpts, backend bind.ContractBackend, _rollupTemplate common.Address, _globalInboxAddress common.Address, _challengeFactoryAddress common.Address) (common.Address, *types.Transaction, *ArbFactory, error) {
@@ -199,7 +198,7 @@ func (_ArbFactory *ArbFactoryTransactorRaw) Transact(opts *bind.TransactOpts, me
 
 // ChallengeFactoryAddress is a free data retrieval call binding the contract method 0x62e3c0b1.
 //
-// Solidity: function challengeFactoryAddress() constant returns(address)
+// Solidity: function challengeFactoryAddress() view returns(address)
 func (_ArbFactory *ArbFactoryCaller) ChallengeFactoryAddress(opts *bind.CallOpts) (common.Address, error) {
 	var (
 		ret0 = new(common.Address)
@@ -211,21 +210,21 @@ func (_ArbFactory *ArbFactoryCaller) ChallengeFactoryAddress(opts *bind.CallOpts
 
 // ChallengeFactoryAddress is a free data retrieval call binding the contract method 0x62e3c0b1.
 //
-// Solidity: function challengeFactoryAddress() constant returns(address)
+// Solidity: function challengeFactoryAddress() view returns(address)
 func (_ArbFactory *ArbFactorySession) ChallengeFactoryAddress() (common.Address, error) {
 	return _ArbFactory.Contract.ChallengeFactoryAddress(&_ArbFactory.CallOpts)
 }
 
 // ChallengeFactoryAddress is a free data retrieval call binding the contract method 0x62e3c0b1.
 //
-// Solidity: function challengeFactoryAddress() constant returns(address)
+// Solidity: function challengeFactoryAddress() view returns(address)
 func (_ArbFactory *ArbFactoryCallerSession) ChallengeFactoryAddress() (common.Address, error) {
 	return _ArbFactory.Contract.ChallengeFactoryAddress(&_ArbFactory.CallOpts)
 }
 
 // GlobalInboxAddress is a free data retrieval call binding the contract method 0x582923c7.
 //
-// Solidity: function globalInboxAddress() constant returns(address)
+// Solidity: function globalInboxAddress() view returns(address)
 func (_ArbFactory *ArbFactoryCaller) GlobalInboxAddress(opts *bind.CallOpts) (common.Address, error) {
 	var (
 		ret0 = new(common.Address)
@@ -237,21 +236,21 @@ func (_ArbFactory *ArbFactoryCaller) GlobalInboxAddress(opts *bind.CallOpts) (co
 
 // GlobalInboxAddress is a free data retrieval call binding the contract method 0x582923c7.
 //
-// Solidity: function globalInboxAddress() constant returns(address)
+// Solidity: function globalInboxAddress() view returns(address)
 func (_ArbFactory *ArbFactorySession) GlobalInboxAddress() (common.Address, error) {
 	return _ArbFactory.Contract.GlobalInboxAddress(&_ArbFactory.CallOpts)
 }
 
 // GlobalInboxAddress is a free data retrieval call binding the contract method 0x582923c7.
 //
-// Solidity: function globalInboxAddress() constant returns(address)
+// Solidity: function globalInboxAddress() view returns(address)
 func (_ArbFactory *ArbFactoryCallerSession) GlobalInboxAddress() (common.Address, error) {
 	return _ArbFactory.Contract.GlobalInboxAddress(&_ArbFactory.CallOpts)
 }
 
 // RollupTemplate is a free data retrieval call binding the contract method 0x8689d996.
 //
-// Solidity: function rollupTemplate() constant returns(address)
+// Solidity: function rollupTemplate() view returns(address)
 func (_ArbFactory *ArbFactoryCaller) RollupTemplate(opts *bind.CallOpts) (common.Address, error) {
 	var (
 		ret0 = new(common.Address)
@@ -263,14 +262,14 @@ func (_ArbFactory *ArbFactoryCaller) RollupTemplate(opts *bind.CallOpts) (common
 
 // RollupTemplate is a free data retrieval call binding the contract method 0x8689d996.
 //
-// Solidity: function rollupTemplate() constant returns(address)
+// Solidity: function rollupTemplate() view returns(address)
 func (_ArbFactory *ArbFactorySession) RollupTemplate() (common.Address, error) {
 	return _ArbFactory.Contract.RollupTemplate(&_ArbFactory.CallOpts)
 }
 
 // RollupTemplate is a free data retrieval call binding the contract method 0x8689d996.
 //
-// Solidity: function rollupTemplate() constant returns(address)
+// Solidity: function rollupTemplate() view returns(address)
 func (_ArbFactory *ArbFactoryCallerSession) RollupTemplate() (common.Address, error) {
 	return _ArbFactory.Contract.RollupTemplate(&_ArbFactory.CallOpts)
 }
@@ -433,7 +432,7 @@ func (_ArbFactory *ArbFactoryFilterer) ParseRollupCreated(log types.Log) (*ArbFa
 const CloneFactoryABI = "[]"
 
 // CloneFactoryBin is the compiled bytecode used for deploying new contracts.
-var CloneFactoryBin = "0x6080604052348015600f57600080fd5b50603e80601d6000396000f3fe6080604052600080fdfea265627a7a723158207d097d5e2f137b07789dd089d368b908eebbecda041c56e17c963c8aff089cd764736f6c634300050f0032"
+var CloneFactoryBin = "0x6080604052348015600f57600080fd5b50603e80601d6000396000f3fe6080604052600080fdfea265627a7a7231582014499adf675d1d9478ed19c566b4f750dc94b33ec36c0cd9ed6abd9987faa64f64736f6c63430005110032"
 
 // DeployCloneFactory deploys a new Ethereum contract, binding an instance of CloneFactory to it.
 func DeployCloneFactory(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *CloneFactory, error) {
@@ -744,21 +743,21 @@ func (_IArbRollup *IArbRollupTransactorRaw) Transact(opts *bind.TransactOpts, me
 
 // ForwardContractMessage is a paid mutator transaction binding the contract method 0xc5818317.
 //
-// Solidity: function forwardContractMessage(address _sender, bytes _data) returns()
+// Solidity: function forwardContractMessage(address _sender, bytes _data) payable returns()
 func (_IArbRollup *IArbRollupTransactor) ForwardContractMessage(opts *bind.TransactOpts, _sender common.Address, _data []byte) (*types.Transaction, error) {
 	return _IArbRollup.contract.Transact(opts, "forwardContractMessage", _sender, _data)
 }
 
 // ForwardContractMessage is a paid mutator transaction binding the contract method 0xc5818317.
 //
-// Solidity: function forwardContractMessage(address _sender, bytes _data) returns()
+// Solidity: function forwardContractMessage(address _sender, bytes _data) payable returns()
 func (_IArbRollup *IArbRollupSession) ForwardContractMessage(_sender common.Address, _data []byte) (*types.Transaction, error) {
 	return _IArbRollup.Contract.ForwardContractMessage(&_IArbRollup.TransactOpts, _sender, _data)
 }
 
 // ForwardContractMessage is a paid mutator transaction binding the contract method 0xc5818317.
 //
-// Solidity: function forwardContractMessage(address _sender, bytes _data) returns()
+// Solidity: function forwardContractMessage(address _sender, bytes _data) payable returns()
 func (_IArbRollup *IArbRollupTransactorSession) ForwardContractMessage(_sender common.Address, _data []byte) (*types.Transaction, error) {
 	return _IArbRollup.Contract.ForwardContractMessage(&_IArbRollup.TransactOpts, _sender, _data)
 }
