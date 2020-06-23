e42fca7613492ec14070db18363a084a082fbcb5
Harry Kalodner <harry@offchainlabs.com>
2020-06-23 15:06:49 -0400

    Add typescript support

--- packages/arb-bridge-eth/buidler.config.ts
+++ packages/arb-bridge-eth/buidler.config.ts
@@ -1,9 +1,11 @@
+import { task, usePlugin } from '@nomiclabs/buidler/config'
+
 usePlugin('@nomiclabs/buidler-waffle')
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async () => {
-  const accounts = await ethers.getSigners()
+task('accounts', 'Prints the list of accounts', async (taskArgs, bre) => {
+  const accounts = await bre.ethers.getSigners()
 
   for (const account of accounts) {
     console.log(await account.getAddress())
