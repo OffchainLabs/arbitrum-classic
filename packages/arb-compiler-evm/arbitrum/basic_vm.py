ea1300d7abc0416a1dfd550608918008c31ae865
Harry Kalodner <harry@offchainlabs.com>
2020-07-09 14:26:11 -0400

    Fix inbox impl in py compiler

--- packages/arb-compiler-evm/arbitrum/basic_vm.py
+++ packages/arb-compiler-evm/arbitrum/basic_vm.py
@@ -109,13 +109,9 @@ class BasicVM:
             self.stack.push(val)
 
     def inbox(self):
-        if (
-            self.env.messages == value.Tuple([])
-            and self.stack.peak() > self.env.time_bounds[0]
-        ):
+        if self.env.messages == value.Tuple([]):
             raise VMBlocked()
 
-        self.stack.pop()
         self.stack.push(self.env.messages)
         self.env.messages = value.Tuple([])
 
