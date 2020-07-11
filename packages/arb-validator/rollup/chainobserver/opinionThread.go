bc784b2620526ace28ed85b3504c38a4891cb39f
Harry Kalodner <harry@offchainlabs.com>
2020-07-11 13:27:29 -0400

    Use mutex instead of channel for preparedAssertions

--- packages/arb-validator/rollup/chainobserver/opinionThread.go
+++ packages/arb-validator/rollup/chainobserver/opinionThread.go
@@ -23,6 +23,7 @@ import (
 	"github.com/offchainlabs/arbitrum/packages/arb-validator/structures"
 	"log"
 	"math/big"
+	"sync"
 	"time"
 
 	"github.com/offchainlabs/arbitrum/packages/arb-util/common"
@@ -34,9 +35,9 @@ import (
 
 func (chain *ChainObserver) startOpinionUpdateThread(ctx context.Context) {
 	go func() {
-		assertionPreparedChan := make(chan *chainlistener.PreparedAssertion, 20)
 		preparingAssertions := make(map[common.Hash]bool)
 		preparedAssertions := make(map[common.Hash]*chainlistener.PreparedAssertion)
+		preparedAssertionsMut := new(sync.Mutex)
 
 		updateCurrent := func() {
 			currentOpinion := chain.calculatedValidNode
@@ -59,8 +60,9 @@ func (chain *ChainObserver) startOpinionUpdateThread(ctx context.Context) {
 			var newOpinion valprotocol.ChildType
 			var nextMachine machine.Machine
 			var validExecution *protocol.ExecutionAssertion
+			preparedAssertionsMut.Lock()
 			prepped, found := preparedAssertions[currentHash]
-
+			preparedAssertionsMut.Unlock()
 			disputable := successor.Disputable()
 
 			if disputable == nil {
@@ -96,8 +98,9 @@ func (chain *ChainObserver) startOpinionUpdateThread(ctx context.Context) {
 			}
 			// Reset prepared
 			preparingAssertions = make(map[common.Hash]bool)
+			preparedAssertionsMut.Lock()
 			preparedAssertions = make(map[common.Hash]*chainlistener.PreparedAssertion)
-
+			preparedAssertionsMut.Unlock()
 			chain.RLock()
 			correctNode := chain.NodeGraph.GetSuccessor(currentOpinion, newOpinion)
 			if correctNode != nil {
@@ -128,9 +131,6 @@ func (chain *ChainObserver) startOpinionUpdateThread(ctx context.Context) {
 			select {
 			case <-ctx.Done():
 				return
-			case prepped := <-assertionPreparedChan:
-				log.Println("opinion thread prepared assertion")
-				preparedAssertions[prepped.Prev.Hash()] = prepped
 			case <-ticker.C:
 				chain.RLock()
 				// Catch up to current head
@@ -159,11 +159,16 @@ func (chain *ChainObserver) startOpinionUpdateThread(ctx context.Context) {
 						go func() {
 							prepped := chain.PrepareAssertion()
 							log.Println("prepped assertion", prepped)
-							assertionPreparedChan <- prepped
+							preparedAssertionsMut.Lock()
+							preparedAssertions[prepped.Prev.Hash()] = prepped
+							preparedAssertionsMut.Unlock()
+							log.Println("sent prepped assertion")
 						}()
 					}
 				} else {
+					preparedAssertionsMut.Lock()
 					prepared, isPrepared := preparedAssertions[chain.calculatedValidNode.Hash()]
+					preparedAssertionsMut.Unlock()
 					log.Println("opinion2", chain.calculatedValidNode.Hash().ShortString(), isPrepared, chain.NodeGraph.Leaves().IsLeaf(chain.calculatedValidNode))
 					if isPrepared && chain.NodeGraph.Leaves().IsLeaf(chain.calculatedValidNode) {
 						chain.RUnlock()
