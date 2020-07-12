f8ee6e915dc3c197699883835445d3c977536550
Harry Kalodner <harry@offchainlabs.com>
2020-07-12 18:15:44 -0400

    Fix election demo

--- demos/election/truffle-config.js
+++ demos/election/truffle-config.js
@@ -1,42 +1,40 @@
-const ArbProvider = require("arb-provider-truffle");
-const mnemonic =
-  "jar deny prosper gasp flush glass core corn alarm treat leg smart";
+const fetch = require('node-fetch')
+global.fetch = fetch
+
+const Web3 = require('web3')
+const ArbProvider = require('arb-provider-web3')
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: '*', // Match any network id
     },
     arbitrum: {
-      provider: function() {
-        if (typeof this.provider.prov == "undefined") {
-          this.provider.prov = ArbProvider.provider(
-            __dirname,
-            "build/contracts",
-            {
-              mnemonic: mnemonic
-            }
-          );
-        }
-        return this.provider.prov;
+      provider: function () {
+        return ArbProvider(
+          'http://localhost:1235',
+          new Web3.providers.HttpProvider('http://localhost:7545')
+        )
       },
-      network_id: "*"
-    }
+      network_id: '*',
+      gasPrice: 0,
+    },
   },
   compilers: {
     solc: {
-      version: "0.4.25", // Fetch exact version from solc-bin (default: truffle's version)
+      version: '0.4.25', // Fetch exact version from solc-bin (default: truffle's version)
       docker: true, // Use "0.5.3" you've installed locally with docker (default: false)
       settings: {
         // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 200
-        }
-      }
-    }
-  }
-};
+          runs: 200,
+        },
+      },
+    },
+  },
+}
