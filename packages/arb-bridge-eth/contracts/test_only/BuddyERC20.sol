032bb07b101937e0e05df0b0ffcfe8d42b480e5b
Minh Truong <minhptruong1@gmail.com>
2020-07-29 12:39:11 -0400

    buddy contract

--- packages/arb-bridge-eth/contracts/test_only/BuddyERC20.sol
+++ packages/arb-bridge-eth/contracts/test_only/BuddyERC20.sol
@@ -17,3 +17,52 @@
  */
 
 pragma solidity ^0.5.11;
+
+import "arbos-contracts/contracts/ArbSys.sol";
+import "../inbox/IGlobalInbox.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract ArbBaseERC20 is ERC20, ERC20Detailed {
+    constructor() public ERC20Detailed("Token Buddy", "TB", 18) {}
+}
+
+// contract PairedErc20 is ArbBaseERC20 {
+//     function mint(address account, uint256 amount) external;
+//     function burn(address account, uint256 amount) external;
+// }
+
+contract ArbERC20 is ArbBaseERC20 {
+    function adminMint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+
+    function withdraw(address account, uint256 amount) public {
+        _burn(msg.sender, amount);
+        ArbSys(100).withdrawERC20(account, amount);
+    }
+}
+
+contract BuddyERC20 is ArbBaseERC20 {
+    address public inbox;
+
+    constructor() public {}
+
+    function initialize(address _rollupChain, address _inbox) public {
+        inbox = _inbox;
+        IGlobalInbox(_inbox).deployL2ContractPair(
+            _rollupChain,
+            type(ArbERC20).creationCode
+        );
+    }
+
+    function mint(address account, uint256 amount) public {
+        require(inbox == msg.sender, "must be authorized rollup-chain");
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public {
+        require(inbox == msg.sender, "must be authorized rollup-chain");
+        _burn(account, amount);
+    }
+}
