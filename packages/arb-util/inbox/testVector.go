c0ade08ea724ad2e31e11dbf1549c22dde7dbec6
Harry Kalodner <harry@offchainlabs.com>
2020-08-04 03:31:14 -0400

    Update arbos and add arbos tests to validator

--- packages/arb-util/inbox/testVector.go
+++ packages/arb-util/inbox/testVector.go
@@ -4,17 +4,12 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/offchainlabs/arbitrum/packages/arb-util/value"
+	"math/big"
 )
 
-type JSONValue interface {
-}
-
-type JSONTuple struct {
-	Tuple []JSONValue `json:"Tuple"`
-}
-
-type JSONInt struct {
-	Int string `json:"Int"`
+type JSONValue struct {
+	Tuple []JSONValue `json:"Tuple,omitempty"`
+	Int   *string     `json:"Int,omitempty"`
 }
 
 type TestVector struct {
@@ -58,21 +53,80 @@ func TestVectorJSON(inbox []InboxMessage, logs []value.Value, sends []value.Valu
 	return json.Marshal(vector)
 }
 
+func LoadTestVector(data []byte) ([]InboxMessage, []value.Value, []value.Value, error) {
+	testVector := new(TestVector)
+	if err := json.Unmarshal(data, testVector); err != nil {
+		return nil, nil, nil, err
+	}
+	inboxMessages := make([]InboxMessage, 0, len(testVector.Inbox))
+	for _, msg := range testVector.Inbox {
+		val, err := jsonToValue(msg)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		im, err := NewInboxMessageFromValue(val)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		inboxMessages = append(inboxMessages, im)
+	}
+	avmLogs := make([]value.Value, 0, len(testVector.Logs))
+	for _, avmLog := range testVector.Logs {
+		val, err := jsonToValue(avmLog)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		avmLogs = append(avmLogs, val)
+	}
+	avmSends := make([]value.Value, 0, len(testVector.Sends))
+	for _, avmSend := range testVector.Sends {
+		val, err := jsonToValue(avmSend)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		avmSends = append(avmSends, val)
+	}
+	return inboxMessages, avmLogs, avmSends, nil
+}
+
 func valueToJSON(val value.Value) (JSONValue, error) {
 	switch val := val.(type) {
 	case value.IntValue:
-		return JSONInt{Int: val.BigInt().Text(16)}, nil
+		intString := val.BigInt().Text(16)
+		return JSONValue{Int: &intString}, nil
 	case value.TupleValue:
 		vals := make([]JSONValue, 0)
 		for _, subVal := range val.Contents() {
 			jsonSubVal, err := valueToJSON(subVal)
 			if err != nil {
-				return nil, err
+				return JSONValue{}, err
 			}
 			vals = append(vals, jsonSubVal)
 		}
-		return JSONTuple{Tuple: vals}, nil
+		return JSONValue{Tuple: vals}, nil
 	default:
-		return nil, errors.New("unsupported type")
+		return JSONValue{}, errors.New("unsupported type")
+	}
+}
+
+func jsonToValue(val JSONValue) (value.Value, error) {
+	if val.Int != nil {
+		intVal, ok := new(big.Int).SetString(*val.Int, 16)
+		if !ok {
+			return nil, errors.New("invalid int value")
+		}
+		return value.NewIntValue(intVal), nil
+	} else if val.Tuple != nil {
+		vals := make([]value.Value, 0)
+		for _, jsonSubVal := range val.Tuple {
+			subVal, err := jsonToValue(jsonSubVal)
+			if err != nil {
+				return nil, err
+			}
+			vals = append(vals, subVal)
+		}
+		return value.NewTupleFromSlice(vals)
+	} else {
+		return nil, errors.New("unsupported json value")
 	}
 }
