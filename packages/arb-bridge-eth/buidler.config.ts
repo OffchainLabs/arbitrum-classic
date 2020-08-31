5f23f998b4c3f947b4ae050678dcfd868267a82b
Harry Kalodner <harry@offchainlabs.com>
2020-08-31 08:31:33 -0400

    Add back in old verifcation script

--- packages/arb-bridge-eth/buidler.config.ts
+++ packages/arb-bridge-eth/buidler.config.ts
@@ -8,6 +8,11 @@ if (!process.env.DOCKER) {
   usePlugin('solidity-coverage')
   usePlugin('buidler-spdx-license-identifier')
   usePlugin('buidler-gas-reporter')
+  usePlugin('@nomiclabs/buidler-etherscan')
+
+  const verifyTask = require('./scripts/verifyTask') // eslint-disable-line @typescript-eslint/no-var-requires
+  const setupVerifyTask = verifyTask.default
+  setupVerifyTask()
 }
 
 task('accounts', 'Prints the list of accounts', async (taskArgs, bre) => {
