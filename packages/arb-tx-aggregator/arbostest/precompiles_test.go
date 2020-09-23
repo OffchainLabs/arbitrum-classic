d2e046870e601361f98a3afdd9ac9db78a33f0e5
Harry Kalodner <harry@offchainlabs.com>
2020-09-23 12:29:21 -0400

    Update arbos and add ECPairing tests

--- packages/arb-tx-aggregator/arbostest/precompiles_test.go
+++ packages/arb-tx-aggregator/arbostest/precompiles_test.go
@@ -18,9 +18,11 @@ package arbostest
 
 import (
 	"bytes"
+	"crypto"
 	"crypto/rand"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
+	gethcrypto "github.com/ethereum/go-ethereum/crypto"
 	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 	"github.com/offchainlabs/arbitrum/packages/arb-avm-cpp/cmachine"
 	"github.com/offchainlabs/arbitrum/packages/arb-evm/evm"
@@ -92,6 +94,39 @@ func testPrecompile(t *testing.T, precompileNum byte, data []byte, correct []byt
 	t.Logf("Used %v gas", res.GasUsed)
 }
 
+func TestECRecover(t *testing.T) {
+	pk, err := gethcrypto.GenerateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hashedMsg := common.RandHash()
+	sig, err := gethcrypto.Sign(hashedMsg[:], pk)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sig[64] += 27
+
+	data := hashedMsg.Bytes()
+	data = append(data, math.U256Bytes(big.NewInt(int64(sig[64])))...)
+	data = append(data, sig[:64]...)
+
+	signer := gethcrypto.PubkeyToAddress(pk.PublicKey)
+	ret := make([]byte, 32)
+	copy(ret[12:], signer.Bytes())
+
+	testPrecompile(t, 1, data, ret[:])
+}
+
+func TestSha256(t *testing.T) {
+	data := common.RandBytes(100)
+	sha256 := crypto.SHA256.New()
+	sha256.Write(data)
+	hashedCorrect := sha256.Sum(nil)
+
+	testPrecompile(t, 2, data, hashedCorrect)
+}
+
 func TestECAdd(t *testing.T) {
 	_, g1x, err := bn256.RandomG1(rand.Reader)
 	if err != nil {
@@ -117,4 +152,77 @@ func TestECMul(t *testing.T) {
 	correct := new(bn256.G1).ScalarMult(g1x, k).Marshal()
 
 	testPrecompile(t, 7, data, correct)
+}
+
+func testECPairing(t *testing.T, g1Points []*bn256.G1, g2Points []*bn256.G2) {
+	var data []byte
+
+	if len(g1Points) != len(g2Points) {
+		t.Fatal("input slices have different lengths")
+	}
+
+	for i, g1 := range g1Points {
+		data = append(data, g1.Marshal()...)
+		data = append(data, g2Points[i].Marshal()...)
+	}
+
+	isPair := bn256.PairingCheck(g1Points, g2Points)
+	var correct []byte
+	if isPair {
+		correct = math.U256Bytes(big.NewInt(1))
+	} else {
+		correct = math.U256Bytes(big.NewInt(0))
+	}
+
+	testPrecompile(t, 8, data, correct)
+}
+
+func TestECPairing(t *testing.T) {
+	_, p, err := bn256.RandomG1(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, q, err := bn256.RandomG2(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s := common.RandBigInt()
+	negOne := big.NewInt(-1)
+
+	sP := new(bn256.G1).ScalarMult(p, s)
+	sQ := new(bn256.G2).ScalarMult(q, s)
+
+	g1Points := []*bn256.G1{
+		new(bn256.G1).ScalarMult(p, negOne),
+		sP,
+	}
+
+	g2Points := []*bn256.G2{
+		sQ,
+		q,
+	}
+
+	testECPairing(t, g1Points, g2Points)
+}
+
+func TestECPairingRandom(t *testing.T) {
+	var g1Points []*bn256.G1
+	var g2Points []*bn256.G2
+
+	for i := 0; i < 5; i++ {
+		_, g1, err := bn256.RandomG1(rand.Reader)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, g2, err := bn256.RandomG2(rand.Reader)
+		if err != nil {
+			t.Fatal(err)
+		}
+		g1Points = append(g1Points, g1)
+		g2Points = append(g2Points, g2)
+
+	}
+
+	testECPairing(t, g1Points, g2Points)
 }
