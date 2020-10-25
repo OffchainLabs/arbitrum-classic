e696a5849fbb7e4a435ccf5bfe4c99f61ca91b55
Harry Kalodner <harry@offchainlabs.com>
2020-10-25 11:11:39 -0400

    Fix last commit

--- packages/arb-validator-core/ethbridge/globalInboxWatcher.go
+++ packages/arb-validator-core/ethbridge/globalInboxWatcher.go
@@ -225,19 +225,12 @@ func (gi *globalInboxWatcher) processLog(
 		if err != nil {
 			return arbbridge.MessageDeliveredEvent{}, err
 		}
-
-		type TransactionBatchTxCallArgs struct {
-			Chain       ethcommon.Address
-			MessageData []byte
-		}
-
 		args := make(map[string]interface{})
 		err = l2MessageFromOriginCallABI.Inputs.UnpackIntoMap(args, tx.Data()[4:])
 		if err != nil {
 			return arbbridge.MessageDeliveredEvent{}, err
 		}
-
-		return gi.parseMessageFromOrigin(evmLog, timestamp, args["MessageData"].([]byte))
+		return gi.parseMessageFromOrigin(evmLog, timestamp, args["messageData"].([]byte))
 	default:
 		return arbbridge.MessageDeliveredEvent{}, errors2.New("unknown arbitrum event type")
 	}
