8c7e487f21ee9a236068d955708501b5df1f1c83
dzgoldman <dzgoldman@wesleyan.edu>
2020-11-06 17:50:08 -0500

    clean up, add comments, schema, and tests

--- packages/arb-provider-ethers/src/lib/byte_serialize_params.ts
+++ packages/arb-provider-ethers/src/lib/byte_serialize_params.ts
@@ -2,12 +2,18 @@ import { isAddress as _isAddress } from '@ethersproject/address'
 import { concat, hexZeroPad } from '@ethersproject/bytes'
 import { BigNumber } from '@ethersproject/bignumber'
 
-import { getAddressIndex } from 'arb-provider-ethers'
+import { getAddressIndex } from './l2bridge'
 import { JsonRpcProvider } from 'ethers/providers'
 
-export const initSerializeAndLookUpIndices = (arbProviderUrl: string) => {
-  const arbProvider = new JsonRpcProvider(arbProviderUrl)
-  return async (params: any) => {
+type PrimativeType = string | number | boolean | BigNumber
+type PrimativeOrPrimativeArray = PrimativeType | PrimativeType[]
+type BytesNumber = 1 | 4 | 8 | 16 | 32
+
+// to use:
+// const mySerializeParamsFunction = initSerializeAndLookUpIndices("myAggregatorUrl")
+export const initSerializeAndLookUpIndices = (arbAggregatorUrl: string) => {
+  const arbProvider = new JsonRpcProvider(arbAggregatorUrl)
+  return async (params: PrimativeOrPrimativeArray[]) => {
     return await serializeParams(params, async (address: string) => {
       return await getAddressIndex(address, arbProvider)
     })
@@ -16,37 +22,40 @@ export const initSerializeAndLookUpIndices = (arbProviderUrl: string) => {
 
 const isAddress = (input: any) => typeof input === 'string' && _isAddress(input)
 
-type PrimativeType = string | number | boolean | BigNumber
+const toUint = (val: PrimativeType, bytes: BytesNumber) =>
+  hexZeroPad(BigNumber.from(val).toHexString(), bytes)
 
+//  outputs string suitable for formatting
 const formatPrimative = (value: PrimativeType) => {
   if (isAddress(value)) {
     return value as string
   } else if (typeof value === 'boolean') {
-    return new Uint8Array([value ? 1 : 0])
+    return toUint(value ? 1 : 0, 1)
   } else if (
     typeof value === 'number' ||
     +value ||
     BigNumber.isBigNumber(value)
   ) {
-    return hexZeroPad(BigNumber.from(value).toHexString(), 32)
+    return toUint(value, 32)
   } else {
     throw new Error('unsupported type')
   }
 }
 
 export const serializeParams = async (
-  params: (PrimativeType | PrimativeType[])[],
+  params: PrimativeOrPrimativeArray[],
   addressToIndex: (address: string) => Promise<number> = () =>
     new Promise(exec => exec(-1))
 ) => {
-  const formattedParams: (string | Uint8Array | Uint32Array)[] = []
+  const formattedParams: string[] = []
 
   for (let i = 0; i < params.length; i++) {
     const param = params[i]
 
+    // handle arrays
     if (Array.isArray(param)) {
-      let paramArray: PrimativeType[] | number[] = param as PrimativeType[]
-      formattedParams.push(new Uint8Array([paramArray.length]))
+      let paramArray: PrimativeType[] = param as PrimativeType[]
+      formattedParams.push(toUint(paramArray.length, 1))
 
       if (isAddress(paramArray[0])) {
         const indices = await Promise.all(
@@ -54,16 +63,16 @@ export const serializeParams = async (
             async address => await addressToIndex(address as string)
           )
         )
+        // If all addresses are registered, serialize as indices
         if (indices.every(i => i > -1)) {
           paramArray = indices as number[]
-          formattedParams.push(new Uint8Array([1]))
+          formattedParams.push(toUint(1, 1))
           paramArray.forEach(value => {
-            formattedParams.push(
-              hexZeroPad(BigNumber.from(value).toHexString(), 4)
-            )
+            formattedParams.push(toUint(value, 4))
           })
+          // otherwise serialize as address
         } else {
-          formattedParams.push(new Uint8Array([0]))
+          formattedParams.push(toUint(0, 1))
           paramArray.forEach(value => {
             formattedParams.push(formatPrimative(value))
           })
@@ -74,24 +83,18 @@ export const serializeParams = async (
         })
       }
     } else {
-      //  not an array: handle address indexing
+      //  handle non-arrays
       if (isAddress(param)) {
         const index = await addressToIndex(param as string)
         if (index > -1) {
-          formattedParams.push(new Uint8Array([1]))
-          formattedParams.push(
-            hexZeroPad(BigNumber.from(index).toHexString(), 4)
-          )
+          formattedParams.push(toUint(1, 1), toUint(index, 4))
         } else {
-          formattedParams.push(new Uint8Array([0]))
-          formattedParams.push(formatPrimative(param))
+          formattedParams.push(toUint(0, 1), formatPrimative(param))
         }
       } else {
         formattedParams.push(formatPrimative(param))
       }
     }
   }
-  const res = concat(formattedParams)
-  console.info('*** Serialized byte array of length ***', res.length)
-  return res
+  return concat(formattedParams)
 }
