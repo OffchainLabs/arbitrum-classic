4ec693b598f875c9471aa22b4142b9c0ecff4671
Harry Kalodner <harry@offchainlabs.com>
2020-11-30 13:26:42 -0500

    Add interview file

---
+++ packages/arb-avm-cpp/tests/interview.hpp
@@ -0,0 +1,13 @@
+//
+//  interview.hpp
+//  avm_tests
+//
+//  Created by Harry Kalodner on 11/30/20.
+//
+
+#ifndef interview_hpp
+#define interview_hpp
+
+#include <stdio.h>
+
+#endif /* interview_hpp */
