b49a3783f321eb2e1318d74078c3d42f26f235d2
Sami Mäkelä <mrsmkl@gmail.com>
2020-12-04 14:02:15 +0200

    lint

--- packages/arb-bridge-eth/test/bufferproof.ts
+++ packages/arb-bridge-eth/test/bufferproof.ts
@@ -40,8 +40,8 @@ function merkleHash(arr: bytes32[], offset: number, sz: number): bytes32 {
   if (sz === 1) {
     return keccak1(arr[offset])
   } else {
-    let h1 = merkleHash(arr, offset, sz / 2)
-    let h2 = merkleHash(arr, offset + sz / 2, sz / 2)
+    const h1 = merkleHash(arr, offset, sz / 2)
+    const h2 = merkleHash(arr, offset + sz / 2, sz / 2)
     return keccak2(h1, h2)
   }
 }
@@ -92,11 +92,11 @@ export function makeZeros(): bytes32[] {
   return zeros
 }
 
-function fromBytes(buf: Buffer) {
-  let str = buf.toString("hex")
-  let res = []
-  for (let i = 0; i < buf.length/32; i++) {
-    res.push("0x"+str.substr(i*64, 64))
+export function fromBytes(buf: Buffer): bytes32[] {
+  const str = buf.toString('hex')
+  const res = []
+  for (let i = 0; i < buf.length / 32; i++) {
+    res.push('0x' + str.substr(i * 64, 64))
   }
   return res
 }
