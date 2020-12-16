affd43fbb007d7d163478ec439ff7010ca19e548
Harry Kalodner <harry@offchainlabs.com>
2020-12-16 15:04:47 -0500

    Fix race conditions

--- packages/arb-util/machine/aggregator.go
+++ packages/arb-util/machine/aggregator.go
@@ -21,6 +21,7 @@ import (
 	"github.com/offchainlabs/arbitrum/packages/arb-util/common"
 	"github.com/offchainlabs/arbitrum/packages/arb-util/value"
 	"github.com/pkg/errors"
+	"sync"
 )
 
 type BlockInfo struct {
@@ -51,6 +52,7 @@ type BlockEntry struct {
 }
 
 type InMemoryAggregatorStore struct {
+	sync.Mutex
 	messages     []value.Value
 	logs         []value.Value
 	blocks       []*BlockEntry
@@ -66,6 +68,8 @@ func NewInMemoryAggregatorStore() *InMemoryAggregatorStore {
 }
 
 func (as *InMemoryAggregatorStore) GetMessage(index uint64) (value.Value, error) {
+	as.Lock()
+	defer as.Unlock()
 	if index >= uint64(len(as.messages)) {
 		return nil, errors.New("failed to get l2message")
 	}
@@ -73,6 +77,8 @@ func (as *InMemoryAggregatorStore) GetMessage(index uint64) (value.Value, error)
 }
 
 func (as *InMemoryAggregatorStore) GetLog(index uint64) (value.Value, error) {
+	as.Lock()
+	defer as.Unlock()
 	if index >= uint64(len(as.logs)) {
 		return nil, errors.New("failed to get log")
 	}
@@ -80,6 +86,8 @@ func (as *InMemoryAggregatorStore) GetLog(index uint64) (value.Value, error) {
 }
 
 func (as *InMemoryAggregatorStore) GetPossibleRequestInfo(requestId common.Hash) *uint64 {
+	as.Lock()
+	defer as.Unlock()
 	request, ok := as.requestIndex[requestId]
 	if !ok {
 		return nil
@@ -88,6 +96,8 @@ func (as *InMemoryAggregatorStore) GetPossibleRequestInfo(requestId common.Hash)
 }
 
 func (as *InMemoryAggregatorStore) GetPossibleBlock(blockHash common.Hash) *uint64 {
+	as.Lock()
+	defer as.Unlock()
 	block, ok := as.blockIndex[blockHash]
 	if !ok {
 		return nil
@@ -96,6 +106,8 @@ func (as *InMemoryAggregatorStore) GetPossibleBlock(blockHash common.Hash) *uint
 }
 
 func (as *InMemoryAggregatorStore) GetBlock(height uint64) (*BlockInfo, error) {
+	as.Lock()
+	defer as.Unlock()
 	if height > uint64(len(as.blocks)) {
 		return nil, nil
 	}
@@ -111,6 +123,8 @@ func (as *InMemoryAggregatorStore) GetBlock(height uint64) (*BlockInfo, error) {
 }
 
 func (as *InMemoryAggregatorStore) LatestBlock() (*common.BlockId, error) {
+	as.Lock()
+	defer as.Unlock()
 	if len(as.blocks) == 0 {
 		return nil, errors.New("No blocks")
 	}
@@ -122,16 +136,22 @@ func (as *InMemoryAggregatorStore) LatestBlock() (*common.BlockId, error) {
 }
 
 func (as *InMemoryAggregatorStore) SaveLog(val value.Value) error {
+	as.Lock()
+	defer as.Unlock()
 	as.logs = append(as.logs, val)
 	return nil
 }
 
 func (as *InMemoryAggregatorStore) SaveMessage(val value.Value) error {
+	as.Lock()
+	defer as.Unlock()
 	as.messages = append(as.messages, val)
 	return nil
 }
 
 func (as *InMemoryAggregatorStore) SaveBlock(header *types.Header, logIndex uint64) error {
+	as.Lock()
+	defer as.Unlock()
 	as.blocks = append(as.blocks, &BlockEntry{
 		header:   header,
 		logIndex: &logIndex,
@@ -140,6 +160,8 @@ func (as *InMemoryAggregatorStore) SaveBlock(header *types.Header, logIndex uint
 }
 
 func (as *InMemoryAggregatorStore) SaveEmptyBlock(header *types.Header) error {
+	as.Lock()
+	defer as.Unlock()
 	as.blocks = append(as.blocks, &BlockEntry{
 		header: header,
 	})
@@ -147,16 +169,22 @@ func (as *InMemoryAggregatorStore) SaveEmptyBlock(header *types.Header) error {
 }
 
 func (as *InMemoryAggregatorStore) SaveBlockHash(blockHash common.Hash, blockHeight uint64) error {
+	as.Lock()
+	defer as.Unlock()
 	as.blockIndex[blockHash] = blockHeight
 	return nil
 }
 
 func (as *InMemoryAggregatorStore) SaveRequest(requestId common.Hash, logIndex uint64) error {
+	as.Lock()
+	defer as.Unlock()
 	as.requestIndex[requestId] = logIndex
 	return nil
 }
 
 func (as *InMemoryAggregatorStore) Reorg(height uint64, messageCount uint64, logCount uint64) error {
+	as.Lock()
+	defer as.Unlock()
 	as.messages = as.messages[:messageCount]
 	as.logs = as.logs[:logCount]
 	as.blocks = as.blocks[:height]
