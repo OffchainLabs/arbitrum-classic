32357e8ba57f6796c0dc8042deb475038a03ff92
Harry Kalodner <harry@offchainlabs.com>
2020-12-16 09:19:03 -0500

    Add more logging

--- packages/arb-tx-aggregator/cmd/arb-dev-aggregator/arb-dev-aggregator.go
+++ packages/arb-tx-aggregator/cmd/arb-dev-aggregator/arb-dev-aggregator.go
@@ -62,6 +62,8 @@ func main() {
 	// Print stack trace when `.Error().Stack().Err(err).` is added to zerolog call
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
 	// Print line number that log was created on
 	logger = log.With().Caller().Str("component", "arb-dev-aggregator").Logger()
 
