15b76b96dcb54ba576e7a0e0ec656c2797381a1c
Harry Kalodner <harry@offchainlabs.com>
2021-01-17 03:26:06 -0500

    Add comments to RollupCore.sol

--- packages/arb-bridge-eth/contracts/rollup/RollupCore.sol
+++ packages/arb-bridge-eth/contracts/rollup/RollupCore.sol
@@ -48,91 +48,164 @@ contract RollupCore {
 
     mapping(address => uint256) private _withdrawableFunds;
 
-    function getNode(uint256 i) public view returns (INode) {
-        return _nodes[i];
+    /**
+     * @notice Get the address of the Node contract for the given node
+     * @param nodeNum Index of the node
+     * @return Address of the Node contract
+     */
+    function getNode(uint256 nodeNum) public view returns (INode) {
+        return _nodes[nodeNum];
     }
 
-    function getStakerAddress(uint256 i) public view returns (address) {
-        return _stakerList[i];
+    /**
+     * @notice Get the address of the staker at the given index
+     * @param stakerNum Index of the staker
+     * @return Address of the staker
+     */
+    function getStakerAddress(uint256 stakerNum) public view returns (address) {
+        return _stakerList[stakerNum];
     }
 
+    /**
+     * @notice Check whether the given staker is staked
+     * @param staker Staker address to check
+     * @return True or False for whether the staker was staked
+     */
     function isStaked(address staker) public view returns (bool) {
         return _stakerMap[staker].isStaked;
     }
 
+    /**
+     * @notice Get the latest staked node of the given staker
+     * @param staker Staker address to lookup
+     * @return Latest node staked of the staker
+     */
     function latestStakedNode(address staker) public view returns (uint256) {
         return _stakerMap[staker].latestStakedNode;
     }
 
+    /**
+     * @notice Get the current challenge of the given staker
+     * @param staker Staker address to lookup
+     * @return Current challenge of the staker
+     */
     function currentChallenge(address staker) public view returns (address) {
         return _stakerMap[staker].currentChallenge;
     }
 
+    /**
+     * @notice Get the amount staked of the given staker
+     * @param staker Staker address to lookup
+     * @return Amount staked of the staker
+     */
     function amountStaked(address staker) public view returns (uint256) {
         return _stakerMap[staker].amountStaked;
     }
 
-    function zombieAddress(uint256 i) public view returns (address) {
-        return _zombies[i].stakerAddress;
+    /**
+     * @notice Get the original staker address of the zombie at the given index
+     * @param zombieNum Index of the zombie to lookup
+     * @return Original staker address of the zombie
+     */
+    function zombieAddress(uint256 zombieNum) public view returns (address) {
+        return _zombies[zombieNum].stakerAddress;
     }
 
-    function zombieLatestStakedNode(uint256 i) public view returns (uint256) {
-        return _zombies[i].latestStakedNode;
+    /**
+     * @notice Get Latest node that the given zombie at the given index is staked on
+     * @param zombieNum Index of the zombie to lookup
+     * @return Latest node that the given zombie is staked on
+     */
+    function zombieLatestStakedNode(uint256 zombieNum) public view returns (uint256) {
+        return _zombies[zombieNum].latestStakedNode;
     }
 
+    // @return Current number of un-removed zombies
     function zombieCount() public view returns (uint256) {
         return _zombies.length;
     }
 
+    /**
+     * @notice Get the amount of funds withdrawable by the given address
+     * @param owner Address to check the funds of
+     * @return Amount of funds withdrawable by owner
+     */
     function withdrawableFunds(address owner) public view returns (uint256) {
         return _withdrawableFunds[owner];
     }
 
+    /**
+     * @return Index of the first unresolved node
+     * @dev If all nodes have been resolved, this will be latestNodeCreated + 1
+     */
     function firstUnresolvedNode() public view returns (uint256) {
         return _firstUnresolvedNode;
     }
 
+    // @return Index of the latest confirmed node
     function latestConfirmed() public view returns (uint256) {
         return _latestConfirmed;
     }
 
+    // @return Index of the latest rollup node created
     function latestNodeCreated() public view returns (uint256) {
         return _latestNodeCreated;
     }
 
+    // @return Ethereum block that the most recent stake was created
     function lastStakeBlock() public view returns (uint256) {
         return _lastStakeBlock;
     }
 
+    // @return Number of active stakers currently staked
     function stakerCount() public view returns (uint256) {
         return _stakerList.length;
     }
 
+    /**
+     * @notice Initialize the core with an initial node
+     * @param initialNode Initial node to start the chain with
+     */
     function initializeCore(INode initialNode) internal {
         _nodes[0] = initialNode;
         _firstUnresolvedNode = 1;
     }
 
+    /**
+     * @notice React to a new node being created by storing it an incrementing the latest node counter
+     * @param node Node that was newly created
+     */
     function nodeCreated(INode node) internal {
         _latestNodeCreated++;
         _nodes[_latestNodeCreated] = node;
     }
 
+    /**
+     * @notice Update the latest node created
+     * @param newLatestNodeCreated New value for the latest node created
+     */
     function updateLatestNodeCreated(uint256 newLatestNodeCreated) internal {
         _latestNodeCreated = newLatestNodeCreated;
     }
 
+    // @notice Reject the next unresolved node
     function rejectNextNode() internal {
         destroyNode(_firstUnresolvedNode);
         _firstUnresolvedNode++;
     }
 
+    // @notice Confirm the next unresolved node
     function confirmNextNode() internal {
         destroyNode(_latestConfirmed);
         _latestConfirmed = _firstUnresolvedNode;
         _firstUnresolvedNode++;
     }
 
+    /**
+     * @notice Create a new stake
+     * @param stakerAddress Address of the new staker
+     * @param depositAmount Stake amount of the new staker
+     */
     function createNewStake(address payable stakerAddress, uint256 depositAmount) internal {
         uint256 stakerIndex = _stakerList.length;
         _stakerList.push(stakerAddress);
@@ -146,6 +219,12 @@ contract RollupCore {
         _lastStakeBlock = block.number;
     }
 
+    /**
+     * @notice Check to see whether the two stakers are in the same challenge
+     * @param stakerAddress1 Address of the first staker
+     * @param stakerAddress2 Address of the second staker
+     * @return Address of the challenge that the two stakers are in
+     */
     function inChallenge(address stakerAddress1, address stakerAddress2)
         internal
         view
@@ -155,19 +234,50 @@ contract RollupCore {
         Staker storage staker2 = _stakerMap[stakerAddress2];
         address challenge = staker1.currentChallenge;
         require(challenge == staker2.currentChallenge, "IN_CHAL");
+        require(challenge != address(0), "NO_CHAL");
         return challenge;
     }
 
+    /**
+     * @notice Make the given staker as not being in a challenge
+     * @param stakerAddress Address of the staker to remove from a challenge
+     */
     function clearChallenge(address stakerAddress) internal {
         Staker storage staker = _stakerMap[stakerAddress];
         staker.currentChallenge = address(0);
     }
 
+    /**
+     * @notice Mark both the given stakers as engaged in the challenge
+     * @param staker1 Address of the first staker
+     * @param staker2 Address of the second staker
+     * @param challenge Address of the challenge both stakers are now in
+     */
+    function challengeStarted(
+        address staker1,
+        address staker2,
+        address challenge
+    ) internal {
+        _stakerMap[staker1].currentChallenge = challenge;
+        _stakerMap[staker2].currentChallenge = challenge;
+    }
+
+    /**
+     * @notice Add to the stake of the given staker by the given amount
+     * @param stakerAddress Address of the staker to increase the stake of
+     * @param amountAdded Amount of stake to add to the staker
+     */
     function increaseStakeBy(address stakerAddress, uint256 amountAdded) internal {
         Staker storage staker = _stakerMap[stakerAddress];
         staker.amountStaked += amountAdded;
     }
 
+    /**
+     * @notice Reduce the stake of the given staker to the given target
+     * @param stakerAddress Address of the staker to reduce the stake of
+     * @param target Amount of stake to leave with the staker
+     * @return Amount of value released from the stake
+     */
     function reduceStakeTo(address stakerAddress, uint256 target) internal returns (uint256) {
         Staker storage staker = _stakerMap[stakerAddress];
         uint256 current = staker.amountStaked;
@@ -178,27 +288,49 @@ contract RollupCore {
         return amountWithdrawn;
     }
 
+    /**
+     * @notice Remove the given staker and turn them into a zombie
+     * @param stakerAddress Address of the staker to remove
+     */
     function turnIntoZombie(address stakerAddress) internal {
         Staker storage staker = _stakerMap[stakerAddress];
         _zombies.push(Zombie(stakerAddress, staker.latestStakedNode));
-        deleteStaker(staker, stakerAddress);
+        deleteStaker(stakerAddress);
     }
 
-    function zombieUpdateLatestStakedNode(uint256 i, uint256 latest) internal {
-        _zombies[i].latestStakedNode = latest;
+    /**
+     * @notice Update the latest staked node of the zombie at the given index
+     * @param zombieNum Index of the zombie to move
+     * @param latest New latest node the zombie is staked on
+     */
+    function zombieUpdateLatestStakedNode(uint256 zombieNum, uint256 latest) internal {
+        _zombies[zombieNum].latestStakedNode = latest;
     }
 
-    function removeZombie(uint256 i) internal {
-        _zombies[i] = _zombies[_zombies.length - 1];
+    /**
+     * @notice Remove the zombie at the given index
+     * @param zombieNum Index of the zombie to remove
+     */
+    function removeZombie(uint256 zombieNum) internal {
+        _zombies[zombieNum] = _zombies[_zombies.length - 1];
         _zombies.pop();
     }
 
+    /**
+     * @notice Remove the given staker and return their stake
+     * @param stakerAddress Address of the staker withdrawing their stake
+     */
     function withdrawStaker(address stakerAddress) internal {
         Staker storage staker = _stakerMap[stakerAddress];
         _withdrawableFunds[stakerAddress] += staker.amountStaked;
-        deleteStaker(staker, stakerAddress);
+        deleteStaker(stakerAddress);
     }
 
+    /**
+     * @notice Advance the given staker to the given node
+     * @param stakerAddress Address of the staker adding their stake
+     * @param nodeNum Index of the node to stake on
+     */
     function stakeOnNode(address stakerAddress, uint256 nodeNum) internal {
         Staker storage staker = _stakerMap[stakerAddress];
         INode node = _nodes[nodeNum];
@@ -206,22 +338,23 @@ contract RollupCore {
         staker.latestStakedNode = nodeNum;
     }
 
-    function challengeStarted(
-        address staker1,
-        address staker2,
-        address challenge
-    ) internal {
-        _stakerMap[staker1].currentChallenge = challenge;
-        _stakerMap[staker2].currentChallenge = challenge;
-    }
-
+    /**
+     * @notice Clear the withdrawable funds for the given address
+     * @param owner Address of the account to remove funds from
+     * @return Amount of funds removed from account
+     */
     function withdrawFunds(address owner) internal returns (uint256) {
         uint256 amount = _withdrawableFunds[owner];
         _withdrawableFunds[owner] = 0;
         return amount;
     }
 
-    function deleteStaker(Staker storage staker, address stakerAddress) private {
+    /**
+     * @notice Remove the given staker
+     * @param stakerAddress Address of the staker to remove
+     */
+    function deleteStaker(address stakerAddress) private {
+        Staker storage staker = _stakerMap[stakerAddress];
         uint256 stakerIndex = staker.index;
         _stakerList[stakerIndex] = _stakerList[_stakerList.length - 1];
         _stakerMap[_stakerList[stakerIndex]].index = stakerIndex;
@@ -229,6 +362,10 @@ contract RollupCore {
         delete _stakerMap[stakerAddress];
     }
 
+    /**
+     * @notice Destroy the given node and clear out its address
+     * @param nodeNum Index of the node to remove
+     */
     function destroyNode(uint256 nodeNum) private {
         _nodes[nodeNum].destroy();
         _nodes[nodeNum] = INode(0);
