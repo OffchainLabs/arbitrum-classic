f49a45f41b1ea1d637f7ac4bbfdfa1c404f9f655
Lee Bousfield <ljbousfield@gmail.com>
2021-02-09 10:42:05 -0600

    Fix instrumentation

--- packages/arb-avm-cpp/configure-for-fuzzing.sh
+++ packages/arb-avm-cpp/configure-for-fuzzing.sh
@@ -8,7 +8,7 @@ cores="$(nproc --all || sysctl -n hw.logicalcpu)"
 
 mkdir -p build
 cd build
-cmake -DCMAKE_C_FLAGS=-finstrument-functions -DCMAKE_CXX_FLAGS=-finstrument-functions -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=hfuzz-clang -DCMAKE_CXX_COMPILER=hfuzz-clang++ ..
 make -j"$cores"
 cd ..
 ./scripts/install-cmachine-build
--- packages/arb-node-core/cmd/machine-fuzz/build.sh
+++ packages/arb-node-core/cmd/machine-fuzz/build.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 set -eu
 
 rm -f machine-fuzz
-CC=hfuzz-clang CXX=hfuzz-clang++ CFLAGS=-finstrument-functions CXXFLAGS=-finstrument-functions CGO_CFLAGS=-finstrument-functions CGO_CXXFLAGS=-finstrument-functions go build
+CC=hfuzz-clang CXX=hfuzz-clang++ go build
