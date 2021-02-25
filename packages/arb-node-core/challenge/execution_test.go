f9af95889afbde00096aa35ace432ed6bf9658cc
Lee Bousfield <ljbousfield@gmail.com>
2021-02-25 08:51:08 -0600

    Test challenge with a message (currently broken)

--- packages/arb-node-core/challenge/execution_test.go
+++ packages/arb-node-core/challenge/execution_test.go
@@ -2,17 +2,23 @@ package challenge
 
 import (
 	"io/ioutil"
+	"math"
 	"math/big"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/offchainlabs/arbitrum/packages/arb-avm-cpp/cmachine"
+	"github.com/offchainlabs/arbitrum/packages/arb-evm/message"
 	"github.com/offchainlabs/arbitrum/packages/arb-node-core/test"
 	"github.com/offchainlabs/arbitrum/packages/arb-util/arbos"
+	"github.com/offchainlabs/arbitrum/packages/arb-util/common"
+	"github.com/offchainlabs/arbitrum/packages/arb-util/core"
+	"github.com/offchainlabs/arbitrum/packages/arb-util/inbox"
+	"github.com/offchainlabs/arbitrum/packages/arb-util/protocol"
 )
 
-func TestExecutionChallenge(t *testing.T) {
+func runExecutionTest(t *testing.T, messages []inbox.InboxMessage, gas *big.Int, faultConfig faultConfig) {
 	tmpDir, err := ioutil.TempDir("", "arbitrum")
 	test.FailIfError(t, err)
 	defer func() {
@@ -33,6 +39,10 @@ func TestExecutionChallenge(t *testing.T) {
 		t.Fatal("failed to start thread")
 	}
 
+	if len(messages) > 0 {
+		_, err = core.DeliverMessagesAndWait(arbCore, messages, common.Hash{}, false)
+		test.FailIfError(t, err)
+	}
 	for {
 		if arbCore.MachineIdle() {
 			break
@@ -40,11 +50,9 @@ func TestExecutionChallenge(t *testing.T) {
 		<-time.After(time.Millisecond * 200)
 	}
 
-	challengedNode := initializeChallengeData(t, arbCore, big.NewInt(200000))
+	challengedNode := initializeChallengeData(t, arbCore, gas)
 
-	faultyCore := newFaultyCore(arbCore, faultConfig{
-		distortMachineAtGas: big.NewInt(100000),
-	})
+	faultyCore := newFaultyCore(arbCore, faultConfig)
 
 	time := big.NewInt(100)
 	executeChallenge(
@@ -55,4 +63,33 @@ func TestExecutionChallenge(t *testing.T) {
 		arbCore,
 		faultyCore,
 	)
+}
+
+func TestChallengeToOSP(t *testing.T) {
+	runExecutionTest(t, []inbox.InboxMessage{}, big.NewInt(200000), faultConfig{distortMachineAtGas: big.NewInt(100000)})
+}
+
+func TestChallengeToOSPWithMessage(t *testing.T) {
+	owner := common.RandAddress()
+	chain := common.RandAddress()
+	initMsg := message.NewInboxMessage(
+		message.Init{
+			ChainParams: protocol.ChainParams{
+				StakeRequirement:        big.NewInt(0),
+				StakeToken:              common.Address{},
+				GracePeriod:             common.NewTimeBlocks(big.NewInt(3)),
+				MaxExecutionSteps:       0,
+				ArbGasSpeedLimitPerTick: 0,
+			},
+			Owner:       owner,
+			ExtraConfig: []byte{},
+		},
+		chain,
+		big.NewInt(0),
+		inbox.ChainTime{
+			BlockNum:  common.NewTimeBlocksInt(0),
+			Timestamp: big.NewInt(0),
+		},
+	)
+	runExecutionTest(t, []inbox.InboxMessage{initMsg}, big.NewInt(math.MaxInt64), faultConfig{distortMachineAtGas: big.NewInt(1500000)})
 }
--- packages/arb-node-core/go.mod
+++ packages/arb-node-core/go.mod
@@ -5,12 +5,13 @@ go 1.13
 require (
 	github.com/ethereum/go-ethereum v1.9.24
 	github.com/offchainlabs/arbitrum/packages/arb-avm-cpp v0.7.3
-	github.com/offchainlabs/arbitrum/packages/arb-evm v0.7.3 // indirect
+	github.com/offchainlabs/arbitrum/packages/arb-evm v0.7.3
 	github.com/offchainlabs/arbitrum/packages/arb-util v0.7.3
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.20.0
 )
 
 replace github.com/offchainlabs/arbitrum/packages/arb-util => ../arb-util
+replace github.com/offchainlabs/arbitrum/packages/arb-evm => ../arb-evm
 
 replace github.com/offchainlabs/arbitrum/packages/arb-avm-cpp => ../arb-avm-cpp
