11b881749e1a87964e3d308732c694a10d3ff826
dzgoldman <dzgoldman@wesleyan.edu>
2021-02-28 18:50:56 -0500

    set up typechain for peripherals contracts (synchronize typechain, hardhat, etc version in arb-os contracts; create new arb-ts workspace; update update-abi script; update-abi

--- packages/arb-bridge-peripherals/contracts/tokenbridge/libraries/DecimalConverter.sol
+++ packages/arb-bridge-peripherals/contracts/tokenbridge/libraries/DecimalConverter.sol
@@ -4,13 +4,13 @@
 pragma solidity ^0.6.0;
 
 library DecimalConverter {
-  function from777to20(uint256 amount, uint8 decimals) internal pure returns (uint256) {
-    require(decimals <= 18, 'DEC');
-    return amount / (10 ** uint256(18 - decimals));
-  }
+    function from777to20(uint8 decimals, uint256 amount) internal pure returns (uint256) {
+        require(decimals <= 18, "DEC");
+        return amount / (10**uint256(18 - decimals));
+    }
 
-  function from20to777(uint256 amount, uint8 decimals) internal pure returns (uint256) {
-    require(decimals <= 18, 'DEC');
-    return amount * (10 ** uint256(18 - decimals));
-  }
+    function from20to777(uint8 decimals, uint256 amount) internal pure returns (uint256) {
+        require(decimals <= 18, "DEC");
+        return amount * (10**uint256(18 - decimals));
+    }
 }
---
+++ packages/arbos-contracts/hardhat.config.ts
@@ -0,0 +1,3 @@
+import * as config from 'arb-bridge-eth/hardhat.config'
+// use same config from bridge package
+module.exports = config
