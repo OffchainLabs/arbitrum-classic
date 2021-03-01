2d546cd16fd3b0cd6e70238cefa6f22bdf5a9a2d
fred <fredlacs@gmail.com>
2021-03-01 11:41:15 -0300

    Test if create2 address calculated correctly

--- packages/arb-bridge-peripherals/test/canonicalBridge.ts
+++ packages/arb-bridge-peripherals/test/canonicalBridge.ts
@@ -15,39 +15,76 @@
  */
 
 /* eslint-env node, mocha */
-import { ethers } from 'hardhat';
-import { assert, expect } from 'chai';
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+import { ethers } from 'hardhat'
+import { assert, expect } from 'chai'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+import { Contract, ContractFactory } from 'ethers'
 
 describe('Bridge peripherals layer 2', () => {
-  let accounts: SignerWithAddress[];
-  before(async function() {
-    accounts = await ethers.getSigners();
+  let accounts: SignerWithAddress[]
+  let TestBridge: ContractFactory
+  let testBridge: Contract
+
+  before(async function () {
+    accounts = await ethers.getSigners()
+    TestBridge = await ethers.getContractFactory('TestBridge')
+    testBridge = await TestBridge.deploy()
   })
-  
+
+  it('should calculate proxy address correctly', async function () {
+    const address: string = (await testBridge.functions.templateERC20())[0]
+    // OZ's init code not the same as in https://eips.ethereum.org/EIPS/eip-1167
+    const proxyBytecode =
+      '0x3d602d80600a3d3981f3363d3d373d3d3d363d73' +
+      address.substr(2) +
+      '5af43d82803e903d91602b57fd5bf3'
+
+    const l1ERC20 = '0x0000000000000000000000000000000000000001'
+    const l2ERC20Address = await testBridge.calculateBridgedERC20Address(
+      l1ERC20
+    )
+    const l2AddressExpected = ethers.utils.getCreate2Address(
+      testBridge.address,
+      ethers.utils.hexZeroPad(l1ERC20, 32),
+      ethers.utils.keccak256(proxyBytecode)
+    )
+    assert.equal(
+      l2ERC20Address,
+      l2AddressExpected,
+      'Address calculated incorrectly'
+    )
+  })
   it('should mint erc20 tokens correctly', async function () {
-    const TestBridge = await ethers.getContractFactory("TestBridge");
-    const testBridge = await TestBridge.deploy();
+    const l1ERC20 = '0x0000000000000000000000000000000000000001'
+    const account = '0x0000000000000000000000000000000000000002'
+    const amount = '1'
+    const decimals = '18'
 
-    const l1ERC20 = "0x0000000000000000000000000000000000000001";
-    const account = "0x0000000000000000000000000000000000000002";
-    const amount = "1";
-    const decimals = "18";
+    const l2ERC20Address = await testBridge.calculateBridgedERC20Address(
+      l1ERC20
+    )
 
-    const l2ERC20Address = await testBridge.calculateBridgedERC20Address(l1ERC20);
+    const preTokenCode = await ethers.provider.getCode(l2ERC20Address)
+    assert.equal(preTokenCode, '0x', 'Something already deployed to address')
 
-    const preTokenCode = await ethers.provider.getCode(l2ERC20Address);
-    assert.equal(preTokenCode, "0x", "Something already deployed to address");
+    const tx = await testBridge.mintERC20FromL1(
+      l1ERC20,
+      account,
+      amount,
+      decimals
+    )
 
-    const tx = await testBridge.mintERC20FromL1(l1ERC20, account, amount, decimals);
+    const postTokenCode = await ethers.provider.getCode(l2ERC20Address)
+    assert.notEqual(
+      postTokenCode,
+      '0x',
+      'Token not deployed to correct address'
+    )
 
-    const postTokenCode = await ethers.provider.getCode(l2ERC20Address);
-    assert.notEqual(postTokenCode, "0x", "Token not deployed to correct address");
+    const Erc20 = await ethers.getContractFactory('StandardArbERC20')
+    const erc20 = await Erc20.attach(l2ERC20Address)
 
-    const Erc20 = await ethers.getContractFactory("StandardArbERC20");
-    const erc20 = await Erc20.attach(l2ERC20Address);
-
-    const balance = await erc20.balanceOf(account);
-    assert.equal(balance.toString(), amount, "Tokens not minted correctly");
+    const balance = await erc20.balanceOf(account)
+    assert.equal(balance.toString(), amount, 'Tokens not minted correctly')
   })
 })
