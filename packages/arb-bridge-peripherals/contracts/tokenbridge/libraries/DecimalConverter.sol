d1890ea5401532904ab57ccbb8d984ce734ae0b5
fred <fredlacs@gmail.com>
2021-03-01 14:39:16 -0300

    Add test cases for minting tokens and migrating

--- packages/arb-bridge-peripherals/contracts/tokenbridge/libraries/DecimalConverter.sol
+++ packages/arb-bridge-peripherals/contracts/tokenbridge/libraries/DecimalConverter.sol
@@ -4,12 +4,12 @@
 pragma solidity ^0.6.0;
 
 library DecimalConverter {
-  function from777to20(uint8 decimals, uint amount) internal pure returns (uint256) {
+  function from777to20(uint256 amount, uint8 decimals) internal pure returns (uint256) {
     require(decimals <= 18, 'DEC');
     return amount / (10 ** uint256(18 - decimals));
   }
 
-  function from20to777(uint8 decimals, uint amount) internal pure returns (uint256) {
+  function from20to777(uint256 amount, uint8 decimals) internal pure returns (uint256) {
     require(decimals <= 18, 'DEC');
     return amount * (10 ** uint256(18 - decimals));
   }
