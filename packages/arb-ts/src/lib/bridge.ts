e179616235dedd3c9cd64244fa5e360b66cc8656
dzgoldman <dzgoldman@wesleyan.edu>
2021-03-12 11:25:28 -0800

    fix import casing, connect via right l1/l2 addresses

--- packages/arb-ts/src/lib/bridge.ts
+++ packages/arb-ts/src/lib/bridge.ts
@@ -29,9 +29,9 @@ export class Bridge extends L2Bridge {
     ethSigner: Signer,
     arbSigner: Signer
   ) {
-    super(erc20BridgeAddress, ethSigner)
+    super(arbERC20BridgeAddress, ethSigner)
     // TODO can presumably get arbERC20BridgeAddress directly from the L! bridge
-    this.l1Bridge = new L1Bridge(arbERC20BridgeAddress, arbSigner)
+    this.l1Bridge = new L1Bridge(erc20BridgeAddress, arbSigner)
   }
   public updateAllBalances() {
     this.updateAllTokens()
