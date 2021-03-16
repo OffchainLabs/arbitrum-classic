7c11327a7d260f480ce5ca25ee2e6d96ef7ef76e
Joshua Colvin <jcolvin@offchainlabs.com>
2021-03-16 14:59:15 -0700

    Additional tests from Sami

--- packages/arb-avm-cpp/tests/buffer.cpp
+++ packages/arb-avm-cpp/tests/buffer.cpp
@@ -88,7 +88,7 @@ TEST_CASE("Buffer") {
         REQUIRE(hash_buffer(arr, 0, 131072) == hash_acc(arr, SIZE));
     }
 
-    SECTION("hashing with single zeroes") {
+    SECTION("hashing with single items") {
         const int SIZE = 1024 * 32;
         for (int j = 0; j < 1024; j++) {
             uint8_t arr[SIZE] = {};
@@ -115,6 +115,23 @@ TEST_CASE("Buffer") {
         }
     }
 
+    SECTION("hashing random buffer") {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        const int SIZE = 1024 * 32;
+        std::uniform_int_distribution<> distrib(0, SIZE);
+        for (int i = 0; i < 100; i++) {
+            Buffer buf;
+            uint8_t arr[SIZE] = {};
+            for (int j = 0; j < 3; j++) {
+                auto index = distrib(gen);
+                buf = buf.set(index, 100);
+                arr[index] = 100;
+                REQUIRE(hash_buffer(arr, 0, SIZE) == buf.hash());
+            }
+        }
+    }
+
     SECTION("last index") {
         Buffer buf;
         REQUIRE(buf.lastIndex() == 0);
@@ -134,7 +151,30 @@ TEST_CASE("Buffer") {
         REQUIRE(buf.lastIndex() == 20000);
         buf = buf.set(300000, 123);
         REQUIRE(buf.lastIndex() == 300000);
+        uint64_t idx = 300000L*300000L;
+        buf = buf.set(idx, 123);
+        REQUIRE(buf.lastIndex() == idx);
+        uint64_t idx2 = (1UL << 63UL);
+        buf = buf.set(idx2, 123);
+        REQUIRE(buf.lastIndex() == idx2);
     }
+}
+
+Buffer checkBuffer(ArbStorage& storage, Buffer& buf) {
+    ValueCache value_cache{0, 0};
+    {
+        auto transaction = storage.makeReadWriteTransaction();
+        auto results = saveValue(*transaction, buf);
+        transaction->commit();
+        REQUIRE(results.status.ok());
+    }
+
+    auto transaction = storage.makeReadTransaction();
+    auto res = getValue(*transaction, hash_value(buf), value_cache);
+    REQUIRE(std::holds_alternative<CountedData<value>>(res));
+    REQUIRE(hash_value(std::get<CountedData<value>>(res).data) ==
+            hash_value(buf));
+    return std::get<Buffer>(std::get<CountedData<value>>(res).data);
 }
 
 TEST_CASE("Buffer Serialization") {
@@ -143,21 +183,272 @@ TEST_CASE("Buffer Serialization") {
 
     ValueCache value_cache{0, 0};
 
+    std::random_device
+        rd;  // Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd());  // Standard mersenne_twister_engine seeded with
+                             // rd()
+    std::uniform_int_distribution<> distrib(0, 20000);
+
+    for (int i = 0; i < 1000; i++) {
+        Buffer buf;
+        for (int j = 0; j < 10; j++) {
+            auto index = distrib(gen);
+            buf = buf.set(index, 100);
+        }
+        auto buf2 = checkBuffer(storage, buf);
+        for (int j = 0; j < 10; j++) {
+            auto index = distrib(gen);
+            buf = buf.set(index, 100);
+            buf2 = buf2.set(index, 100);
+            REQUIRE(buf.hash() == buf2.hash());
+        }
+        checkBuffer(storage, buf);
+        checkBuffer(storage, buf2);
+    }
     Buffer buf;
-    buf = buf.set(8192, 123);
+    buf = buf.set(300000L*300000L, 12);
+    checkBuffer(storage, buf);
+    buf = buf.set(1UL << 63UL, 13);
+    checkBuffer(storage, buf);
 
-    {
-        auto transaction = storage.makeReadWriteTransaction();
-        auto results = saveValue(*transaction, buf);
-        transaction->commit();
-        REQUIRE(results.status.ok());
-    }
-
-    {
-        auto transaction = storage.makeReadTransaction();
-        auto res = getValue(*transaction, hash_value(buf), value_cache);
-        REQUIRE(std::holds_alternative<CountedData<value>>(res));
-        REQUIRE(hash_value(std::get<CountedData<value>>(res).data) ==
-                hash_value(buf));
-    }
 }
+
+TEST_CASE("Buffer Hash Failure") {
+    DBDeleter deleter;
+    ArbStorage storage(dbpath);
+
+    ValueCache value_cache{0, 0};
+
+    Buffer buf;
+    buf = buf.set(17750, 100);
+    auto buf2 = checkBuffer(storage, buf);
+    buf = buf.set(14721, 100);
+    buf2 = buf2.set(14721, 100);
+    REQUIRE(buf.hash() == buf2.hash());
+}
+
+std::vector<uint256_t> splitProof(std::vector<unsigned char> data) {
+    auto buf = reinterpret_cast<const char*>(data.data());
+    std::vector<uint256_t> res;
+    for (uint64_t i = 0; i < data.size()/32; i++) {
+        auto a = deserializeUint256t(buf);
+        res.push_back(a);
+    }
+    return res;
+}
+
+// From OneStepProof2.sol
+uint256_t getProof(uint256_t buf, uint64_t loc, std::vector<uint256_t> proof) {
+    // empty tree is full of zeros
+    if (proof.size() == 0) {
+        REQUIRE(buf == hash(0));
+        return 0;
+    }
+    uint256_t acc = hash(proof[0]);
+    for (uint64_t i = 1; i < proof.size(); i++) {
+        if (loc & 1 == 1) acc = hash(proof[i], acc);
+        else acc = hash(acc, proof[i]);
+        loc = loc >> 1;
+    }
+    REQUIRE(acc == buf);
+    // maybe it is a zero outside the actual tree
+    if (loc > 0) return 0;
+    return proof[0];
+}
+
+uint256_t getByte(Buffer buf, uint64_t loc) {
+    return buf.get(loc*32+31);
+}
+
+TEST_CASE("Buffer get proofs") {
+
+
+    SECTION("Empty buffer") {
+        Buffer buf;
+        for (int i = 0; i < 10000; i++) {
+            auto proof = buf.makeProof(i*32);
+            auto proof2 = splitProof(proof);
+            REQUIRE(getByte(buf, i) == getProof(buf.hash(), i, proof2));
+        }
+    }
+
+    SECTION("Buffer with one element") {
+        Buffer buf;
+        buf = buf.set(10000*32+31, 123);
+        for (int i = 0; i < 11000; i++) {
+            auto proof = buf.makeProof(i*32);
+            auto proof2 = splitProof(proof);
+            REQUIRE(getByte(buf, i) == getProof(buf.hash(), i, proof2));
+        }
+    }
+
+    SECTION("Full buffer") {
+        Buffer buf;
+        for (int i = 0; i < 10000; i++) {
+           buf = buf.set(i*32+31, i%256);
+        }
+        for (int i = 0; i < 1000; i++) {
+            auto proof = buf.makeProof(i*32);
+            auto proof2 = splitProof(proof);
+            REQUIRE(getByte(buf, i) == getProof(buf.hash(), i, proof2));
+        }
+        for (int i = 10000; i < 11000; i++) {
+            auto proof = buf.makeProof(i*32);
+            auto proof2 = splitProof(proof);
+            REQUIRE(getByte(buf, i) == getProof(buf.hash(), i, proof2));
+        }
+    }
+
+    SECTION("Random buffer") {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distrib(0, 20000);
+        for (int i = 0; i < 1000; i++) {
+            Buffer buf;
+            for (int j = 0; j < 3; j++) {
+                auto index = distrib(gen);
+                buf = buf.set(index*32+31, 100);
+            }
+            for (int j = 0; j < 10; j++) {
+                auto index = distrib(gen);
+                auto proof = buf.makeProof(index*32);
+                auto proof2 = splitProof(proof);
+                REQUIRE(getByte(buf, index) == getProof(buf.hash(), index, proof2));
+            }
+        }
+    }
+
+}
+
+std::vector<uint256_t> makeZeros() {
+    std::vector<uint256_t> zeros;
+    zeros.resize(64);
+    zeros[0] = hash(0);
+    for (int i = 1; i < 64; i++) {
+        zeros[i] = hash(zeros[i - 1], zeros[i - 1]);
+    }
+    return zeros;
+}
+
+int calcHeight(uint64_t loc) {
+    if (loc == 0) return 1;
+    else return 1 + calcHeight(loc >> 1);
+}
+
+// From OneStepProof2.sol
+uint256_t setProof(uint256_t buf, uint64_t loc, uint256_t v, std::vector<uint256_t> proof,
+    uint64_t nh,
+    uint256_t normal1,
+    uint256_t normal2) {
+    // three possibilities, the tree depth stays same, it becomes lower or it's extended
+    uint256_t acc = hash(v);
+    // check that the proof matches original
+    getProof(buf, loc, proof);
+    std::vector<uint256_t> zeros = makeZeros();
+    // extended
+    if (loc >= uint64_t(1 << (proof.size() - 1))) {
+        if (v == 0) return buf;
+        int height = calcHeight(loc);
+        // build the left branch
+        for (int i = proof.size(); i < height - 1; i++) {
+            buf = hash(buf, zeros[i - 1]);
+        }
+        for (int i = 1; i < height - 1; i++) {
+            if (loc & 1 == 1) acc = hash(zeros[i - 1], acc);
+            else acc = hash(acc, zeros[i - 1]);
+            loc = loc >> 1;
+        }
+        return hash(buf, acc);
+    }
+    for (uint64_t i = 1; i < proof.size(); i++) {
+        uint256_t a = loc & 1 == 1 ? proof[i] : acc;
+        uint256_t b = loc & 1 == 1 ? acc : proof[i];
+        acc = hash(a, b);
+        loc = loc >> 1;
+    }
+    if (v != 0) return acc;
+    if (!(normal2 != zeros[nh] || nh == 0)) throw std::runtime_error("fail");
+    uint256_t res = nh == 0 ? normal1 : hash(normal1, normal2);
+    if (nh > 0) nh--;
+    uint256_t acc2 = res;
+    for (uint64_t i = nh; i < proof.size() - 1; i++) {
+        acc2 = hash(acc2, zeros[i]);
+    }
+    REQUIRE(acc2 == acc);
+    return res;
+}
+
+void testSetProof(Buffer buf, uint64_t loc, uint8_t val) {
+    auto prevHash = buf.hash();
+    auto proof = buf.makeProof(loc*32);
+    auto proof2 = splitProof(proof);
+    Buffer nbuf = buf.set(loc*32+31, val);
+    auto nproof = nbuf.makeNormalizationProof();
+    auto nproof2 = splitProof(nproof);
+    uint64_t nh = nproof[31];
+    auto proofHash = setProof(prevHash, loc, val, proof2, nh, nproof2[1], nproof2[2]);
+    REQUIRE(nbuf.hash() == proofHash);
+}
+
+TEST_CASE("Buffer set proofs") {
+
+    SECTION("Empty buffer") {
+        Buffer buf;
+        for (int i = 0; i < 1000; i++) {
+            testSetProof(buf, i, 0);
+            testSetProof(buf, i, 123);
+            Buffer nbuf = buf.set(i*32+31, 123);
+            testSetProof(nbuf, i, 0);
+        }
+    }
+
+    SECTION("Buffer with one elem") {
+        Buffer buf;
+        buf = buf.set(500*32+31, 123);
+        for (int i = 0; i < 1000; i++) {
+            testSetProof(buf, i, 0);
+            testSetProof(buf, i, 123);
+            Buffer nbuf = buf.set(i*32+31, 123);
+            testSetProof(nbuf, i, 0);
+        }
+        uint64_t idx = 300000L*300000L;
+        testSetProof(buf, idx, 12);
+        uint64_t idx2 = (1UL << 58UL);
+        testSetProof(buf, idx2, 12);
+    }
+
+    SECTION("Full buffer") {
+        Buffer buf;
+        for (int i = 0; i < 10000; i++) {
+           buf = buf.set(i*32+31, i%256);
+        }
+        for (int i = 0; i < 1000; i += 10) {
+            testSetProof(buf, i, 0);
+            testSetProof(buf, i, 123);
+        }
+        for (int i = 10000; i < 11000; i += 10) {
+            testSetProof(buf, i, 0);
+            testSetProof(buf, i, 123);
+        }
+    }
+
+    SECTION("Random buffer") {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distrib(0, 20000);
+        for (int i = 0; i < 1000; i++) {
+            Buffer buf;
+            for (int j = 0; j < 3; j++) {
+                auto index = distrib(gen);
+                buf = buf.set(index*32+31, 100);
+                testSetProof(buf, index, 0);
+            }
+            for (int j = 0; j < 10; j++) {
+                auto index = distrib(gen);
+                testSetProof(buf, index, 123);
+            }
+        }
+    }
+
+}
+
