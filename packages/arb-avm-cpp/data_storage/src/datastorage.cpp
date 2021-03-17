a29fbf91ec7090cb4bb9bfdac7dd3c6d263cd4f4
Joshua Colvin <jcolvin@offchainlabs.com>
2021-03-17 11:46:41 -0700

    Increase max write ahead log size to 50MB

--- packages/arb-avm-cpp/data_storage/src/datastorage.cpp
+++ packages/arb-avm-cpp/data_storage/src/datastorage.cpp
@@ -53,8 +53,8 @@ DataStorage::DataStorage(const std::string& db_path) {
     // Increase the number of threads to open files to offset slow disk access
     options.max_file_opening_threads = 50;
 
-    // Decrease the WAL log size to 20MB so that DB is flushed regularly
-    options.max_total_wal_size = 20971520;
+    // Decrease the WAL log size to 50MB so that DB is flushed regularly
+    options.max_total_wal_size = 52428800;
 
     // No need to wait for manual flush to finish
     flush_options.wait = false;
