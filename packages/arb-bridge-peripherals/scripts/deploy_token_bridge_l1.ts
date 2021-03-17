0ea8608b92a24fba4bd569517a566d62f8e3fa09
fred <fredlacs@gmail.com>
2021-03-16 21:02:24 -0500

    Await for batch proof information to be available

--- packages/arb-bridge-peripherals/scripts/deploy_token_bridge_l1.ts
+++ packages/arb-bridge-peripherals/scripts/deploy_token_bridge_l1.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from '@ethersproject/bignumber'
 import { concat, id, keccak256, zeroPad } from 'ethers/lib/utils'
 import { ethers } from 'hardhat'
 import deployments from '../deployment.json'
@@ -46,15 +47,16 @@ const main = async () => {
     id('InboxMessageDeliveredFromOrigin(uint256)'),
   ]
   const inboxEvent = deployReceipt.logs.filter(
-    log => log.topics[0] === inboxEventSignature[0] || log.topics[0] === inboxEventSignature[1]
+    log =>
+      log.topics[0] === inboxEventSignature[0] ||
+      log.topics[0] === inboxEventSignature[1]
   )
 
-  if(inboxEvent.length !== 1) {
+  if (inboxEvent.length !== 1) {
     console.log(inboxEvent)
-    throw new Error("Triggered inbox multiple times?")
+    throw new Error('Triggered inbox multiple times?')
   }
   const inboxSequenceNumber = inboxEvent[0].topics[1]
-
 
   const l2DeployTxHash = keccak256(
     concat([
@@ -63,66 +65,110 @@ const main = async () => {
     ])
   )
 
-
-  const l2Provider = new ethers.providers.JsonRpcProvider("https://devnet-l2.arbitrum.io/rpc")
+  const l2Provider = new ethers.providers.JsonRpcProvider(
+    'https://devnet-l2.arbitrum.io/rpc'
+  )
   const l2TxReceipt = await l2Provider.getTransactionReceipt(l2DeployTxHash)
 
-  const l2ToL1EventId = id('L2ToL1Transaction(address,address,uint256,uint256,uint256,uint256,uint256,uint256,uint256,bytes)')
+  const l2ToL1EventId = id(
+    'L2ToL1Transaction(address,address,uint256,uint256,uint256,uint256,uint256,uint256,uint256,bytes)'
+  )
   const logs = l2TxReceipt.logs.filter(log => log.topics[0] === l2ToL1EventId)
-  
-  if(logs.length !== 1) throw new Error("Not exactly 1 log emitted of L2 to L1 tx?")
+
+  if (logs.length !== 1)
+    throw new Error('Not exactly 1 log emitted of L2 to L1 tx?')
 
   const abi = [
     `event L2ToL1Transaction(address caller, address indexed destination, uint indexed uniqueId,
       uint indexed batchNumber, uint indexInBatch, uint arbBlockNum,
-      uint ethBlockNum, uint timestamp, uint callvalue, bytes data)`
-  ];
+      uint ethBlockNum, uint timestamp, uint callvalue, bytes data)`,
+  ]
   const iface = new ethers.utils.Interface(abi)
 
   const {
-     caller,
-     destination,
-     uniqueId,
-     batchNumber,
-     indexInBatch,
-     arbBlockNum,
-     ethBlockNum,
-     timestamp,
-     callvalue,
-     data,
+    caller,
+    destination,
+    uniqueId,
+    batchNumber,
+    indexInBatch,
+    arbBlockNum,
+    ethBlockNum,
+    timestamp,
+    callvalue,
+    data,
   } = iface.parseLog(logs[0]).args
+
+  const wait = (ms: number) => new Promise(res => setTimeout(res, ms))
+
+  const getProof = async (): Promise<{
+    proof: Array<string>
+    path: BigNumber
+    l2Sender: string
+    l1Dest: string
+    l2Block: BigNumber
+    l1Block: BigNumber
+    timestamp: BigNumber
+    amount: BigNumber
+    calldataForL1: string
+  }> => {
+    const nodeInterfaceAddress = '0x00000000000000000000000000000000000000C8'
+    const foo = new ethers.utils.Interface([
+      `function lookupMessageBatchProof(uint256 batchNum, uint64 index)
+          external
+          view
+          returns (
+              bytes32[] memory proof,
+              uint256 path,
+              address l2Sender,
+              address l1Dest,
+              uint256 l2Block,
+              uint256 l1Block,
+              uint256 timestamp,
+              uint256 amount,
+              bytes memory calldataForL1
+          )`,
+    ])
+    const nodeInterface = new ethers.Contract(
+      nodeInterfaceAddress,
+      foo
+    ).connect(l2Provider)
+    try {
+      const res = await nodeInterface.callStatic.lookupMessageBatchProof(
+        batchNumber,
+        indexInBatch
+      )
+      return res
+    } catch (e) {
+      const expectedError = "batch doesn't exist"
+      if (e.error.message === expectedError) {
+        console.log(
+          'Withdrawal detected, but batch not created yet. Going to wait a bit.'
+        )
+        await wait(1000)
+      } else {
+        console.log("Withdrawal proof didn't work. Not sure why")
+        console.log(e)
+        console.log('Going to try again after waiting')
+        await wait(1000)
+      }
+      console.log('New attempt starting')
+      return getProof()
+    }
+  }
+
+  const {
+    proof,
+    path,
+    l2Sender,
+    l1Dest,
+    l2Block,
+    l1Block,
+    timestamp: proofTimestamp,
+    amount,
+    calldataForL1,
+  } = await getProof()
   
-  const nodeInterfaceAddress = "0x00000000000000000000000000000000000000C8"
-  const foo = new ethers.utils.Interface([
-    `function lookupMessageBatchProof(uint256 batchNum, uint64 index)
-        external
-        view
-        returns (
-            bytes32[] memory proof,
-            uint256 path,
-            address l2Sender,
-            address l1Dest,
-            uint256 l2Block,
-            uint256 l1Block,
-            uint256 timestamp,
-            uint256 amount,
-            bytes memory calldataForL1
-        )`
-  ])
-
-  const nodeInterface = new ethers.Contract(nodeInterfaceAddress, foo).connect(l2Provider);
-
-  console.log("pre res")
-  const res = await nodeInterface.callStatic.lookupMessageBatchProof(batchNumber, indexInBatch)
-  console.log("res")
-  console.log(res)
-  // console.log(caller)
-  // console.log(destination)
-  // console.log("batchNumber")
-  // console.log(batchNumber)
-  // console.log("indexInBatch")
-  // console.log(indexInBatch)
-
+  
 
   // TODO: check if L2 counterpart worked
 }
