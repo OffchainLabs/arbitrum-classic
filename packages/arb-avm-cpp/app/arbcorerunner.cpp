d39f77e535a52659d9e92d581b6779fdd4b3c159
Harry Kalodner <harry@offchainlabs.com>
2021-03-17 23:36:01 -0400

    Beginning of ArbStore analysis utility

---
+++ packages/arb-avm-cpp/app/arbcorerunner.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021, Offchain Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <data_storage/arbstorage.hpp>
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    using namespace std::chrono_literals;
+    if (argc < 3) {
+        std::cout << "Usage: \narbcore_runner dbpath arbospath\n";
+        return 1;
+    }
+    auto dbpath = std::string(argv[1]);
+    auto arbospath = std::string(argv[2]);
+    ArbStorage storage{dbpath};
+    auto status = storage.initialize(arbospath);
+    if (!status.ok()) {
+        std::cerr << "Failed to get initialize storage" << status.ToString()
+                  << std::endl;
+        return -1;
+    }
+    auto core = storage.getArbCore();
+    auto log_count = core->logInsertedCount();
+    if (!log_count.status.ok()) {
+        std::cerr << "Failed to get log count " << log_count.status.ToString()
+                  << std::endl;
+        return -1;
+    }
+    std::cout << "Log count: " << log_count.data << "\n";
+    return 0;
+}
