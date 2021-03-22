09962f2265f11984c90bd15846a004ae738cf2aa
dzgoldman <dzgoldman@wesleyan.edu>
2021-03-22 15:03:46 -0400

    various arb-ts update for token bridge (event data, etc)

--- packages/arb-ts/src/index.ts
+++ packages/arb-ts/src/index.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-export { Bridge } from './lib/bridge'
+export { Bridge, L2ToL1EventResult } from './lib/bridge'
 export { L1Bridge, L1TokenData } from './lib/l1Bridge'
 export { L2Bridge, L2TokenData } from './lib/l2Bridge'
 export { ArbConversion } from './lib/conversion'
