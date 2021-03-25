f433fd17976b0492582247fb030d13625bd92a06
Harry Kalodner <harry@offchainlabs.com>
2021-03-25 16:31:48 -0400

    Use proxies for l2 deployment

--- packages/arb-bridge-peripherals/scripts/deploy_token_bridge_l2.ts
+++ packages/arb-bridge-peripherals/scripts/deploy_token_bridge_l2.ts
@@ -3,23 +3,39 @@ import { deploy1820Registry } from './utils'
 import { writeFileSync } from 'fs'
 import deployments from '../deployment.json'
 
-
 const main = async () => {
   const accounts = await ethers.getSigners()
-  const StandardArbERC20 = await ethers.getContractFactory(
-    'StandardArbERC20'
+  const StandardArbERC20 = await ethers.getContractFactory('StandardArbERC20')
+  const StandardArbERC777 = await ethers.getContractFactory('StandardArbERC777')
+  const TransparentUpgradeableProxy = await ethers.getContractFactory(
+    'TransparentUpgradeableProxy'
   )
-  const StandardArbERC777 = await ethers.getContractFactory(
-    'StandardArbERC777'
-  )
+  const ProxyAdmin = await ethers.getContractFactory('ProxyAdmin')
+
   const standardArbERC20 = await StandardArbERC20.deploy()
   const standardArbERC777 = await StandardArbERC777.deploy()
+  const proxyAdmin = await ProxyAdmin.deploy()
+  const arbERC20Proxy = await TransparentUpgradeableProxy.deploy(
+    standardArbERC20.address,
+    proxyAdmin.address,
+    '0x'
+  )
+  const arbERC777Proxy = await TransparentUpgradeableProxy.deploy(
+    standardArbERC777.address,
+    proxyAdmin.address,
+    '0x'
+  )
 
   const contracts = JSON.stringify({
     ...deployments,
-    standardArbERC20: standardArbERC20.address,
-    standardArbERC777: standardArbERC777.address,
-    l2ChainId: ethers.BigNumber.from(ethers.provider.network.chainId).toHexString(),
+    standardArbERC20: arbERC20Proxy.address,
+    standardArbERC777: arbERC777Proxy.address,
+    standardArbERC20Raw: standardArbERC20.address,
+    standardArbERC777Raw: standardArbERC777.address,
+    proxyAdmin: proxyAdmin.address,
+    l2ChainId: ethers.BigNumber.from(
+      ethers.provider.network.chainId
+    ).toHexString(),
   })
 
   const path = './deployment.json'
@@ -27,7 +43,7 @@ const main = async () => {
 
   // TODO: should append/check if previous entries
   writeFileSync(path, contracts)
-  
+
   console.log('Almost done')
   await deploy1820Registry(accounts[0])
   console.log('Done')
