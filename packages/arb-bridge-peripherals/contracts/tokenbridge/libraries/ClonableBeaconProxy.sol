77a999841df28dac8f74d048af493164fdc1a5c6
fred <fredlacs@gmail.com>
2021-03-27 12:02:19 +0000

    Refactor to push beacon address in constructor as a parameter

--- packages/arb-bridge-peripherals/contracts/tokenbridge/libraries/ClonableBeaconProxy.sol
+++ packages/arb-bridge-peripherals/contracts/tokenbridge/libraries/ClonableBeaconProxy.sol
@@ -4,10 +4,6 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/proxy/BeaconProxy.sol";
 
-interface ProxySetter {
-    function getBeacon() external view returns (address);
-}
-
 contract ClonableBeaconProxy is BeaconProxy {
-    constructor() public BeaconProxy(ProxySetter(msg.sender).getBeacon(), "") {}
+    constructor(address beacon) public BeaconProxy(beacon, "") {}
 }
