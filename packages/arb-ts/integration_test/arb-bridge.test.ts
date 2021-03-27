c64acde9c1c5ffbc6ffd998dace09716475e94b6
dzgoldman <dzgoldman@wesleyan.edu>
2021-03-27 13:53:34 -0400

    integration tests; nicen-up scripting, clean-up, add more tests

--- packages/arb-ts/integration_test/arb-bridge.test.ts
+++ packages/arb-ts/integration_test/arb-bridge.test.ts
@@ -10,6 +10,47 @@ import { StandardArbERC20__factory } from '../src/lib/abi/factories/StandardArbE
 import { StandardArbERC777__factory } from '../src/lib/abi/factories/StandardArbERC777__factory'
 
 import { EthERC20Bridge } from '../src/lib/abi/EthERC20Bridge'
+import yargs from 'yargs/yargs'
+import chalk from 'chalk'
+
+console.log()
+console.log(chalk.green(`Starting Token Bridge Integrations Tests!`))
+console.log()
+
+const prettyLog = (text: string) => {
+  console.log(chalk.blue(`*** ${text}`))
+  console.log()
+}
+
+const warn = (text: string) => {
+  console.log(chalk.red(`WARNING: ${text}`))
+  console.log()
+}
+const argv = yargs(process.argv.slice(2)).argv
+
+const network = ((_network?: string | number) => {
+  if (!_network) {
+    prettyLog('Using default network: kovan4')
+    return 'kovan4'
+  } else if (!Object.keys(config).includes(_network.toString())) {
+    warn(
+      `Network ${_network} not suppored; supported networks: ${Object.keys(
+        config
+      ).join(',')}`
+    )
+    process.exit(1)
+    return
+  } else {
+    prettyLog('Using network ' + _network)
+    return _network
+  }
+})(argv.network as string | number | undefined)
+
+if (network === 'kovan4' && !process.env.INFURA_KEY) {
+  warn('To use kovan, set env var INFURA_KEY')
+  process.exit(1)
+}
+
 const { parseEther } = utils
 const {
   ethRPC,
@@ -34,9 +75,14 @@ const wait = (ms = 10000) => new Promise(res => setTimeout(res, ms))
 
 const depositAmount = '0.01'
 let erc20Address = existantTestERC20
+if (erc20Address) {
+  prettyLog('Using token already deployed at l1 address: ' + erc20Address)
+} else {
+  prettyLog(`No L1 token address provided; we'll do it live!`)
+}
 
-console.info('preFundedWallet', preFundedWallet.address)
-console.info('test wallet', l1TestWallet.address)
+prettyLog('Using preFundedWallet: ' + preFundedWallet.address)
+prettyLog('Randomly generated test wallet: ' + l1TestWallet.address)
 
 const bridge = new Bridge(
   erc20BridgeAddress,
@@ -46,6 +92,18 @@ const bridge = new Bridge(
 )
 
 describe('setup', () => {
+  it('prefunded wallet is prefunded', async () => {
+    const balance = await preFundedWallet.getBalance()
+    const hasBalance = balance.gt(utils.parseEther(depositAmount))
+    expect(hasBalance).to.be.true
+    if (!hasBalance) {
+      warn(
+        preFundedWallet.address +
+          ' not prefunded; set a funded wallet via env-var DEVNET_PRIVKEY. exiting.'
+      )
+      process.exit()
+    }
+  })
   it('fund l1 test wallet with eth', async () => {
     const res = await preFundedWallet.sendTransaction({
       to: l1TestWallet.address,
@@ -57,37 +115,61 @@ describe('setup', () => {
   })
 })
 
-describe('deposit ether', () => {
+describe('Ether', () => {
   let testWalletL1EthBalance: BigNumber
   let testWalletL2EthBalance: BigNumber
 
-  it('has expected intial values', async () => {
+  it('has expected initial values', async () => {
     testWalletL1EthBalance = await bridge.getAndUpdateL1EthBalance()
     testWalletL2EthBalance = await bridge.getAndUpdateL2EthBalance()
     expect(testWalletL1EthBalance.eq(parseEther(depositAmount))).to.be.true
     expect(testWalletL2EthBalance.eq(constants.Zero)).to.be.true
   })
 
-  it('deposits ether', async () => {
-    const ethToL2DepositAmount = parseEther('0.0001')
+  const ethToL2DepositAmount = parseEther('0.0001')
+  const ethFromL2WithdrawAmmount = parseEther('0.00001')
+
+  it('deposit ether transaction succeeds', async () => {
     const res = await bridge.depositETH(ethToL2DepositAmount)
     const rec = await res.wait()
 
     expect(rec.status).to.equal(1)
+  })
+  it('L2 address has expected balance after deposit eth', async () => {
     await wait()
     testWalletL2EthBalance = await bridge.getAndUpdateL2EthBalance()
     expect(testWalletL2EthBalance.eq(ethToL2DepositAmount)).to.be.true
   })
-})
+  it('withdraw Ether transaction succeeds and emits event', async () => {
+    const withdrawEthRes = await bridge.withdrawETH(ethFromL2WithdrawAmmount)
+    const withdrawEthRec = await withdrawEthRes.wait()
 
+    expect(withdrawEthRec.status).to.equal(1)
+    const withdrawEventData = (
+      await bridge.getWithdrawalsInL2Transaction(withdrawEthRec)
+    )[0]
+    expect(withdrawEventData).to.exist
+
+    const { indexInBatch, batchNumber } = withdrawEventData
+    await wait()
+    const proof = await bridge.tryGetProof(indexInBatch, batchNumber)
+    expect(proof).to.exist
+  })
+
+  it('balance deducted after withdraw eth', async () => {
+    wait()
+    const bal = await bridge.getAndUpdateL2EthBalance()
+    // lazy check, will update once gasPrice is activated
+    expect(bal.lt(ethToL2DepositAmount)).to.be.true
+  })
+})
 const tokenDepositAmmount = BigNumber.from(50)
 
 const tokenDepositAmountE18 = utils.parseUnits('50', 18)
-const tokenWithdrawAmount = BigNumber.from(2)
 const tokenWithdrawAmountE18 = utils.parseUnits('2', 18)
 
 describe('ERC20', () => {
-  it('create l1 erc20 w initial supply', async () => {
+  it('create/ensure l1 erc20 w initial supply', async () => {
     const testTokenFactory = await new TestERC20__factory(preFundedWallet)
     const testToken = erc20Address
       ? await testTokenFactory.attach(erc20Address)
@@ -97,7 +179,6 @@ describe('ERC20', () => {
     expect(bal.gt(BigNumber.from(40000000))).to.be.true
 
     erc20Address = testToken.address
-    console.info('Using token at l1 address', erc20Address)
     const res = await testToken.transfer(
       l1TestWallet.address,
       BigNumber.from(200)
@@ -118,7 +199,7 @@ describe('ERC20', () => {
     expect(allowed).to.be.true
   })
 
-  it('initial erc20 deposit works', async () => {
+  it('initial erc20 deposit txns — L1 and L2 — both succeed', async () => {
     const despositRes = await bridge.depositAsERC20(
       erc20Address,
       tokenDepositAmmount,
@@ -129,6 +210,7 @@ describe('ERC20', () => {
     )
 
     const depositRec = await despositRes.wait()
+    await wait()
 
     const tokenDepositData = (
       await bridge.getDepositTokenEventData(depositRec, 'ERC20')
@@ -145,7 +227,9 @@ describe('ERC20', () => {
 
     expect(depositRec.status === 1).to.be.true
     expect(retriableReceipt.status === 1).to.be.true
+  })
 
+  it('L2 wallet has expected balance after erc20 deposit', async () => {
     const l2Data = await bridge.getAndUpdateL2TokenData(erc20Address)
 
     const testWalletL2Balance = l2Data && l2Data.ERC20 && l2Data.ERC20.balance
@@ -154,7 +238,7 @@ describe('ERC20', () => {
       .to.be.true
   })
 
-  it('erc20 is properly deployed in L2', async () => {
+  it('erc20 contract is properly deployed in L2', async () => {
     const erc20L2Address = await bridge.getERC20L2Address(erc20Address)
     const arbERC20 = StandardArbERC20__factory.connect(
       erc20L2Address,
@@ -167,7 +251,9 @@ describe('ERC20', () => {
     expect(balance.eq(tokenDepositAmmount)).to.be.true
   })
 
-  it('withdraw erc20', async () => {
+  const tokenWithdrawAmount = BigNumber.from(2)
+
+  it('withdraw erc20 succeeds and emits event data', async () => {
     const withdrawRes = await bridge.withdrawERC20(
       erc20Address,
       tokenWithdrawAmount
@@ -179,6 +265,13 @@ describe('ERC20', () => {
     )[0]
 
     expect(withdrawEventData).to.exist
+    const { indexInBatch, batchNumber } = withdrawEventData
+    await wait()
+    const proof = await bridge.tryGetProof(indexInBatch, batchNumber)
+    expect(proof).to.exist
+  })
+
+  it('balance properly deducted after erc20 withdraw', async () => {
     await wait()
     const l2Data = await bridge.getAndUpdateL2TokenData(erc20Address)
     const testWalletL2Balance = l2Data && l2Data.ERC20 && l2Data.ERC20.balance
