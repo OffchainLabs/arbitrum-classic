015f79ffa47830336e65a6ddc5795ce58b0a1379
fred <fredlacs@gmail.com>
2021-04-13 14:05:37 +0100

    Remove syscalls from bridge

--- packages/arb-bridge-eth/contracts/bridge/OutboxEntry.sol
+++ packages/arb-bridge-eth/contracts/bridge/OutboxEntry.sol
@@ -20,46 +20,34 @@ pragma solidity ^0.6.11;
 
 import "../libraries/Cloneable.sol";
 
-import "./interfaces/IOutbox.sol";
-import "./interfaces/IBridge.sol";
-
 contract OutboxEntry is Cloneable {
-    IBridge bridge;
+    address outbox;
     bytes32 public root;
     uint256 public numRemaining;
     mapping(bytes32 => bool) public spentOutput;
 
-    function initialize(
-        IBridge _bridge,
-        bytes32 _root,
-        uint256 _numInBatch
-    ) external {
+    function initialize(bytes32 _root, uint256 _numInBatch) external {
+        require(outbox == address(0), "ALREADY_INIT");
         require(root == 0, "ALREADY_INIT");
         require(_root != 0, "BAD_ROOT");
-        bridge = _bridge;
+        outbox = msg.sender;
         root = _root;
         numRemaining = _numInBatch;
     }
 
-    function spendOutput(bytes32 _root, bytes32 _id) external {
-        requireBridgeSystemCall();
+    function spendOutput(bytes32 _root, bytes32 _id) external returns (uint256) {
+        require(msg.sender == outbox, "NOT_FROM_OUTBOX");
         require(!spentOutput[_id], "ALREADY_SPENT");
         require(_root == root, "BAD_ROOT");
+
         spentOutput[_id] = true;
         numRemaining--;
-        if (numRemaining == 0) {
-            safeSelfDestruct(msg.sender);
-        }
+
+        return numRemaining;
     }
 
     function destroy() external {
-        requireBridgeSystemCall();
+        require(msg.sender == outbox, "NOT_FROM_OUTBOX");
         safeSelfDestruct(msg.sender);
-    }
-
-    function requireBridgeSystemCall() private view {
-        require(msg.sender == address(bridge), "ONLY_BRIDGE");
-        // Make sure this call was generated as a system call from the bridge rather than an L2 call
-        require(IOutbox(bridge.activeOutbox()).l2ToL1Sender() == address(0), "ONLY_SYSTEM");
     }
 }
