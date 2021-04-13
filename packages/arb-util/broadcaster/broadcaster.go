d1d669addcf03f690fca4d3a0d8058b5c0ba1d71
Joshua Colvin <jcolvin@offchainlabs.com>
2021-04-13 13:07:32 -0700

    Normalize logger calls, cleanup poller

--- packages/arb-util/broadcaster/broadcaster.go
+++ packages/arb-util/broadcaster/broadcaster.go
@@ -28,7 +28,7 @@ type Broadcaster struct {
 	startBroadcasterMutex *sync.Mutex
 	broadcasterStarted    bool
 	settings              Settings
-	poller                *netpoll.Poller
+	poller                netpoll.Poller
 	acceptDesc            *netpoll.Desc
 }
 
@@ -47,19 +47,17 @@ func (b *Broadcaster) Start() error {
 		return nil
 	}
 
-	connectionPoller, err := netpoll.New(nil)
-	b.poller = &connectionPoller
+	var err error
+	b.poller, err = netpoll.New(nil)
 	if err != nil {
 		logger.Error().Err(err).Msg("unable to initialize netpoll for monitoring client connection events")
 		return err
 	}
 
-	var (
-		// Make pool of X size, Y sized work queue and one pre-spawned
-		// goroutine.
-		pool          = gopool.NewPool(b.settings.Workers, b.settings.Queue, 1)
-		clientManager = NewClientManager(pool)
-	)
+	// Make pool of X size, Y sized work queue and one pre-spawned
+	// goroutine.
+	var pool          = gopool.NewPool(b.settings.Workers, b.settings.Queue, 1)
+	var clientManager = NewClientManager(pool)
 
 	b.clientManager = clientManager // maintain the pointer in this instance... used for testing
 
@@ -68,44 +66,51 @@ func (b *Broadcaster) Start() error {
 	// it and stores it as a Client connection in ClientManager instance.
 	//
 	// Called below in accept() loop.
-	handle := func(conn net.Conn) {
+	handle := func(unsafeConn net.Conn) {
 
-		safeConn := deadliner{conn, b.settings.IoTimeout}
+		safeConn := deadliner{unsafeConn, b.settings.IoTimeout}
 
 		// Zero-copy upgrade to WebSocket connection.
 		hs, err := ws.Upgrade(safeConn)
 		if err != nil {
-			logger.Warn().Err(err).Str("connection_name", nameConn(conn)).Msg("upgrade error")
-			_ = conn.Close()
+			logger.Warn().Err(err).Str("connection_name", nameConn(safeConn)).Msg("upgrade error")
+			_ = safeConn.Close()
 			return
 		}
 
-		logger.Info().Msgf("%s: established websocket connection: %+v", nameConn(conn), hs)
+		logger.Info().Msgf("%s: established websocket connection: %+v", nameConn(safeConn), hs)
 
 		// Register incoming client in clientManager.
 		client := clientManager.Register(safeConn)
 
+		// TODO: Does safeConn need to be used here?
 		// Create netpoll event descriptor to handle only read events.
-		desc := netpoll.Must(netpoll.HandleRead(conn))
+		desc := netpoll.Must(netpoll.HandleRead(unsafeConn))
 
 		// Subscribe to events about conn.
-		err = connectionPoller.Start(desc, func(ev netpoll.Event) {
+		err = b.poller.Start(desc, func(ev netpoll.Event) {
 			if ev&(netpoll.EventReadHup|netpoll.EventHup) != 0 {
 				// ReadHup or Hup received, means the client has close the connection
 				// remove it from the clientManager registry.
-				err = connectionPoller.Stop(desc)
-				if err != nil {
-					logger.Warn().Err(err).Msg("error stopping poller")
-				}
+				logger.Info().Str("connection_name", nameConn(safeConn)).Msg("Hup received")
+				_ = b.poller.Stop(desc)
+				_ = desc.Close()
 				clientManager.Remove(client)
 				return
+			}
+
+			if ev > 1 {
+				logger.Info().Str("connection_name", nameConn(safeConn)).Int("event", int(ev)).Msg("event greater than 1 received")
 			}
 
 			// receive client messages, close on error
 			pool.Schedule(func() {
 				if err := client.Receive(); err != nil {
-					_ = connectionPoller.Stop(desc)
+					logger.Warn().Err(err).Str("connection_name", nameConn(safeConn)).Msg("receive error")
+					_ = b.poller.Stop(desc)
+					_ = desc.Close()
 					clientManager.Remove(client)
+					return
 				}
 			})
 		})
@@ -136,7 +141,7 @@ func (b *Broadcaster) Start() error {
 	accept := make(chan error, 1)
 
 	// Subscribe to events about listener.
-	err = connectionPoller.Start(acceptDesc, func(e netpoll.Event) {
+	err = b.poller.Start(acceptDesc, func(e netpoll.Event) {
 		// We do not want to accept incoming connection when goroutine pool is
 		// busy. So if there are no free goroutines during 1ms we want to
 		// cooldown the server and do not receive connection for some short
@@ -170,7 +175,7 @@ func (b *Broadcaster) Start() error {
 			time.Sleep(delay)
 		}
 
-		err = connectionPoller.Resume(acceptDesc)
+		err = b.poller.Resume(acceptDesc)
 		if err != nil {
 			logger.Warn().Err(err).Msg("error in poller.Start")
 		}
@@ -189,7 +194,7 @@ func (b *Broadcaster) Broadcast(messages []*inbox.InboxMessage) error {
 }
 
 func (b *Broadcaster) Stop() {
-	_ = (*b.poller).Stop(b.acceptDesc)
+	_ = b.poller.Stop(b.acceptDesc)
 	_ = b.acceptDesc.Close()
 }
 
