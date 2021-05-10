baburgess/arbitrum

Continue integrating broadcaster
- Finished integrating broadcaster feed
- Started integrating broadcastclient into nodes

Todo
- FInish integrating broadcast client
- Update broadcastmessage to hold correct objects
- Add configuration options so nodes can specify the sequencer URL as well as public key for validating sequencer messages
- Add signature checking to broadcastclient

packages/arb-node-core/cmd/arb-validator/arb-validator.go
@@ -44,6 +44,7 @@ import (
 	"github.com/offchainlabs/arbitrum/packages/arb-node-core/monitor"
 	"github.com/offchainlabs/arbitrum/packages/arb-node-core/nodehealth"
 	"github.com/offchainlabs/arbitrum/packages/arb-node-core/staker"
+	"github.com/offchainlabs/arbitrum/packages/arb-util/broadcastclient"
 	"github.com/offchainlabs/arbitrum/packages/arb-util/common"
 )
 
@@ -80,6 +81,12 @@ func main() {
 			log.Error().Err(err).Msg("healthcheck server failed")
 		}
 	}()
+
+	broadcastClient := broadcastclient.NewBroadcastClient("ws://127.0.0.1:9742/", nil)
+	sequencerFeed, err := broadcastClient.Connect()
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to start broadcastclient")
+	}
 
 	if len(os.Args) < 2 {
 		usageStr := "Usage: arb-validator [folder] [RPC URL] [rollup address] [validator utils address] [strategy] " + cmdhelp.WalletArgsString
@@ -233,7 +240,7 @@ func main() {
 		logger.Fatal().Str("chain", hex.EncodeToString(chainMachineHash[:])).Str("arbCore", hex.EncodeToString(initialMachineHash[:])).Msg("Initial machine hash loaded from arbos.mexe doesn't match chain's initial machine hash")
 	}
 
-	reader, err := mon.StartInboxReader(ctx, client, common.NewAddressFromEth(rollupAddr), healthChan)
+	reader, err := mon.StartInboxReader(ctx, client, common.NewAddressFromEth(rollupAddr), healthChan, sequencerFeed)
 	if err != nil {
 		logger.Fatal().Err(err).Msg("Failed to create inbox reader")
 	}

packages/arb-node-core/staker/staker_test.go
@@ -220,7 +220,10 @@ func runStakersTest(t *testing.T, faultConfig challenge.FaultConfig, maxGasPerNo
 		}
 	}()
 
-	reader, err := mon.StartInboxReader(ctx, client, common.NewAddressFromEth(rollupAddr), healthChan)
+	// Make a dummy feed for now
+	var sequencerFeed chan monitor.SequencerFeedItem
+
+	reader, err := mon.StartInboxReader(ctx, client, common.NewAddressFromEth(rollupAddr), healthChan, sequencerFeed)
 	test.FailIfError(t, err)
 	defer reader.Stop()
 

OffchainLabs/arbitrum-classic

packages/arb-node-core/cmd/arb-validator/arb-validator.go
@@ -44,6 +44,7 @@ import (
 	"github.com/offchainlabs/arbitrum/packages/arb-node-core/monitor"
 	"github.com/offchainlabs/arbitrum/packages/arb-node-core/nodehealth"
 	"github.com/offchainlabs/arbitrum/packages/arb-node-core/staker"
+	"github.com/offchainlabs/arbitrum/packages/arb-util/broadcastclient"
 	"github.com/offchainlabs/arbitrum/packages/arb-util/common"
 )
 
@@ -90,6 +91,12 @@ func startup() error {
 			log.Error().Err(err).Msg("healthcheck server failed")
 		}
 	}()
+
+	broadcastClient := broadcastclient.NewBroadcastClient("ws://127.0.0.1:9742/", nil)
+	sequencerFeed, err := broadcastClient.Connect()
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to start broadcastclient")
+	}
 
 	if len(os.Args) < 2 {
 		usageStr := "Usage: arb-validator [folder] [RPC URL] [rollup address] [validator utils address] [strategy] " + cmdhelp.WalletArgsString
@@ -246,7 +253,7 @@ func startup() error {
 		return errors.Errorf("Initial machine hash loaded from arbos.mexe doesn't match chain's initial machine hash: chain %v, arbCore %v", hexutil.Encode(chainMachineHash[:]), initialMachineHash)
 	}
 
-	_, err = mon.StartInboxReader(ctx, client, common.NewAddressFromEth(rollupAddr), healthChan)
+	_, err := mon.StartInboxReader(ctx, client, common.NewAddressFromEth(rollupAddr), healthChan, sequencerFeed)
 	if err != nil {
 		return errors.Wrap(err, "failed to create inbox reader")
 	}

packages/arb-node-core/staker/staker_test.go
@@ -266,7 +266,10 @@ func runStakersTest(t *testing.T, faultConfig challenge.FaultConfig, maxGasPerNo
 		nodehealth.StartNodeHealthCheck(ctx, healthChan)
 	}()
 
-	_, err = mon.StartInboxReader(ctx, client, common.NewAddressFromEth(rollupAddr), healthChan)
+	// Make a dummy feed for now
+	var sequencerFeed chan monitor.SequencerFeedItem
+
+	_, err := mon.StartInboxReader(ctx, client, common.NewAddressFromEth(rollupAddr), healthChan, sequencerFeed)
 	test.FailIfError(t, err)
 
 	for i := 1; i <= 10; i++ {