df8b9bff56bb858f799a988cdd6ee9aeabf722b1
Joshua Colvin <jcolvin@offchainlabs.com>
2021-05-20 23:53:41 -0700

    Update rocksdb settings to reduce memory usage

--- packages/arb-avm-cpp/data_storage/src/datastorage.cpp
+++ packages/arb-avm-cpp/data_storage/src/datastorage.cpp
@@ -18,26 +18,25 @@
 
 #include <rocksdb/convenience.h>
 #include <rocksdb/filter_policy.h>
-#include <avm_values/value.hpp>
 #include <data_storage/storageresult.hpp>
-#include <data_storage/value/utils.hpp>
 
+#include <iostream>
 #include <string>
 
 DataStorage::DataStorage(const std::string& db_path) {
-    rocksdb::TransactionDBOptions txn_options;
-    rocksdb::Options options = rocksdb::Options();
-    rocksdb::ColumnFamilyOptions cf_options;
-    rocksdb::ColumnFamilyOptions small_cf_options;
-    rocksdb::ColumnFamilyOptions hashkey_cf_options;
-    rocksdb::BlockBasedTableOptions table_options;
-    rocksdb::BlockBasedTableOptions bloom_table_options;
+    rocksdb::TransactionDBOptions txn_options{};
+    rocksdb::Options options{};
+    rocksdb::ColumnFamilyOptions cf_options{};
+    rocksdb::ColumnFamilyOptions small_cf_options{};
+    rocksdb::ColumnFamilyOptions hashkey_cf_options{};
+    rocksdb::BlockBasedTableOptions table_options{};
     options.create_if_missing = true;
     options.create_missing_column_families = true;
 
+    cf_options.target_file_size_multiplier = 10;
+
     // As recommended for new applications by
     // https://github.com/facebook/rocksdb/wiki/Setup-Options-and-Basic-Tuning
-    /*
     cf_options.level_compaction_dynamic_level_bytes = true;
     options.max_background_compactions = 4;
     options.max_background_flushes = 2;
@@ -48,49 +47,20 @@ DataStorage::DataStorage(const std::string& db_path) {
     table_options.pin_l0_filter_and_index_blocks_in_cache = true;
     options.table_factory.reset(
         rocksdb::NewBlockBasedTableFactory(table_options));
-    */
-
     table_options.format_version = 4;
 
     // No need to keep old log files
-    options.keep_log_file_num = 1;
-
-    // Increase the number of threads to open files to offset slow disk access
-    options.max_file_opening_threads = 200;
-
-    // Set WAL to a non-zero value so that old logs can be cleaned up
-    options.max_total_wal_size = 1024 * 1024 * 50;
-
-    // Various settings to constrain memory growth
-    /*
-    options.max_open_files = 512;
-    options.write_buffer_size = 1024 * 1024 * 8;
-    options.db_write_buffer_size = 1024 * 1024 * 64;
-
-    // No need to wait for manual flush to finish
-    flush_options.wait = false;
+    options.keep_log_file_num = 3;
 
     // Settings for small tables
     small_cf_options = cf_options;
     small_cf_options.num_levels = 2;
-    small_cf_options.write_buffer_size = 1024 * 1024 * 8;
     small_cf_options.OptimizeForSmallDb();
-    */
 
-    bloom_table_options = table_options;
-    // bloom_table_options.filter_policy.reset(
-    //     rocksdb::NewBloomFilterPolicy(10, false));
-    // bloom_table_options.optimize_filters_for_memory = true;
-
-    // Settings for refcounted data table using bloom filters and no iterators
+    // Settings for refcounted data table
     hashkey_cf_options = cf_options;
-    /*
-    hashkey_cf_options.write_buffer_size = 1024 * 1024 * 32;
     hashkey_cf_options.OptimizeForPointLookup(16);
     hashkey_cf_options.level_compaction_dynamic_level_bytes = true;
-    hashkey_cf_options.table_factory = std::unique_ptr<rocksdb::TableFactory>(
-        rocksdb::NewBlockBasedTableFactory(bloom_table_options));
-    */
 
     txn_db_path = db_path;
 
@@ -99,7 +69,7 @@ DataStorage::DataStorage(const std::string& db_path) {
     column_descriptors[DEFAULT_COLUMN] = {rocksdb::kDefaultColumnFamilyName,
                                           small_cf_options};
     column_descriptors[STATE_COLUMN] = {"states", small_cf_options};
-    column_descriptors[CHECKPOINT_COLUMN] = {"checkpoints", small_cf_options};
+    column_descriptors[CHECKPOINT_COLUMN] = {"checkpoints", cf_options};
     column_descriptors[DELAYEDMESSAGE_COLUMN] = {"delayedmessages", cf_options};
     column_descriptors[SEQUENCERBATCHITEM_COLUMN] = {"sequencerbatchitems",
                                                      cf_options};
@@ -107,7 +77,7 @@ DataStorage::DataStorage(const std::string& db_path) {
                                                  cf_options};
     column_descriptors[LOG_COLUMN] = {"logs", cf_options};
     column_descriptors[SEND_COLUMN] = {"sends", cf_options};
-    column_descriptors[SIDELOAD_COLUMN] = {"sideloads", small_cf_options};
+    column_descriptors[SIDELOAD_COLUMN] = {"sideloads", cf_options};
     column_descriptors[AGGREGATOR_COLUMN] = {"aggregator", cf_options};
     column_descriptors[REFCOUNTED_COLUMN] = {"refcounted", hashkey_cf_options};
 
