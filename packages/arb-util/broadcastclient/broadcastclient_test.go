a87eb822bb80c741793bad7af270f03d1cacf327
Joshua Colvin <jcolvin@offchainlabs.com>
2021-06-06 16:23:03 -0700

    Fix random broadcast test error
    Remove possible race conditions in broadcastclient unit test

--- packages/arb-util/broadcastclient/broadcastclient_test.go
+++ packages/arb-util/broadcastclient/broadcastclient_test.go
@@ -97,7 +97,7 @@ func TestServerDisconnectsAClientIfItDoesNotRespondToPings(t *testing.T) {
 	broadcastClient := NewBroadcastClient("ws://127.0.0.1:9743/", nil, 20*time.Second)
 
 	// connect returns
-	_, err = broadcastClient.Connect(ctx)
+	client, err := broadcastClient.Connect(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -106,19 +106,34 @@ func TestServerDisconnectsAClientIfItDoesNotRespondToPings(t *testing.T) {
 	hash1, feedItem1, signature1 := newBroadcastMessage()
 	err = b.BroadcastSingle(hash1, feedItem1.BatchItem, signature1.Bytes())
 
+	// Wait for client to receive batch to ensure it is connected
+	select {
+	case receivedMsg := <-client:
+		t.Logf("Received Message, Sequence Message: %v\n", receivedMsg.FeedItem.BatchItem.SequencerMessage)
+	case <-time.After(5 * time.Second):
+		t.Fatal("Client did not receive batch item")
+	}
+
 	connectionCount := b.ClientConnectionCount()
 	if connectionCount != 1 {
-		t.Errorf("Client Connection Count error %v\n", connectionCount)
+		t.Fatalf("Client Connection Count error %v\n", connectionCount)
 	}
 
 	broadcastClient.Close()
-	time.Sleep(3 * time.Second)
 
-	connectionCount = b.ClientConnectionCount()
-	if connectionCount != 0 {
-		t.Errorf("Client Connection Count error %v\n", connectionCount)
+	// Wait for client to be disconnected from server
+	disconnectTimeout := time.After(5 * time.Second)
+	for {
+		if b.ClientConnectionCount() == 0 {
+			break
+		}
+
+		select {
+		case <-disconnectTimeout:
+			t.Fatal("Client did not receive batch item")
+		case <-time.After(100 * time.Millisecond):
+		}
 	}
-
 }
 
 func TestBroadcastClientReconnectsOnServerDisconnect(t *testing.T) {
@@ -149,6 +164,8 @@ func TestBroadcastClientReconnectsOnServerDisconnect(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// Client set to timeout connection at 2 seconds, and server set to send ping every 50 seconds,
+	// so at least one timeout/reconnect should happen after 4 seconds
 	time.Sleep(4 * time.Second)
 
 	if broadcastClient.GetRetryCount() <= 0 {
