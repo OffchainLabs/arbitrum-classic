b706cc2b6b294a175814600475d92e99e6940cc3
dzgoldman <dzgoldman@wesleyan.edu>
2021-06-09 10:51:50 -0400

    make WETH name/symbol/decimals constants (so they're preserved)

--- packages/arb-bridge-peripherals/contracts/misc/WETH9.sol
+++ packages/arb-bridge-peripherals/contracts/misc/WETH9.sol
@@ -18,9 +18,9 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 contract WETH9 {
-    string public name = "Wrapped Ether";
-    string public symbol = "WETH";
-    uint8 public decimals = 18;
+    string public constant name = "Wrapped Ether";
+    string public constant symbol = "WETH";
+    uint8 public constant decimals = 18;
 
     event Approval(address indexed src, address indexed guy, uint256 wad);
     event Transfer(address indexed src, address indexed dst, uint256 wad);
