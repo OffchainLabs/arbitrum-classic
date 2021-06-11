88981254c0bd5f86f7237e69cc1157c702c11a5d
fred <fredlacs@gmail.com>
2021-06-11 15:32:57 +0100

    Add custom bridge admin funcitonality to set token pair

--- packages/arb-bridge-peripherals/contracts/tokenbridge/ethereum/gateway/L1CustomGateway.sol
+++ packages/arb-bridge-peripherals/contracts/tokenbridge/ethereum/gateway/L1CustomGateway.sol
@@ -30,15 +30,19 @@ contract L1CustomGateway is L1ArbitrumExtendedGateway {
     using Address for address;
     // stores addresses of L2 tokens to be used
     mapping(address => address) public l1ToL2Token;
+    // owner is able to force add custom mappings
+    address public owner;
 
     event TokenSet(address indexed l1Address, address indexed l2Address);
 
     function initialize(
         address _l1Counterpart,
         address _l1Router,
-        address _inbox
+        address _inbox,
+        address _owner
     ) public virtual {
         L1ArbitrumExtendedGateway._initialize(_l1Counterpart, _l1Router, _inbox);
+        owner = _owner;
     }
 
     /**
@@ -80,26 +84,67 @@ contract L1CustomGateway is L1ArbitrumExtendedGateway {
     /**
      * @notice Allows L1 Token contract to trustlessly register its custom L2 counterpart.
 
-     * @param l2Address address of L1 token
+     * @param _l2Address counterpart address of L1 token
      * @param _maxGas max gas for L2 retryable exrecution 
      * @param _gasPriceBid gas price for L2 retryable ticket 
      * @param  _maxSubmissionCost base submission cost  L2 retryable tick3et 
      * @return Retryable ticket ID
      */
     function registerTokenToL2(
-        address l2Address,
+        address _l2Address,
         uint256 _maxGas,
         uint256 _gasPriceBid,
         uint256 _maxSubmissionCost
     ) external virtual returns (uint256) {
         require(address(msg.sender).isContract(), "MUST_BE_CONTRACT");
-        l1ToL2Token[msg.sender] = l2Address;
+        l1ToL2Token[msg.sender] = _l2Address;
+
+        address[] memory l1Addresses = new address[](1);
+        address[] memory l2Addresses = new address[](1);
+        l1Addresses[0] = msg.sender;
+        l2Addresses[0] = _l2Address;
 
         bytes memory _data =
             abi.encodeWithSelector(
                 L2CustomGateway.registerTokenFromL1.selector,
-                msg.sender,
-                l2Address
+                l1Addresses,
+                l2Addresses
+            );
+
+        return sendTxToL2(msg.sender, 0, _maxSubmissionCost, _maxGas, _gasPriceBid, _data);
+    }
+
+    /**
+     * @notice Allows owner to force register a custom L1/L2 token pair.
+     * @dev _l1Addresses[i] counterpart is assumed to be _l2Addresses[i]
+     * @param _l1Addresses array of L1 addresses
+     * @param _l2Addresses array of L2 addresses
+     * @param _maxGas max gas for L2 retryable exrecution
+     * @param _gasPriceBid gas price for L2 retryable ticket
+     * @param  _maxSubmissionCost base submission cost  L2 retryable tick3et
+     * @return Retryable ticket ID
+     */
+    function forceRegisterTokenToL2(
+        address[] calldata _l1Addresses,
+        address[] calldata _l2Addresses,
+        uint256 _maxGas,
+        uint256 _gasPriceBid,
+        uint256 _maxSubmissionCost
+    ) external virtual returns (uint256) {
+        require(msg.sender == owner, "ONLY_OWNER");
+        require(_l1Addresses.length == _l2Addresses.length, "INVALID_LENGTHS");
+
+        for (uint256 i = 0; i < _l1Addresses.length; i++) {
+            // here we assume the owner checked both addresses offchain before force registering
+            // require(address(_l1Addresses[i]).isContract(), "MUST_BE_CONTRACT");
+            l1ToL2Token[_l1Addresses[i]] = _l2Addresses[i];
+        }
+
+        bytes memory _data =
+            abi.encodeWithSelector(
+                L2CustomGateway.registerTokenFromL1.selector,
+                _l1Addresses,
+                _l2Addresses
             );
 
         return sendTxToL2(msg.sender, 0, _maxSubmissionCost, _maxGas, _gasPriceBid, _data);
