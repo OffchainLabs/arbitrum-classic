c569b148d3949348a585cab9cd85900e2de841a7
Joshua Colvin <jcolvin@offchainlabs.com>
2021-06-15 18:13:25 -0700

    Add support for configuration file

--- packages/arb-node-core/cmdhelp/wallet.go
+++ packages/arb-node-core/cmdhelp/wallet.go
@@ -17,7 +17,6 @@
 package cmdhelp
 
 import (
-	"flag"
 	"fmt"
 	"math"
 	"math/big"
@@ -29,29 +28,13 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"golang.org/x/crypto/ssh/terminal"
+
+	"github.com/offchainlabs/arbitrum/packages/arb-util/configuration"
 )
 
 type WalletFlags struct {
 	passphrase *string
 	gasPrice   *float64
-}
-
-func AddWalletFlags(fs *flag.FlagSet) WalletFlags {
-	passphrase := fs.String(
-		"password",
-		"",
-		"password=pass",
-	)
-	gasPrice := fs.Float64(
-		"gasprice",
-		4.5,
-		"gasprice=FloatInGwei",
-	)
-
-	return WalletFlags{
-		passphrase: passphrase,
-		gasPrice:   gasPrice,
-	}
 }
 
 // GetKeystore returns a transaction authorization based on an existing ethereum
@@ -61,8 +44,7 @@ func AddWalletFlags(fs *flag.FlagSet) WalletFlags {
 // optional "gasprice" arguement.
 func GetKeystore(
 	validatorFolder string,
-	args WalletFlags,
-	flags *flag.FlagSet,
+	wallet *configuration.Wallet,
 	chainId *big.Int,
 ) (*bind.TransactOpts, func([]byte) ([]byte, error), error) {
 	ks := keystore.NewKeyStore(
@@ -71,15 +53,8 @@ func GetKeystore(
 		keystore.StandardScryptP,
 	)
 
-	found := false
-	flags.Visit(func(f *flag.Flag) {
-		if f.Name == "password" {
-			found = true
-		}
-	})
-
 	var passphrase string
-	if !found {
+	if len(wallet.Password) == 0 {
 		if len(ks.Accounts()) == 0 {
 			fmt.Print("Enter new account password: ")
 		} else {
@@ -94,7 +69,7 @@ func GetKeystore(
 
 		passphrase = strings.TrimSpace(passphrase)
 	} else {
-		passphrase = *args.passphrase
+		passphrase = wallet.Password
 	}
 
 	var account accounts.Account
@@ -116,7 +91,7 @@ func GetKeystore(
 		return nil, nil, err
 	}
 
-	gasPriceAsFloat := 1e9 * (*args.gasPrice)
+	gasPriceAsFloat := 1e9 * wallet.GasPrice
 	if gasPriceAsFloat < math.MaxInt64 {
 		auth.GasPrice = big.NewInt(int64(gasPriceAsFloat))
 	}
--- packages/arb-util/go.mod
+++ packages/arb-util/go.mod
@@ -8,11 +8,13 @@ require (
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.0.4
 	github.com/gobwas/ws-examples v0.0.0-20190625122829-a9e8908d9484
+	github.com/knadh/koanf v1.1.0
 	github.com/mailru/easygo v0.0.0-20190618140210-3c14a0dc985f
 	github.com/offchainlabs/arbitrum/packages/arb-node-core v0.8.0
 	github.com/offchainlabs/go-solidity-sha3 v0.1.2
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.21.0
+	github.com/spf13/pflag v1.0.5
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
 
