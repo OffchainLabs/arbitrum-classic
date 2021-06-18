70bc89868fe0dbb3d503fcd4c98ebf34fba418f5
dzgoldman <dzgoldman@wesleyan.edu>
2021-06-17 20:53:11 -0400

    remove test hash

--- packages/arb-ts/scripts/checkRetryableStatus.ts
+++ packages/arb-ts/scripts/checkRetryableStatus.ts
@@ -1,6 +1,5 @@
 import { checkRetryableStatus } from './lib'
-const l1Hash =
-  '0x7703437080b3f1b85b09ca00c5a64e51e3b715b525b20835a93fd050d73f27f4'
+const l1Hash = ''
 
 if (!l1Hash) {
   throw new Error('Need to set an L1 hash')
