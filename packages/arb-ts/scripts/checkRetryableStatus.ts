7f360af1bff3ba0c4f57fce37b9beccdcc368a42
dzgoldman <dzgoldman@wesleyan.edu>
2021-06-17 20:52:10 -0400

    remove old check retryable script, fix logging in new one

--- packages/arb-ts/scripts/checkRetryableStatus.ts
+++ packages/arb-ts/scripts/checkRetryableStatus.ts
@@ -1,5 +1,6 @@
 import { checkRetryableStatus } from './lib'
-const l1Hash = ''
+const l1Hash =
+  '0x7703437080b3f1b85b09ca00c5a64e51e3b715b525b20835a93fd050d73f27f4'
 
 if (!l1Hash) {
   throw new Error('Need to set an L1 hash')
