03aa084a45fa06362e6f447639ebba29a83a879e
dzgoldman <dzgoldman@wesleyan.edu>
2021-06-22 15:05:51 -0400

    use process codes for checkRetryableStatus

--- packages/arb-ts/scripts/checkRetryableStatus.ts
+++ packages/arb-ts/scripts/checkRetryableStatus.ts
@@ -5,6 +5,9 @@ if (!l1Hash) {
   throw new Error('Need to set an L1 hash')
 }
 
-checkRetryableStatus(l1Hash).then(() => {
-  console.log('done')
-})
+checkRetryableStatus(l1Hash)
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
