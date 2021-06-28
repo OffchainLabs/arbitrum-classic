bba192fd02765cce32fae5744f165f856bab2d61
fred <fredlacs@gmail.com>
2021-06-28 18:35:19 +0100

    Push upgraded deploy script and fix unit test

--- packages/arb-bridge-peripherals/scripts/token_bridge_upgrade.ts
+++ packages/arb-bridge-peripherals/scripts/token_bridge_upgrade.ts
@@ -7,22 +7,25 @@ import { networks } from 'arb-ts/src/lib/networks'
 import MainnetAddresses from '../deployment-42161.json'
 // import RinkebyAddresses from '../deployment-421611.json'
 
-const POST_UPGRADE_INIT = "0x95fcea78"
+const POST_UPGRADE_INIT = '0x95fcea78'
 
 const infuraKey = process.env['INFURA_KEY']
 if (!infuraKey) throw new Error('No INFURA_KEY')
 
-const l1Prov = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
-// const l1Prov = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/' + infuraKey)
-// const l2Prov = new ethers.providers.JsonRpcProvider('https://arb1.arbitrum.io/rpc')
+const l1Prov = new ethers.providers.JsonRpcProvider(
+  'https://mainnet.infura.io/v3/' + infuraKey
+)
+const l2Prov = new ethers.providers.JsonRpcProvider(
+  'https://arb1.arbitrum.io/rpc'
+)
 
 const l1PrivKey = process.env['L1_PRIVKEY']
 if (!l1PrivKey) throw new Error('No L1_PRIVKEY')
-// const l2PrivKey = process.env['L2_PRIVKEY']
-// if (!l2PrivKey) throw new Error('No L2_PRIVKEY')
+const l2PrivKey = process.env['L2_PRIVKEY']
+if (!l2PrivKey) throw new Error('No L2_PRIVKEY')
 
 const l1Wallet = ethers.Wallet.fromMnemonic(l1PrivKey)
-// const l2Wallet = ethers.Wallet.fromMnemonic(l2PrivKey)
+const l2Wallet = ethers.Wallet.fromMnemonic(l2PrivKey)
 
 const l1ProxyAdminAddr = MainnetAddresses.l1ProxyAdmin
 const l2ProxyAdminAddr = MainnetAddresses.l2ProxyAdmin
@@ -40,14 +43,13 @@ const l2ProxyAdminAddr = MainnetAddresses.l2ProxyAdmin
 // const l2Wallet = new ethers.Wallet(privKey)
 
 const l1Signer = l1Wallet.connect(l1Prov)
-// const l2Signer = l2Wallet.connect(l2Prov)
+const l2Signer = l2Wallet.connect(l2Prov)
 
 const wait = (ms: number) => new Promise(res => setTimeout(res, ms))
 
 const main = async () => {
-  // const l1Network = await l1Prov.getNetwork()
-  // const tokenbridge = networks[l1Network.chainId].tokenBridge
-  const tokenbridge = networks['1'].tokenBridge
+  const l1Network = await l1Prov.getNetwork()
+  const tokenbridge = networks[l1Network.chainId].tokenBridge
   const l1ERC20GatewayProxy = tokenbridge.l1ERC20Gateway
   const l2ERC20GatewayProxy = tokenbridge.l2ERC20Gateway
   const l1CustomGatewayProxy = tokenbridge.l1CustomGateway
@@ -55,9 +57,11 @@ const main = async () => {
 
   //   upgrade contracts
   // l1 validation
-  const ProxyAdmin__factory = await ethers.getContractFactory("ProxyAdmin")
+  const ProxyAdmin__factory = await ethers.getContractFactory('ProxyAdmin')
 
-  const l1ProxyAdmin = ProxyAdmin__factory.attach(l1ProxyAdminAddr).connect(l1Signer)
+  const l1ProxyAdmin = ProxyAdmin__factory.attach(l1ProxyAdminAddr).connect(
+    l1Signer
+  )
 
   const l1Admin = await l1ProxyAdmin.owner()
 
@@ -66,13 +70,15 @@ const main = async () => {
   }
 
   // l2 validation
-  // const l2ProxyAdmin = ProxyAdmin__factory.attach(l2ProxyAdminAddr).connect(l2Signer)
+  const l2ProxyAdmin = ProxyAdmin__factory.attach(l2ProxyAdminAddr).connect(
+    l2Signer
+  )
 
-  // const l2Admin = await l2ProxyAdmin.owner()
+  const l2Admin = await l2ProxyAdmin.owner()
 
-  // if (l2Admin.toLowerCase() !== l2Wallet.address.toLowerCase()) {
-  //   throw new Error('Wrong account')
-  // }
+  if (l2Admin.toLowerCase() !== l2Wallet.address.toLowerCase()) {
+    throw new Error('Wrong account')
+  }
 
   console.log('Data validated.')
 
@@ -91,21 +97,19 @@ const main = async () => {
   const newl1Custom = await l1CustomFactory.deploy()
   await newl1Custom.deployed()
 
+  console.log('deploying new L2 logic std')
+  const l2Erc20Factory = await (
+    await ethers.getContractFactory('L2ERC20Gateway')
+  ).connect(l2Signer)
+  const newL2Erc20 = await l2Erc20Factory.deploy()
+  await newL2Erc20.deployed()
 
-  // console.log('deploying new L2 logic std')
-  // const l2Erc20Factory = await (
-  //   await ethers.getContractFactory('L2ERC20Gateway')
-  // ).connect(l2Signer)
-  // const newL2Erc20 = await l2Erc20Factory.deploy()
-  // await newL2Erc20.deployed()
-
-
-  // console.log('deploying new L2 logic custom')
-  // const l2CustomFactory = await (
-  //   await ethers.getContractFactory('L2CustomGateway')
-  // ).connect(l2Signer)
-  // const newl2Custom = await l2CustomFactory.deploy()
-  // await newl2Custom.deployed()
+  console.log('deploying new L2 logic custom')
+  const l2CustomFactory = await (
+    await ethers.getContractFactory('L2CustomGateway')
+  ).connect(l2Signer)
+  const newl2Custom = await l2CustomFactory.deploy()
+  await newl2Custom.deployed()
 
   // console.log('deploying new L2 logic weth')
   // const l2WethFactory = await (
@@ -136,25 +140,25 @@ const main = async () => {
   const l1receiptcustom = await l1txcustom.wait()
   console.log({ l1receiptcustom })
 
-  // // console.log('upgrading L2 std')
-  // // const l2tx = await l2ProxyAdmin.upgradeAndCall(
-  // //   l2ERC20GatewayProxy,
-  // //   newL2Erc20.address,
-  // //   POST_UPGRADE_INIT
-  // // )
-  // // console.log({ l2tx })
-  // // const l2receipt = await l2tx.wait()
-  // // console.log({ l2receipt })
+  console.log('upgrading L2 std')
+  const l2tx = await l2ProxyAdmin.upgradeAndCall(
+    l2ERC20GatewayProxy,
+    newL2Erc20.address,
+    POST_UPGRADE_INIT
+  )
+  console.log({ l2tx })
+  const l2receipt = await l2tx.wait()
+  console.log({ l2receipt })
 
-  // // console.log('upgrading L2 custom')
-  // // const l2txcustom = await l2ProxyAdmin.upgradeAndCall(
-  // //   l2CustomGatewayProxy,
-  // //   newl2Custom.address,
-  // //   POST_UPGRADE_INIT
-  // // )
-  // console.log({ l2txcustom })
-  // const l2receiptcustom = await l2txcustom.wait()
-  // console.log({ l2receiptcustom })
+  console.log('upgrading L2 custom')
+  const l2txcustom = await l2ProxyAdmin.upgradeAndCall(
+    l2CustomGatewayProxy,
+    newl2Custom.address,
+    POST_UPGRADE_INIT
+  )
+  console.log({ l2txcustom })
+  const l2receiptcustom = await l2txcustom.wait()
+  console.log({ l2receiptcustom })
 }
 
 main()
