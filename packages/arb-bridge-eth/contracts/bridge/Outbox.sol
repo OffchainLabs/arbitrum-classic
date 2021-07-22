63da4db3bc375bfe01e49abdedef14b5058cf6ac
fred <fredlacs@gmail.com>
2021-07-22 12:38:15 +0100

    Update wording in docstring

--- packages/arb-bridge-eth/contracts/bridge/Outbox.sol
+++ packages/arb-bridge-eth/contracts/bridge/Outbox.sol
@@ -114,7 +114,7 @@ contract Outbox is IOutbox, Cloneable {
     /**
      * @notice Executes a messages in an Outbox entry.
      * @dev Reverts if dispute period hasn't expired, since the outbox entry
-     * is only created once the rollup confirms the respective node.
+     * is only created once the rollup confirms the respective assertion.
      * @param batchNum Index of OutboxEntry in outboxEntries array
      * @param proof Merkle proof of message inclusion in outbox entry
      * @param index Merkle path to message
