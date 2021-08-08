6f927a6e2e5a4dfcef2d5317aa6ff0bf2177bf5d
fred <fredlacs@gmail.com>
2021-08-08 18:50:07 +0100

    Add comment referencing admin storage slot

--- packages/arb-bridge-eth/contracts/libraries/ProxyUtil.sol
+++ packages/arb-bridge-eth/contracts/libraries/ProxyUtil.sol
@@ -20,6 +20,9 @@ pragma solidity ^0.6.11;
 
 library ProxyUtil {
     function getProxyAdmin() internal view returns (address admin) {
+        // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/proxy/TransparentUpgradeableProxy.sol#L48
+        // Storage slot with the admin of the proxy contract.
+        // This is the keccak-256 hash of "eip1967.proxy.admin" subtracted by 1, and is
         bytes32 slot = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
         assembly {
             admin := sload(slot)
