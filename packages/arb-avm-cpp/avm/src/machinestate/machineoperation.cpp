60c758f0206bf6427fbf5ec6793a937fdb208203
Lee Bousfield <ljbousfield@gmail.com>
2021-08-18 14:39:50 -0500

    Fix MachineState when code is not present (for tests)

--- packages/arb-avm-cpp/avm/src/machinestate/machineoperation.cpp
+++ packages/arb-avm-cpp/avm/src/machinestate/machineoperation.cpp
@@ -56,10 +56,13 @@ const uint256_t& assumeInt(const value& val) {
     return *aNum;
 }
 
-CodePointStub& assumeCodePoint(value& val) {
+CodePointStub& assumeCodePoint(const MachineState& m, value& val) {
     auto cp = std::get_if<CodePointStub>(&val);
     if (!cp) {
         throw bad_pop_type{};
+    }
+    if (m.code) {
+        m.code->resolveCodePointStub(*cp);
     }
     return *cp;
 }
@@ -542,16 +545,14 @@ void rset(MachineState& m) {
 
 void jump(MachineState& m) {
     m.stack.prepForMod(1);
-    auto& target = assumeCodePoint(m.stack[0]);
-    m.code->resolveCodePointStub(target);
+    auto& target = assumeCodePoint(m, m.stack[0]);
     m.pc = target.pc;
     m.stack.popClear();
 }
 
 void cjump(MachineState& m) {
     m.stack.prepForMod(2);
-    auto& target = assumeCodePoint(m.stack[0]);
-    m.code->resolveCodePointStub(target);
+    auto& target = assumeCodePoint(m, m.stack[0]);
     auto& cond = assumeInt(m.stack[1]);
     if (cond != 0) {
         m.pc = target.pc;
@@ -941,31 +942,21 @@ void errcodept(MachineState& m) {
 
 void pushinsn(MachineState& m) {
     m.stack.prepForMod(2);
-    auto target = std::get_if<CodePointStub>(&m.stack[1]);
-    if (!target) {
-        m.state = Status::Error;
-        return;
-    }
-    m.code->resolveCodePointStub(*target);
+    auto target = assumeCodePoint(m, m.stack[1]);
     auto& op_int = assumeInt(m.stack[0]);
     auto op = static_cast<uint8_t>(op_int);
-    m.stack[1] = m.code->addOperation(target->pc, {static_cast<OpCode>(op)});
+    m.stack[1] = m.code->addOperation(target.pc, {static_cast<OpCode>(op)});
     m.stack.popClear();
     ++m.pc;
 }
 
 void pushinsnimm(MachineState& m) {
     m.stack.prepForMod(3);
-    auto target = std::get_if<CodePointStub>(&m.stack[2]);
-    if (!target) {
-        m.state = Status::Error;
-        return;
-    }
-    m.code->resolveCodePointStub(*target);
+    auto target = assumeCodePoint(m, m.stack[2]);
     auto& op_int = assumeInt(m.stack[0]);
     auto op = static_cast<uint8_t>(op_int);
     m.stack[2] = m.code->addOperation(
-        target->pc, {static_cast<OpCode>(op), std::move(m.stack[1])});
+        target.pc, {static_cast<OpCode>(op), std::move(m.stack[1])});
     m.stack.popClear();
     m.stack.popClear();
     ++m.pc;
