33623aac86c9a97d59ef23353d26703c1010e761
dzgoldman <dzgoldman@wesleyan.edu>
2021-08-22 18:36:44 -0400

    include update-y hardhat tasks in arb-bridge-peripherals

--- packages/arb-bridge-peripherals/hardhat.config.ts
+++ packages/arb-bridge-peripherals/hardhat.config.ts
@@ -1,3 +1,27 @@
-import * as config from "arb-bridge-eth/hardhat.config"
-// use same config from bridge package
+import * as config from 'arb-bridge-eth/hardhat.config'
+import { task } from 'hardhat/config'
+import { initUpgrades } from 'arb-upgrades'
+
+require('@openzeppelin/hardhat-upgrades')
+
+task('deploy-logic-one', 'deploy one logic')
+  .addParam('contract', 'contract to deploy')
+  .setAction(async (args, hre) => {
+    const { contract } = args
+    const { deployLogic } = initUpgrades(hre, __dirname)
+    await deployLogic(contract)
+  })
+
+task('deploy-logic-all', 'deploy all logic contracts').setAction(
+  async (_, hre) => {
+    const { deployLogicAll } = initUpgrades(hre, __dirname)
+    await deployLogicAll()
+  }
+)
+
+task('trigger-upgrades', 'triggers upgrade').setAction(async (_, hre) => {
+  const { updateImplementations } = initUpgrades(hre, __dirname)
+  await updateImplementations()
+})
+
 module.exports = config
