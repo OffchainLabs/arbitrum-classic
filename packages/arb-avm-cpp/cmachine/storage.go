d156b9790ab21cd42d1e75aecc193d513880ba01
Joshua Colvin <jcolvin@offchainlabs.com>
2021-09-15 12:26:06 -0700

    Print metadata
    When `--metadata` is provided, print locations of files that are used and print database metadata

--- packages/arb-avm-cpp/cmachine/storage.go
+++ packages/arb-avm-cpp/cmachine/storage.go
@@ -119,6 +119,10 @@ func NewArbStorage(dbPath string, coreConfig *configuration.Core) (*ArbStorage, 
 	return returnVal, nil
 }
 
+func (s *ArbStorage) PrintDatabaseMetadata() {
+	C.printDatabaseMetadata(s.c)
+}
+
 func (s *ArbStorage) Initialize(contractPath string) error {
 	defer runtime.KeepAlive(s)
 	cContractPath := C.CString(contractPath)
