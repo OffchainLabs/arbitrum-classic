77125b8c1bd91aaa15f9a9bacd84fcf19abc656a
fred <fredlacs@gmail.com>
2021-10-07 18:19:48 +0100

    Update signal interface

--- packages/arb-bridge-peripherals/contracts/misc/Registry.sol
+++ packages/arb-bridge-peripherals/contracts/misc/Registry.sol
@@ -3,9 +3,18 @@
 pragma solidity ^0.6.11;
 
 contract Registry {
-    event SignalEmitted(address indexed from, address indexed signal);
+    event SignalEmitted(
+        address sender,
+        address indexed l1Address,
+        address indexed l2Address,
+        address indexed l2OwnerAddress
+    );
 
-    function signal(address signalAddr) external {
-        emit SignalEmitted(msg.sender, signalAddr);
+    function signal(
+        address l1Address,
+        address l2Address,
+        address l2OwnerAddress
+    ) external {
+        emit SignalEmitted(msg.sender, l1Address, l2Address, l2OwnerAddress);
     }
 }
