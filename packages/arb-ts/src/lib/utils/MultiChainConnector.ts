419d165b87f9c55b276094ea978471a933824f1f
dzgoldman <dzgoldman@wesleyan.edu>
2021-11-18 08:44:54 -0500

    latest mutlichainconectooor

--- packages/arb-ts/src/lib/utils/MultiChainConnector.ts
+++ packages/arb-ts/src/lib/utils/MultiChainConnector.ts
@@ -1,6 +1,11 @@
 import { Provider } from '@ethersproject/abstract-provider'
 import { Signer } from '@ethersproject/abstract-signer'
-import networks, { Network } from '../networks'
+import networks, {
+  L1Network,
+  L2Network,
+  l1Networks,
+  l2Networks,
+} from './networks'
 
 export interface SignersAndProviders {
   l1Provider?: Provider
@@ -14,16 +19,19 @@ export class MultiChainConnector {
   l2Provider?: Provider
   l1Signer?: Signer
   l2Signer?: Signer
-  constructor(signersAndProviders: SignersAndProviders) {
+  l1Network?: L1Network
+  l2Network?: L2Network
+
+  public async initSignorsAndProviders(
+    signersAndProviders: SignersAndProviders
+  ) {
     const { l1Provider, l2Provider, l1Signer, l2Signer } = signersAndProviders
-    // TODO: check l1 and l2 networks reasonably match
     if (l1Signer) {
       this.l1Signer = l1Signer
       if (l1Signer.provider) {
         this.l1Provider = l1Signer.provider
       }
     }
-
     if (l2Signer) {
       this.l2Signer = l2Signer
       if (l2Signer.provider) {
@@ -38,9 +46,24 @@ export class MultiChainConnector {
     if (!this.l2Provider && l2Provider) {
       this.l2Provider = l2Provider
     }
-  }
 
-  protected requireL2Provider() {
-    if (!this.l2Provider) throw new Error('Must have l2 provider')
+    if (this.l1Provider) {
+      const chainID = (await this.l1Provider.getNetwork()).chainId.toString()
+      const l1Network = l1Networks[chainID]
+      if (!l1Network) throw new Error('todo: custom')
+      this.l1Network = l1Network
+    }
+    if (this.l2Provider) {
+      const chainID = (await this.l2Provider.getNetwork()).chainId.toString()
+      const l2Network = l2Networks[chainID]
+      if (!l2Network) throw new Error('todo: custom')
+      this.l2Network = l2Network
+    }
+
+    if (this.l1Network && this.l2Network) {
+      if (this.l2Network.partnerChainID !== this.l1Network.chainID) {
+        throw new Error('chains mismatched')
+      }
+    }
   }
 }
