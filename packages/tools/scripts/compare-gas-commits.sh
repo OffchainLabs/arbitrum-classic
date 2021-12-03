59ba6a13b931cebbca377db3c18ac1da8cc5f7bd
Chris Buckland <cpbuckland88@gmail.com>
2021-12-03 10:13:47 +0000

    Added logging and paramaterised the input to gas comparison shell script

--- packages/tools/scripts/compare-gas-commits.sh
+++ packages/tools/scripts/compare-gas-commits.sh
@@ -1,11 +1,13 @@
 # run this from arbitrum project root
 HASH_NOW=$(git rev-parse HEAD)
-HASH_BEFORE=
-HASH_AFTER=
+HASH_BEFORE=$1
+HASH_AFTER=$2
+echo "Comparing gas usage between $HASH_BEFORE and $HASH_AFTER"
 
 # checkout before, run tests, create report
 git checkout HASH_BEFORE
 cd ./packages/arb-eth-bridge
+echo "Measuring gas usage for $HASH_BEFORE"
 yarn test:gas:ci
 cd ../..
 mv gasReporterOutput.json ./packages/tools/gas-$HASH_BEFORE.json
@@ -13,11 +15,13 @@ mv gasReporterOutput.json ./packages/tools/gas-$HASH_BEFORE.json
 # and after
 git checkout HASH_AFTER
 cd ./packages/arb-eth-bridge
+echo "Measuring gas usage for $HASH_AFTER"
 yarn test:gas:ci
 cd ../..
 mv gasReporterOutput.json ./packages/tools/gas-$HASH_AFTER.json
 
 # now compare by running output
+echo "Calculating comparison"
 echo "./gas-$HASH_BEFORE.json ./gas-$HASH_AFTER.json ./comparison-output.csv"
 cd packages/tools
 yarn dev:compare-gas -- "./gas-$HASH_BEFORE.json ./gas-$HASH_AFTER.json ./comparison-output.csv"
