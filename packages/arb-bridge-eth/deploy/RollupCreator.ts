e5148580a5f601a120ee279e25681b99b06f571f
Chris Buckland <cpbuckland88@gmail.com>
2021-12-06 16:31:18 +0000

    Lint in deploy/RollupCreator

--- packages/arb-bridge-eth/deploy/RollupCreator.ts
+++ packages/arb-bridge-eth/deploy/RollupCreator.ts
@@ -30,8 +30,4 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
 module.exports = func
 module.exports.tags = ['RollupCreator', 'live', 'test']
-module.exports.dependencies = [
-  'Rollup',
-  'ChallengeFactory',
-  'BridgeCreator',
-]
+module.exports.dependencies = ['Rollup', 'ChallengeFactory', 'BridgeCreator']
