e38de4950ed2c74478cfcce42af2634ec4bde966
Joshua Colvin <jcolvin@offchainlabs.com>
2021-12-08 21:29:41 -0700

    Add parameter for inbox-reader-delay

--- packages/arb-node-core/cmd/arb-validator/arb-validator.go
+++ packages/arb-node-core/cmd/arb-validator/arb-validator.go
@@ -223,7 +223,16 @@ func startup() error {
 		return errors.Wrap(err, "error setting up staker")
 	}
 
-	_, err = mon.StartInboxReader(ctx, l1Client, common.NewAddressFromEth(rollupAddr), config.Rollup.FromBlock, common.NewAddressFromEth(bridgeUtilsAddr), healthChan, dummySequencerFeed, config.Node.ParanoidInboxReader)
+	_, err = mon.StartInboxReader(
+		ctx,
+		l1Client,
+		common.NewAddressFromEth(rollupAddr),
+		config.Rollup.FromBlock,
+		common.NewAddressFromEth(bridgeUtilsAddr),
+		healthChan,
+		dummySequencerFeed,
+		config.Node,
+	)
 	if err != nil {
 		return errors.Wrap(err, "failed to create inbox reader")
 	}
