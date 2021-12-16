6e5319309cba2e2875c30b3c40162bf78c8fe3bd
YameenMalik <yameen@seed.im>
2021-12-16 13:29:57 +0500

    added script for mining blocks for ganace-chain

---
+++ scripts/mine-blocks.ts
@@ -0,0 +1,20 @@
+import { config } from "dotenv"; config({ path: ".env" });
+import {ethers} from "ethers";
+
+const network:string = process.env['DEPLOY_ON'] || '';
+const network_url:string = process.env[network.toUpperCase() + "_NETWORK"] || '';
+
+const provider = new ethers.providers.JsonRpcProvider(network_url);
+
+async function mineBlocks(numOfBlocks: number) {
+    while (numOfBlocks > 0) {
+      await provider.send("evm_mine", []);
+      numOfBlocks--;
+    }
+}
+
+if (require.main === module) {
+  const numBlocks = Number(process.argv[2]) || 10;
+  console.log(`Mining ${numBlocks} blocks`)
+  mineBlocks(numBlocks)
+}
