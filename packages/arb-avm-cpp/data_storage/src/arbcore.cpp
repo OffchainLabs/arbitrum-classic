4f09dcd4a6dd15266f5c27c179921a912a4ed245
Lee Bousfield <ljbousfield@gmail.com>
2022-01-15 11:43:51 -0600

    Clear messages when loading reorg machine

--- packages/arb-avm-cpp/data_storage/src/arbcore.cpp
+++ packages/arb-avm-cpp/data_storage/src/arbcore.cpp
@@ -1045,6 +1045,7 @@ rocksdb::Status ArbCore::reorgCheckpoints(
     }
 
     // Checkpoint was saved at sideload, attempt to continue running
+    core_machine->machine_state.context.clearInboxMessages();
     core_machine->continueRunningMachine(true);
 
     return rocksdb::Status::OK();
