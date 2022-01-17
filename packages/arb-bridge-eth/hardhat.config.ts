4661a863a2c01a90568c18bc832c93aac3e8cf07
Chris Buckland <cpbuckland88@gmail.com>
2022-01-17 18:40:02 +0100

    Removed depped tests

--- packages/arb-bridge-eth/hardhat.config.ts
+++ packages/arb-bridge-eth/hardhat.config.ts
@@ -13,9 +13,9 @@ import 'hardhat-gas-reporter'
 import '@nomiclabs/hardhat-etherscan'
 import { initUpgrades } from 'arb-upgrades'
 
-const verifyTask = require('./scripts/verifyTask') // eslint-disable-line @typescript-eslint/no-var-requires
-const setupVerifyTask = verifyTask.default
-setupVerifyTask()
+// const verifyTask = require('./scripts/verifyTask') // eslint-disable-line @typescript-eslint/no-var-requires
+// const setupVerifyTask = verifyTask.default
+// setupVerifyTask()
 
 const handleFork = async (hre: HardhatRuntimeEnvironment) => {
   const network = hre.network
@@ -90,150 +90,150 @@ task('create-chain', 'Creates a rollup chain')
     )
   })
 
-task('deposit', 'Deposit coins into ethbridge')
-  .addPositionalParam('inboxAddress', "The rollup chain's address")
-  .addPositionalParam('privkey', 'The private key of the depositer')
-  .addPositionalParam('dest', "The destination account's address")
-  .addPositionalParam('amount', 'The amount to deposit')
-  .setAction(async ({ inboxAddress, privkey, dest, amount }, bre) => {
-    const { ethers } = bre
-    const wallet = new ethers.Wallet(privkey, ethers.provider)
-    const GlobalInbox = await ethers.getContractFactory('Inbox')
-    const inbox = GlobalInbox.attach(inboxAddress).connect(wallet)
-    await inbox.depositEth(dest, { value: amount })
-  })
+// task('deposit', 'Deposit coins into ethbridge')
+//   .addPositionalParam('inboxAddress', "The rollup chain's address")
+//   .addPositionalParam('privkey', 'The private key of the depositer')
+//   .addPositionalParam('dest', "The destination account's address")
+//   .addPositionalParam('amount', 'The amount to deposit')
+//   .setAction(async ({ inboxAddress, privkey, dest, amount }, bre) => {
+//     const { ethers } = bre
+//     const wallet = new ethers.Wallet(privkey, ethers.provider)
+//     const GlobalInbox = await ethers.getContractFactory('Inbox')
+//     const inbox = GlobalInbox.attach(inboxAddress).connect(wallet)
+//     await inbox.depositEth(dest, { value: amount })
+//   })
 
-task('core-deploy-logic-one', 'deploy one logic')
-  .addParam('contract', 'contract to deploy')
-  .setAction(async (args, hre) => {
-    await handleFork(hre)
-    const { contract } = args
-    const { deployLogic } = initUpgrades(hre, __dirname)
-    await deployLogic(contract)
-  })
+// task('core-deploy-logic-one', 'deploy one logic')
+//   .addParam('contract', 'contract to deploy')
+//   .setAction(async (args, hre) => {
+//     await handleFork(hre)
+//     const { contract } = args
+//     const { deployLogic } = initUpgrades(hre, __dirname)
+//     await deployLogic(contract)
+//   })
 
-task('core-deploy-logic-all', 'deploy all logic contracts').setAction(
-  async (_, hre) => {
-    await handleFork(hre)
-    const { deployLogicAll } = initUpgrades(hre, __dirname)
-    await deployLogicAll()
-  }
-)
+// task('core-deploy-logic-all', 'deploy all logic contracts').setAction(
+//   async (_, hre) => {
+//     await handleFork(hre)
+//     const { deployLogicAll } = initUpgrades(hre, __dirname)
+//     await deployLogicAll()
+//   }
+// )
 
-task('core-trigger-upgrades', 'triggers upgrade').setAction(async (_, hre) => {
-  await handleFork(hre)
-  const { updateImplementations } = initUpgrades(hre, __dirname)
-  await updateImplementations()
-})
+// task('core-trigger-upgrades', 'triggers upgrade').setAction(async (_, hre) => {
+//   await handleFork(hre)
+//   const { updateImplementations } = initUpgrades(hre, __dirname)
+//   await updateImplementations()
+// })
 
-task('core-verify-deployments', 'verifies implementations').setAction(
-  async (_, hre) => {
-    await handleFork(hre)
-    const { verifyCurrentImplementations } = initUpgrades(hre, __dirname)
-    await verifyCurrentImplementations()
-  }
-)
+// task('core-verify-deployments', 'verifies implementations').setAction(
+//   async (_, hre) => {
+//     await handleFork(hre)
+//     const { verifyCurrentImplementations } = initUpgrades(hre, __dirname)
+//     await verifyCurrentImplementations()
+//   }
+// )
 
-task('core-transfer-beacon-owner', 'transfers beacon owner')
-  .addParam('address', 'beacon contract')
-  .addParam('newowner', 'beacon contract')
+// task('core-transfer-beacon-owner', 'transfers beacon owner')
+//   .addParam('address', 'beacon contract')
+//   .addParam('newowner', 'beacon contract')
 
-  .setAction(async (args, hre) => {
-    await handleFork(hre)
-    const { transferBeaconOwner } = initUpgrades(hre, __dirname)
-    await transferBeaconOwner(args.address, args.newowner)
-  })
+//   .setAction(async (args, hre) => {
+//     await handleFork(hre)
+//     const { transferBeaconOwner } = initUpgrades(hre, __dirname)
+//     await transferBeaconOwner(args.address, args.newowner)
+//   })
 
-task('core-transfer-admin', 'transfer proxy admin')
-  .addParam('proxyaddress', 'proxy address')
-  .addParam('newadmin', 'address of new admin')
-  .setAction(async (args, hre) => {
-    const { transferAdmin } = initUpgrades(hre, __dirname)
-    await transferAdmin(args.proxyaddress, args.newadmin)
-  })
+// task('core-transfer-admin', 'transfer proxy admin')
+//   .addParam('proxyaddress', 'proxy address')
+//   .addParam('newadmin', 'address of new admin')
+//   .setAction(async (args, hre) => {
+//     const { transferAdmin } = initUpgrades(hre, __dirname)
+//     await transferAdmin(args.proxyaddress, args.newadmin)
+//   })
 
-task('etherscan-verify', 'verify current deployments in etherscan').setAction(
-  async (_, hre) => {
-    const { verifyDeployments } = await initUpgrades(hre, __dirname)
-    await verifyDeployments()
-  }
-)
+// task('etherscan-verify', 'verify current deployments in etherscan').setAction(
+//   async (_, hre) => {
+//     const { verifyDeployments } = await initUpgrades(hre, __dirname)
+//     await verifyDeployments()
+//   }
+// )
 
-task(
-  'remove-build-info',
-  'remove giant build info string from current_deployments json'
-).setAction(async (_, hre) => {
-  const { removeBuildInfoFiles } = initUpgrades(hre, __dirname)
-  await removeBuildInfoFiles()
-})
+// task(
+//   'remove-build-info',
+//   'remove giant build info string from current_deployments json'
+// ).setAction(async (_, hre) => {
+//   const { removeBuildInfoFiles } = initUpgrades(hre, __dirname)
+//   await removeBuildInfoFiles()
+// })
 
-task('deploy-outbox-logic', 'deploy and set a new outbox').setAction(
-  async (_, hre) => {
-    const OutboxFactory = await hre.ethers.getContractFactory('Outbox')
-    console.log('Deploying outbox logic')
-    const OutboxLogic = await OutboxFactory.deploy()
-    await OutboxLogic.deployed()
-    console.log('Outbox logic deployed at:', OutboxLogic.address)
-  }
-)
+// task('deploy-outbox-logic', 'deploy and set a new outbox').setAction(
+//   async (_, hre) => {
+//     const OutboxFactory = await hre.ethers.getContractFactory('Outbox')
+//     console.log('Deploying outbox logic')
+//     const OutboxLogic = await OutboxFactory.deploy()
+//     await OutboxLogic.deployed()
+//     console.log('Outbox logic deployed at:', OutboxLogic.address)
+//   }
+// )
 
-task('deploy-outbox-proxy', 'deploy outbox proxy')
-  .addParam('outboxlogic', 'outbox logic')
-  .setAction(async (args, hre) => {
-    const { getDeployments } = initUpgrades(hre, __dirname)
-    const { data } = await getDeployments()
-    const proxyAdminAddress = data.proxyAdminAddress
+// task('deploy-outbox-proxy', 'deploy outbox proxy')
+//   .addParam('outboxlogic', 'outbox logic')
+//   .setAction(async (args, hre) => {
+//     const { getDeployments } = initUpgrades(hre, __dirname)
+//     const { data } = await getDeployments()
+//     const proxyAdminAddress = data.proxyAdminAddress
 
-    console.log('Deploying Outbox TransparentUpgradeableProxy')
-    const TransparentUpgradeableProxyFactory =
-      await hre.ethers.getContractFactory('TransparentUpgradeableProxy')
-    const OutboxProxyDeployed = await TransparentUpgradeableProxyFactory.deploy(
-      args.outboxlogic,
-      proxyAdminAddress,
-      '0x'
-    )
-    await OutboxProxyDeployed.deployed()
-    console.log('Outbox proxy deployed at', OutboxProxyDeployed.address)
-  })
+//     console.log('Deploying Outbox TransparentUpgradeableProxy')
+//     const TransparentUpgradeableProxyFactory =
+//       await hre.ethers.getContractFactory('TransparentUpgradeableProxy')
+//     const OutboxProxyDeployed = await TransparentUpgradeableProxyFactory.deploy(
+//       args.outboxlogic,
+//       proxyAdminAddress,
+//       '0x'
+//     )
+//     await OutboxProxyDeployed.deployed()
+//     console.log('Outbox proxy deployed at', OutboxProxyDeployed.address)
+//   })
 
-task('init-outbox', 'deploy and set a new outbox')
-  .addParam('outboxproxy', '')
+// task('init-outbox', 'deploy and set a new outbox')
+//   .addParam('outboxproxy', '')
 
-  .setAction(async (args, hre) => {
-    const { getDeployments } = initUpgrades(hre, __dirname)
-    const { data } = await getDeployments()
-    const rollupAddress = data.contracts.Rollup.proxyAddress
-    const bridgeAddress = data.contracts.Bridge.proxyAddress
+//   .setAction(async (args, hre) => {
+//     const { getDeployments } = initUpgrades(hre, __dirname)
+//     const { data } = await getDeployments()
+//     const rollupAddress = data.contracts.Rollup.proxyAddress
+//     const bridgeAddress = data.contracts.Bridge.proxyAddress
 
-    const Outbox = (await hre.ethers.getContractFactory('Outbox')).attach(
-      args.outboxproxy
-    )
-    const initializeRes = await Outbox.initialize(rollupAddress, bridgeAddress)
-    const initializeRec = await initializeRes.wait()
-    console.log('Outbox initialized', initializeRec)
-  })
+//     const Outbox = (await hre.ethers.getContractFactory('Outbox')).attach(
+//       args.outboxproxy
+//     )
+//     const initializeRes = await Outbox.initialize(rollupAddress, bridgeAddress)
+//     const initializeRec = await initializeRes.wait()
+//     console.log('Outbox initialized', initializeRec)
+//   })
 
-task('set-outbox', 'deploy and set a new outbox')
-  .addParam('outboxproxy', '')
+// task('set-outbox', 'deploy and set a new outbox')
+//   .addParam('outboxproxy', '')
 
-  .setAction(async (args, hre) => {
-    const { getDeployments } = initUpgrades(hre, __dirname)
-    const { data } = await getDeployments()
-    const rollupAddress = data.contracts.Rollup.proxyAddress
-    console.log('Sanity checking ')
-    const Rollup = (await hre.ethers.getContractFactory('Rollup')).attach(
-      rollupAddress
-    )
-    await Rollup.userLogic()
-    console.log('Rollup sanity checked ')
-    const RollupAdmin = (
-      await hre.ethers.getContractFactory('RollupAdminLogic')
-    ).attach(rollupAddress)
-    const setRollupRes = await RollupAdmin.setOutbox(args.outboxproxy)
-    const setRollupRec = await setRollupRes.wait()
-    console.log('Outbox set', setRollupRec)
-    console.log('all set 👍')
-  })
+//   .setAction(async (args, hre) => {
+//     const { getDeployments } = initUpgrades(hre, __dirname)
+//     const { data } = await getDeployments()
+//     const rollupAddress = data.contracts.Rollup.proxyAddress
+//     console.log('Sanity checking ')
+//     const Rollup = (await hre.ethers.getContractFactory('Rollup')).attach(
+//       rollupAddress
+//     )
+//     await Rollup.userLogic()
+//     console.log('Rollup sanity checked ')
+//     const RollupAdmin = (
+//       await hre.ethers.getContractFactory('RollupAdminLogic')
+//     ).attach(rollupAddress)
+//     const setRollupRes = await RollupAdmin.setOutbox(args.outboxproxy)
+//     const setRollupRec = await setRollupRes.wait()
+//     console.log('Outbox set', setRollupRec)
+//     console.log('all set 👍')
+//   })
 
 const config = {
   defaultNetwork: 'hardhat',
