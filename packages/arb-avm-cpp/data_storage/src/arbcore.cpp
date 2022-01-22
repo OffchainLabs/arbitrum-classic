06d7484b846e9b44ebb91a4953ef4d10bee8f5c7
Joshua Colvin <jcolvin@offchainlabs.com>
2022-01-21 23:40:13 -0700

    Stop advancing core when gas no longer used
    Instead of paying attention to the number of processed messages, just stop executing when gas is no longer being used.

--- packages/arb-avm-cpp/data_storage/src/arbcore.cpp
+++ packages/arb-avm-cpp/data_storage/src/arbcore.cpp
@@ -732,6 +732,7 @@ rocksdb::Status ArbCore::advanceCoreToTarget(const MachineOutput& target_output,
     while (core_machine->machine_state.output.arb_gas_used <
            target_output.arb_gas_used) {
         // Need to run machine until caught up with current checkpoint
+        auto last_gas_used = core_machine->machine_state.output.arb_gas_used;
         MachineExecutionConfig execConfig;
         execConfig.stop_on_sideload = cache_sideloads;
         execConfig.max_gas = target_output.arb_gas_used;
@@ -740,9 +741,6 @@ rocksdb::Status ArbCore::advanceCoreToTarget(const MachineOutput& target_output,
         uint256_t target_messages = target_output.fully_processed_inbox.count;
         uint256_t have_messages =
             core_machine->machine_state.output.fully_processed_inbox.count;
-        if (target_messages <= have_messages) {
-            break;
-        }
         uint256_t num_messages = target_messages - have_messages;
         if (uint256_t(num_messages) > coreConfig.message_process_count) {
             num_messages = coreConfig.message_process_count;
@@ -781,6 +779,11 @@ rocksdb::Status ArbCore::advanceCoreToTarget(const MachineOutput& target_output,
                 std::cerr << "Error catching up: " << core_error_string << "\n";
                 return rocksdb::Status::Aborted();
             }
+        }
+
+        if (last_gas_used == core_machine->machine_state.output.arb_gas_used) {
+            // No forward progress made
+            break;
         }
     }
 
