47f2e30126f22ea7d1a29b02da4306dffc1e108e
Lee Bousfield <ljbousfield@gmail.com>
2022-01-26 13:57:39 -0600

    Check for duplicate transactions in a batch

--- packages/arb-rpc-node/batcher/sequencerBatcher.go
+++ packages/arb-rpc-node/batcher/sequencerBatcher.go
@@ -308,6 +308,7 @@ func (b *SequencerBatcher) SendTransaction(ctx context.Context, startTx *types.T
 		var batchDataSize int
 		seenOwnTx := false
 		emptiedQueue := true
+		txHashesSet := make(map[ethcommon.Hash]struct{})
 		// This pattern is safe as we acquired a lock so we are the exclusive reader
 		for len(b.txQueue) > 0 {
 			queueItem := <-b.txQueue
@@ -325,6 +326,13 @@ func (b *SequencerBatcher) SendTransaction(ctx context.Context, startTx *types.T
 				emptiedQueue = false
 				break
 			}
+			txHash := queueItem.tx.Hash()
+			_, txAlreadyInBatch := txHashesSet[txHash]
+			if txAlreadyInBatch {
+				queueItem.resultChan <- errors.New("duplicate transaction")
+				continue
+			}
+			txHashesSet[txHash] = struct{}{}
 			if queueItem.tx == startTx {
 				seenOwnTx = true
 			}
