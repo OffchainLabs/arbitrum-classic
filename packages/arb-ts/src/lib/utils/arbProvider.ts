bd8affc29dae64a607753fa7807dd32f3791419d
Chris Buckland <cpbuckland88@gmail.com>
2022-01-27 15:03:59 +0100

    Updates from CR

--- packages/arb-ts/src/lib/utils/arbProvider.ts
+++ packages/arb-ts/src/lib/utils/arbProvider.ts
@@ -10,7 +10,7 @@ import {
 } from '../dataEntities/arbTransaction'
 import { providers, BigNumber, logger } from 'ethers'
 import { Formats, FormatFuncs } from '@ethersproject/providers/lib/formatter'
-import { getL1Network, getL2Network } from '../..'
+import { getL1Network, getL2Network, L2Network } from '../..'
 import { SignerProviderUtils } from '../dataEntities/signerOrProvider'
 import { SequencerInbox, SequencerInbox__factory } from '../abi'
 import {
@@ -124,48 +124,46 @@ class ArbFormatter extends Formatter {
   }
 }
 
+/**
+ * Get batch info for a message of a given sequence number
+ * If eventType is "sequencer" only sequencer events will be looked for
+ * If eventType is "delayed" only force included events will be looked for
+ * @returns
+ */
 const getBatch = async (
   seqNum: BigNumber,
-  l1BlockNumber: number,
-  l1Provider: providers.Provider
+  l1Provider: providers.Provider,
+  l2Network: L2Network,
+  startBlock: number,
+  endBlock: number,
+  eventTypes: 'sequencer' | 'delayed'
 ): Promise<Omit<BatchInfo, 'confirmations'> | null> => {
-  const ethNetwork = await getL1Network(l1Provider)
-  const arbNetwork = await getL2Network(ethNetwork.partnerChainIDs[0])
-  const inbox = SequencerInbox__factory.connect(
-    arbNetwork.ethBridge.sequencerInbox,
-    l1Provider
-  )
-
-  const delta = 1000
-
-  const delayBlocks = (await inbox.maxDelayBlocks()).toNumber()
-
-  const startBlock = l1BlockNumber - delta
-  const delayedBlockMax = l1BlockNumber + delayBlocks
-  const currentBlock = await l1Provider.getBlockNumber()
-
-  const endBlock =
-    delayedBlockMax > currentBlock ? currentBlock : delayedBlockMax
-
   const batchEvents = new EventFetcher(l1Provider)
+
   const events = await batchEvents.getEvents<
     SequencerInbox,
     | DelayedInboxForcedEvent
     | SequencerBatchDeliveredEvent
     | SequencerBatchDeliveredFromOriginEvent
   >(
-    inbox.address,
+    l2Network.ethBridge.sequencerInbox,
     SequencerInbox__factory,
     c => {
-      const eventTopics = [
-        c.interface.getEventTopic(c.interface.getEvent('DelayedInboxForced')),
-        c.interface.getEventTopic(
-          c.interface.getEvent('SequencerBatchDelivered')
-        ),
-        c.interface.getEventTopic(
-          c.interface.getEvent('SequencerBatchDeliveredFromOrigin')
-        ),
-      ]
+      const eventTopics =
+        eventTypes === 'sequencer'
+          ? [
+              c.interface.getEventTopic(
+                c.interface.getEvent('SequencerBatchDelivered')
+              ),
+              c.interface.getEventTopic(
+                c.interface.getEvent('SequencerBatchDeliveredFromOrigin')
+              ),
+            ]
+          : [
+              c.interface.getEventTopic(
+                c.interface.getEvent('DelayedInboxForced')
+              ),
+            ]
 
       return { topics: [eventTopics] }
     },
@@ -185,6 +183,79 @@ const getBatch = async (
     logData: batch.data,
     logTopics: batch.topics,
   }
+}
+
+/**
+ * Get batch info for a message of a given sequence number
+ * Only looks for events created by the sequencer
+ * @param seqNum
+ * @param l2Txl1BlockNumber The l1BlockNumber that was in the receipt of the l2 transaction. This is the value block.number would have during the execution of that transaciton.
+ * @param l1Provider
+ * @param l2Network
+ * @returns
+ */
+const getSequencerBatch = async (
+  seqNum: BigNumber,
+  l2Txl1BlockNumber: number,
+  l1Provider: providers.Provider,
+  l2Network: L2Network
+): Promise<Omit<BatchInfo, 'confirmations'> | null> => {
+  const inbox = SequencerInbox__factory.connect(
+    l2Network.ethBridge.sequencerInbox,
+    l1Provider
+  )
+
+  const delayBlocks = (await inbox.maxDelayBlocks()).toNumber()
+
+  const startBlock = l2Txl1BlockNumber
+  const delayedBlockMax = l2Txl1BlockNumber + delayBlocks
+  const currentBlock = await l1Provider.getBlockNumber()
+
+  const endBlock = Math.min(delayedBlockMax, currentBlock)
+
+  return await getBatch(
+    seqNum,
+    l1Provider,
+    l2Network,
+    startBlock,
+    endBlock,
+    'sequencer'
+  )
+}
+
+/**
+ * Get batch info for a message of a given sequence number
+ * Only looks for force included events
+ * @param seqNum
+ * @param l2Txl1BlockNumber The l1BlockNumber that was in the receipt of the l2 transaction. This is the value block.number would have during the execution of that transaciton.
+ * @param l1Provider
+ * @param l2Network
+ * @returns
+ */
+const getDelayedBatch = async (
+  seqNum: BigNumber,
+  l2Txl1BlockNumber: number,
+  l1Provider: providers.Provider,
+  l2Network: L2Network
+): Promise<Omit<BatchInfo, 'confirmations'> | null> => {
+  const inbox = SequencerInbox__factory.connect(
+    l2Network.ethBridge.sequencerInbox,
+    l1Provider
+  )
+  const delayBlocks = (await inbox.maxDelayBlocks()).toNumber()
+  const delayedBlockMax = l2Txl1BlockNumber + delayBlocks
+  const currentBlock = await l1Provider.getBlockNumber()
+  const startBlock = Math.min(delayedBlockMax, currentBlock)
+  const endBlock = Math.max(startBlock, currentBlock)
+
+  return await getBatch(
+    seqNum,
+    l1Provider,
+    l2Network,
+    startBlock,
+    endBlock,
+    'delayed'
+  )
 }
 
 /**
@@ -219,11 +290,44 @@ export const getRawArbTransactionReceipt = async (
 
     const tx = await getTransaction(l2Provider, txHash)
     if (tx) {
-      const batch = await getBatch(
+      const sequencerBatch = await getSequencerBatch(
         tx.l1SequenceNumber,
         tx.l1BlockNumber,
-        l1ProviderForBatch
+        l1ProviderForBatch,
+        l2Network
       )
+      let batch = sequencerBatch
+
+      // we didnt find a sequencer batch, either it hasnt been included
+      // yet, or we it was included as a delayed batch
+      if (!sequencerBatch) {
+        const currentBlock = await l1ProviderForBatch.getBlockNumber()
+        const inbox = SequencerInbox__factory.connect(
+          l2Network.ethBridge.sequencerInbox,
+          l1ProviderForBatch
+        )
+        const delayBlocks = (await inbox.maxDelayBlocks()).toNumber()
+        const delaySeconds = (await inbox.maxDelaySeconds()).toNumber()
+        const l1Timestamp = (
+          await l1ProviderForBatch.getBlock(tx.l1BlockNumber)
+        ).timestamp
+        const timeNowSec = Date.now() / 1000
+
+        if (
+          currentBlock > delayBlocks + tx.l1BlockNumber &&
+          timeNowSec > delaySeconds + l1Timestamp
+        ) {
+          // we've passed the delayed block period, so it's
+          // worthwhile to look for delayed batches
+          batch = await getDelayedBatch(
+            tx.l1SequenceNumber,
+            tx.l1BlockNumber,
+            l1ProviderForBatch,
+            l2Network
+          )
+        }
+      }
+
       const currentBlock = await l1ProviderForBatch.getBlockNumber()
 
       arbTxReceipt.l1InboxBatchInfo = batch
