8f0dcfe8f18a11dd2d836e72d6782ef83a0efbe3
Joshua Colvin <jcolvin@offchainlabs.com>
2022-03-07 14:21:49 -0700

    Normalize docker images
    * Use Ubuntu instead of Debian
    * Reduce duplication across dockerfiles  to ease maintenance
    * Cleanup intermediate build files to reduce image size

--- .circleci/images/dockerhub.sh
+++ .circleci/images/dockerhub.sh
@@ -6,24 +6,25 @@
 # Exit on error
 set -e
 
-docker build -t offchainlabs/backend-base:0.5.0 backend-base
-docker push offchainlabs/backend-base:0.5.0
+# cpp-base depended on by everything else so build first
+docker build -t offchainlabs/cpp-base:0.6.0 cpp-base
+docker push offchainlabs/cpp-base:0.6.0
 
-docker build -t offchainlabs/cpp-asan-base:0.5.0 cpp-asan-base
-docker push offchainlabs/cpp-asan-base:0.5.0
+docker build -t offchainlabs/backend-base:0.6.0 backend-base
+docker push offchainlabs/backend-base:0.6.0
 
-docker build -t offchainlabs/cpp-base:0.5.0 cpp-base
-docker push offchainlabs/cpp-base:0.5.0
+docker build -t offchainlabs/cpp-asan-base:0.6.0 cpp-asan-base
+docker push offchainlabs/cpp-asan-base:0.6.0
 
-docker build -t offchainlabs/cpp-tsan-base:0.5.0 cpp-tsan-base
-docker push offchainlabs/cpp-tsan-base:0.5.0
+docker build -t offchainlabs/cpp-tsan-base:0.6.0 cpp-tsan-base
+docker push offchainlabs/cpp-tsan-base:0.6.0
 
-docker build -t offchainlabs/dist-base:0.5.0 dist-base
-docker push offchainlabs/dist-base:0.5.0
+docker build -t offchainlabs/dist-base:0.6.0 dist-base
+docker push offchainlabs/dist-base:0.6.0
 
-docker build -t offchainlabs/frontend-base:0.5.0 frontend-base
-docker push offchainlabs/frontend-base:0.5.0
+docker build -t offchainlabs/frontend-base:0.6.0 frontend-base
+docker push offchainlabs/frontend-base:0.6.0
 
-docker build -t offchainlabs/integration-base:0.5.0 integration-base
-docker push offchainlabs/integration-base:0.5.0
+docker build -t offchainlabs/integration-base:0.6.0 integration-base
+docker push offchainlabs/integration-base:0.6.0
 
