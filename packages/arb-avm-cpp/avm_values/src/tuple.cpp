043ffd41b6d854332a7a5d3c129c1f6bd60647d6
Lee Bousfield <ljbousfield@gmail.com>
2022-04-21 20:48:05 -0500

    Use atomics for tuple deferred hashing

--- packages/arb-avm-cpp/avm_values/src/tuple.cpp
+++ packages/arb-avm-cpp/avm_values/src/tuple.cpp
@@ -229,8 +229,12 @@ void Tuple::calculateHashPreImage() const {
                 }
             }
             if (!found_complex) {
-                tup.tpl->cachedPreImage = calcHashPreImage(tup);
-                tup.tpl->deferredHashing = false;
+                tup.tpl->cachedPreImage.writeAtomic(calcHashPreImage(tup));
+                // The "release" ordering here ensures any other thread
+                // with the default seqcst "acquire" ordering will see
+                // the updated hash pre image if it sees this write.
+                tup.tpl->deferredHashing.store(false,
+                                               std::memory_order_release);
                 tups.pop_back();
             }
         }
