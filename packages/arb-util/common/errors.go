662fd188832be2689a2caff71474a20ad53a1605
Joshua Colvin <jcolvin@offchainlabs.com>
2022-06-06 14:43:44 -0700

    address code review comments

--- packages/arb-util/common/errors.go
+++ packages/arb-util/common/errors.go
@@ -3,10 +3,5 @@ package common
 import "strings"
 
 func IsFatalError(err error) bool {
-	if strings.Contains(err.Error(), "arbcore thread aborted") ||
-		strings.Contains(err.Error(), "aborting inbox reader thread") {
-		return true
-	}
-
-	return false
+	return strings.Contains(err.Error(), "arbcore thread aborted")
 }
