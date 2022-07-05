c39180aaba295da15be5f2164ab97896bb69cddc
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-07-05 17:14:20 +0800

    revert: ITransferAndCall.sol

--- packages/arb-bridge-peripherals/contracts/tokenbridge/libraries/ITransferAndCall.sol
+++ packages/arb-bridge-peripherals/contracts/tokenbridge/libraries/ITransferAndCall.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-// solhint-disable-next-line compiler-version
-pragma solidity >=0.4.21 <0.9.0;
+pragma solidity >0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 
