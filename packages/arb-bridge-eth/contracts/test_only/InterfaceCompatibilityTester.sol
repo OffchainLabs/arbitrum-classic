68ff31fb408c90e457e97a828a827e0d40fd74a8
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-07-06 21:35:52 +0800

    refactor: rename to InterfaceCompatibilityTester

--- packages/arb-bridge-eth/contracts/test_only/InterfaceCompatibilityTester.sol
+++ packages/arb-bridge-eth/contracts/test_only/InterfaceCompatibilityTester.sol
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-pragma solidity ^0.8.7;
+pragma solidity >=0.6.9 <0.9.0;
 
 import "../arch/IOneStepProof.sol";
 import "../bridge/interfaces/IBridge.sol";
